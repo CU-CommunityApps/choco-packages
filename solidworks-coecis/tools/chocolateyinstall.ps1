0231dcc53ca8384859fe2ebf984d422d737208f1
Marty Sullivan <mjs472@mjs472.local>
2017-09-17 20:07:25 -0400

    Fix solidworks silent args

--- solidworks-coecis/tools/chocolateyinstall.ps1
+++ solidworks-coecis/tools/chocolateyinstall.ps1
@@ -8,11 +8,13 @@ Add-WindowsFeature NET-Framework-Core
 Start-Process -FilePath msiexec.exe -ArgumentList "/package `"$toolsDir\64bit\Microsoft_VBA\vba71.msi`" /passive" -NoNewWindow -Wait
 Start-Process -FilePath msiexec.exe -ArgumentList "/package `"$toolsDir\64bit\RemoteDebugger\rdbgexp.msi`" /passive" -NoNewWindow -Wait
 
+$serial = Get-Content "$toolsDir\serial.txt"
+
 $packageArgs = @{
 	packageName = $packageName
 	fileType    = 'msi'
 	file        = "$toolsDir\64bit\SOLIDWORKS\SolidWorks.msi"
-	silentArgs  = "OFFICEOPTION=3 ENABLEPERFORMANCE=0 SERVERLIST=1945@solidworks-lic.coecis.cornell.edu"
+	silentArgs  = "OFFICEOPTION=3 ENABLEPERFORMANCE=0 SERVERLIST=1945@solidworks-lic.coecis.cornell.edu SOLIDWORKSSERIALNUMBER="$serial" /qr"
 }
 
 Install-ChocolateyInstallPackage @packageArgs  
