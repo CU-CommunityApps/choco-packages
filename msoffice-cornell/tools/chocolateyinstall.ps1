cd9dfaa40fa1439b156f84c34cfadbb70689f2f4
Marty Sullivan <mjs472@dhcp-10-36-22-278.cit.cornell.edu>
2017-09-18 16:13:57 -0400

    Add MS Office w/ CU Site License

---
+++ msoffice-cornell/tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+$packageName= 'msoffice-cornell'
+$toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$url        = "https://s3.amazonaws.com/cu-deng-appstream-packages/packages/$packageName.zip"
+
+Install-ChocolateyZipPackage $packageName $url $toolsDir
+
+Add-WindowsFeature NET-Framework-Core
+
+$packageArgs = @{
+	packageName = $packageName
+	fileType    = 'exe'
+	file        = "$toolsDir\setup.exe"
+	silentArgs  = "/adminfile $toolsDir\cu_office_config.MSP"
+}
+
+$officePath = (Get-ItemProperty "hklm:\software\microsoft\windows\currentversion\app paths\WINWORD.EXE").Path
+
+echo "Activating MS Office..."
+$result = & cscript ${officePath}ospp.vbs /sethst:kms02.cit.cornell.edu | Out-String
+echo $result
+$result = & cscript ${officePath}ospp.vbs /act | Out-String
+echo $result
+
+Install-ChocolateyInstallPackage @packageArgs  
