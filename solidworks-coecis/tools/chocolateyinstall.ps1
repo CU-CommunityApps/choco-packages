69196b7079f88f73e67f6ff3410110ae3e29e377
Marty Sullivan <mjs472@dhcp-10-36-22-278.cit.cornell.edu>
2017-09-18 12:36:46 -0400

    Try using the packaged EXE, instead of the MSI

--- solidworks-coecis/tools/chocolateyinstall.ps1
+++ solidworks-coecis/tools/chocolateyinstall.ps1
@@ -6,16 +6,16 @@ $url        = "https://s3.amazonaws.com/cu-deng-appstream-packages/packages/$pac
 Install-ChocolateyZipPackage $packageName $url $toolsDir
 
 Add-WindowsFeature NET-Framework-Core
-Start-Process -FilePath msiexec.exe -ArgumentList "/package `"$toolsDir\64bit\Microsoft_VBA\vba71.msi`" /passive" -NoNewWindow -Wait
-Start-Process -FilePath msiexec.exe -ArgumentList "/package `"$toolsDir\64bit\RemoteDebugger\rdbgexp.msi`" /passive" -NoNewWindow -Wait
+#Start-Process -FilePath msiexec.exe -ArgumentList "/package `"$toolsDir\64bit\Microsoft_VBA\vba71.msi`" /passive" -NoNewWindow -Wait
+#Start-Process -FilePath msiexec.exe -ArgumentList "/package `"$toolsDir\64bit\RemoteDebugger\rdbgexp.msi`" /passive" -NoNewWindow -Wait
 
 $serial = Get-Content "$toolsDir\serial.txt"
 
 $packageArgs = @{
 	packageName = $packageName
-	fileType    = 'msi'
-	file        = "$toolsDir\64bit\SOLIDWORKS\SolidWorks.msi"
-	silentArgs  = "OFFICEOPTION=3 ENABLEPERFORMANCE=0 SERVERLIST=1945@solidworks-lic.coecis.cornell.edu SOLIDWORKSSERIALNUMBER=`"$serial`" /qr"
+	fileType    = 'exe'
+	file        = "$toolsDir\startswinstall.exe"
+	silentArgs  = "/install /showui /now"
 }
 
 Install-ChocolateyInstallPackage @packageArgs  
