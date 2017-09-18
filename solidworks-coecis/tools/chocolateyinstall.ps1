fb3c8cd63da19518b2d58d3c625d7c03ae1d6fa1
Marty Sullivan <mjs472@dhcp-10-36-22-278.cit.cornell.edu>
2017-09-18 13:22:01 -0400

    solidworks cleanup, now working

--- solidworks-coecis/tools/chocolateyinstall.ps1
+++ solidworks-coecis/tools/chocolateyinstall.ps1
@@ -1,15 +1,10 @@
 $packageName= 'solidworks-coecis'
-#$toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $toolsDir   = "C:\SOLIDWORKS"
 $url        = "https://s3.amazonaws.com/cu-deng-appstream-packages/packages/$packageName.zip"
 
 Install-ChocolateyZipPackage $packageName $url $toolsDir
 
 Add-WindowsFeature NET-Framework-Core
-#Start-Process -FilePath msiexec.exe -ArgumentList "/package `"$toolsDir\64bit\Microsoft_VBA\vba71.msi`" /passive" -NoNewWindow -Wait
-#Start-Process -FilePath msiexec.exe -ArgumentList "/package `"$toolsDir\64bit\RemoteDebugger\rdbgexp.msi`" /passive" -NoNewWindow -Wait
-
-$serial = Get-Content "$toolsDir\serial.txt"
 
 $packageArgs = @{
 	packageName = $packageName
