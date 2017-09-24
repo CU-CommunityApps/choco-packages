efd3b882d1a1f7cfef0a91a73e58258a78e0fec6
Marty Sullivan <marty-sullivan@users.noreply.github.com>
2017-09-24 19:11:52 -0400

    Add environment variable for inventor license server

--- inventor-coecis/tools/chocolateyinstall.ps1
+++ inventor-coecis/tools/chocolateyinstall.ps1
@@ -8,8 +8,10 @@ $packageArgs = @{
 	packageName = $packageName
 	fileType    = 'exe'
 	file        = "$toolsDir\Img\Setup.exe"
-	silentArgs  = "/qb /I $toolsDir\Img\autodesk-coecis.ini /language en-us"
+	silentArgs  = "/qb /I $toolsDir\Img\inventor-coecis.ini /language en-us"
     validExitCodes = @(0, 259)
 }
+
+[Environment]::SetEnvironmentVariable("ADSKFLEX_LICENSE_FILE", "27200@autodesk-lic.coecis.cornell.edu", "Machine")
 
 Install-ChocolateyInstallPackage @packageArgs  
