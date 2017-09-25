bac8a12f16d0e5f91a10fa90db2b3a0476f5d306
Marty Sullivan <mjs472@mjs472.local>
2017-09-24 22:16:55 -0400

    Add CES Edupack

---
+++ packages/ces-edupack-coecis/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+$packageName= 'ces-edupack'
+$toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$url        = "https://s3.amazonaws.com/cu-deng-appstream-packages/packages/$packageName.zip"
+
+Install-ChocolateyZipPackage $packageName $url $toolsDir
+
+$packageArgs = @{
+	packageName = $packageName
+	fileType    = 'exe'
+	file        = "$toolsDir\edupack_setup.2017.exe"
+	silentArgs  = "/quiet"
+}
+
+Install-ChocolateyInstallPackage @packageArgs  
