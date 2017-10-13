03a2001ef01f02ff7674f6f2503f417bdc7977d9
Marty Sullivan <mjs472@mjs472.local>
2017-10-13 17:03:58 -0400

    Test appstream catalog creation w/ office

---
+++ packages/msoffice-cornell/tools/appstream/catalog.sql
@@ -0,0 +1,49 @@
+INSERT INTO Applications (Name, AbsolutePath, DisplayName, IconFilePath, LaunchParameters, WorkingDirectory) 
+VALUES (
+	'WINWORD',
+	'C:\Program Files\Microsoft Office\Office16\WINWORD.EXE',
+	'MS Word',
+	'C:\ProgramData\Amazon\Photon\AppCatalogHelper\AppIcons\word.png',
+	'',
+	''
+);
+
+INSERT INTO Applications (Name, AbsolutePath, DisplayName, IconFilePath, LaunchParameters, WorkingDirectory) 
+VALUES (
+	'EXCEL',
+	'C:\Program Files\Microsoft Office\Office16\EXCEL.EXE',
+	'MS Excel',
+	'C:\ProgramData\Amazon\Photon\AppCatalogHelper\AppIcons\excel.png',
+	'',
+	''
+);
+
+INSERT INTO Applications (Name, AbsolutePath, DisplayName, IconFilePath, LaunchParameters, WorkingDirectory) 
+VALUES (
+	'MSACCESS',
+	'C:\Program Files\Microsoft Office\Office16\MSACCESS.EXE',
+	'MS Access',
+	'C:\ProgramData\Amazon\Photon\AppCatalogHelper\AppIcons\access.png',
+	'',
+	''
+);
+
+INSERT INTO Applications (Name, AbsolutePath, DisplayName, IconFilePath, LaunchParameters, WorkingDirectory) 
+VALUES (
+	'ONENOTE',
+	'C:\Program Files\Microsoft Office\Office16\ONENOTE.EXE',
+	'MS OneNote',
+	'C:\ProgramData\Amazon\Photon\AppCatalogHelper\AppIcons\onenote.png',
+	'',
+	''
+);
+
+INSERT INTO Applications (Name, AbsolutePath, DisplayName, IconFilePath, LaunchParameters, WorkingDirectory) 
+VALUES (
+	'POWERPNT',
+	'C:\Program Files\Microsoft Office\Office16\POWERPNT.EXE',
+	'MS Powerpoint',
+	'C:\ProgramData\Amazon\Photon\AppCatalogHelper\AppIcons\powerpoint.png',
+	'',
+	''
+);
--- packages/msoffice-cornell/tools/chocolateyinstall.ps1
+++ packages/msoffice-cornell/tools/chocolateyinstall.ps1
@@ -1,5 +1,7 @@
 $packageName= 'msoffice-cornell'
 $toolsDir   = "C:\Windows\Temp\$packageName"
+$sqlite     = "C:\ProgramData\chocolatey\bin\sqlite3.exe"
+$appCatalog = "C:\ProgramData\Amazon\Photon\PhotonAppCatalog.sqlite"
 $url        = "https://s3.amazonaws.com/cu-deng-appstream-packages/packages/$packageName.zip"
 
 Install-ChocolateyZipPackage $packageName $url $toolsDir
@@ -15,10 +17,14 @@ $packageArgs = @{
 
 Install-ChocolateyInstallPackage @packageArgs  
 
-$officePath = (Get-ItemProperty "hklm:\software\microsoft\windows\currentversion\app paths\WINWORD.EXE").Path
+# Add AppStream related items
+Copy-Item "$toolsDir\appstream\icons\*" "C:\ProgramData\Amazon\Photon\AppCatalogHelper\AppIcons"
+Start-Process -FilePath $sqlite -ArgumentList "$appCatalog `".read $toolsDir\appstream\catalog.sql`"" -NoNewWindow -Wait
 
-echo "Activating MS Office..."
-$result = & cscript ${officePath}ospp.vbs /sethst:kms02.cit.cornell.edu | Out-String
-echo $result
-$result = & cscript ${officePath}ospp.vbs /act | Out-String
-echo $result
+#$officePath = (Get-ItemProperty "hklm:\software\microsoft\windows\currentversion\app paths\WINWORD.EXE").Path
+
+#echo "Activating MS Office..."
+#$result = & cscript ${officePath}ospp.vbs /sethst:kms02.cit.cornell.edu | Out-String
+#echo $result
+#$result = & cscript ${officePath}ospp.vbs /act | Out-String
+#echo $result
