ba363e39e62c54fde503180547ed5de61774252e
EC2 Default User <ec2-user@ip-10-92-15-121.ec2.internal>
2017-11-23 07:03:48 +0000

    Fix filename

--- bootstrap/bootstrap.ps1
+++ bootstrap/bootstrap.ps1
@@ -4,5 +4,5 @@ iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/in
 
 Start-Process -NoNewWindow -Wait -FilePath 'C:\ProgramData\chocolatey\bin\choco.exe'    -ArgumentList 'install git python -y'
 Start-Process -NoNewWindow -Wait -FilePath 'C:\Program Files\Git\bin\git.exe'           -ArgumentList 'clone https://github.com/CU-CommunityApps/choco-packages.git .\choco-packages'
-Start-Process -NoNewWindow -Wait -FilePath 'C:\Python36\python.exe'                     -ArgumentList '.\choco-packages\bootstrap\go.py'
+Start-Process -NoNewWindow -Wait -FilePath 'C:\Python36\python.exe'                     -ArgumentList '.\choco-packages\bootstrap\bootstrap.py'
 
