86d92fa628782f295384b7d5c3ace2c0135a8ac4
EC2 Default User <ec2-user@ip-10-92-15-121.ec2.internal>
2017-11-24 02:01:43 +0000

    Add sfn activity to bootstrap

--- bootstrap/utils/__init__.py
+++ bootstrap/utils/__init__.py
@@ -6,7 +6,7 @@ from boto3.session import Session
 from botocore.exceptions import ClientError, WaiterError
 from io import StringIO
 from multiprocessing import cpu_count, Process, JoinableQueue as Queue
-from os import mkdir, path, remove
+from os import environ, mkdir, path, remove
 from sys import stdout, stderr
 from urllib.request import urlopen
 
@@ -21,11 +21,15 @@ class ImageBuild(object):
     def init_logging(self):
         self.logger = logging.getLogger()
         self.logger.setLevel(logging.INFO)
+        self.loggerText = StringIO()
 
         sh = logging.StreamHandler(stdout)
+        shText = logging.StreamHandler(self.loggerText)
         formatter = logging.Formatter('[%(levelname)s] %(asctime)s %(message)s')
         sh.setFormatter(formatter)
+        shText.setFormatter(formatter)
         self.logger.addHandler(sh)
+        self.logger.addHandler(shText)
 
         logging.getLogger('boto').propagate = False
         logging.getLogger('boto3').propagate = False
@@ -43,11 +47,26 @@ class ImageBuild(object):
 
         return logger, log
 
+    def get_stack_outputs(self):
+        aws = Session()
+        cfn = self.aws.resource('cloudformation')
+        stack = cfn.Stack('{App}{Env}Serverless'.format(App=self.appName, Env=self.envName))
+
+        outputs = { }
+        for output in stack.outputs:
+            outputs[output['OutputKey']] = {
+                'value': output['OutputValue'],
+                'exportName': output['ExportName'] if 'ExportName' in output else None,
+            }
+
+        return outputs
+
 class AppStreamImageBuild(ImageBuild):
 
     def __init__(self):
         super().__init__()
 
+    def bootstrap(self):
         try:
             self.logger.info('Getting System User-Data')
             self.user_data = json.load(urlopen('http://169.254.169.254/latest/user-data'))
@@ -59,14 +78,18 @@ class AppStreamImageBuild(ImageBuild):
             raise ImageBuildException('NOT_APPSTREAM_IMAGE_BUILDER')
 
         self.arn = self.user_data['resourceArn'].split(':')
+        self.partition = self.arn[1]
         self.region = self.arn[3]
+        self.account = self.arn[4]
         builderName = self.arn[5].replace('image-builder/', '').split('.')
 
         if len(builderName) < 2:
             raise ImageBuildException('BAD_APPSTREAM_IMAGE_BUILDER_NAME')
 
         self.buildId = builderName[0]
-        self.bucketName = builderName[1]
+        self.appName = builderName[1]
+        self.envName = builderName[2]
+        self.bucketName = builderName[3]
 
         sessionPath = 'https://s3.amazonaws.com/{Bucket}/federated-sessions/{BuildId}.json'.format(Bucket=self.bucketName, BuildId=self.buildId)
 
@@ -85,7 +108,22 @@ class AppStreamImageBuild(ImageBuild):
             region_name=self.region,
         )
 
-        self.logger.info('AppStreamImageBuild {Build} Initialized'.format(Build=self.buildId))
+        self.sfn = self.aws.client('stepfunctions')
+        self.outputs = self.get_stack_outputs()
+        bootstrapActivity = '{App}{Env}AppStreamImageBootstrapActivity'.format(App=self.appName, Env=self.envName)
+        bootstrapArn = self.outputs[bootstrapActivity]['value']
+        
+        task = self.sfn.get_activity_task(
+            activityArn=bootstrapArn,
+            workerName=self.buildId,
+        )
+
+        self.sfn.send_task_success(
+            taskToken=task['taskToken'],
+            output=task['input'],
+        )
+
+        self.logger.info('AppStreamImageBuild {Build} Bootstrapped!'.format(Build=self.buildId))
 
 class EC2ImageBuild(ImageBuild):
 
