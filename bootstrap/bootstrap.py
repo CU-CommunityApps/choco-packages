ecd7c625e1b8f13f384cbc9553fcefac3f409392
EC2 Default User <ec2-user@ip-10-92-15-121.ec2.internal>
2017-11-24 23:24:34 +0000

    Add better top-level stack trace

--- bootstrap/bootstrap.py
+++ bootstrap/bootstrap.py
@@ -1,13 +1,15 @@
+import logging
 from utils import *
 
 try:
     builder = AppStreamImageBuild()
 except ImageBuildException as e:
-    print(e) # TODO Open other types of builds 
+    logging.exception('Init Exception:')
+    # TODO Open other types of builds 
 
 try:
     builder.bootstrap()
     builder.install_packages()
 except Exception as e:
-    print(e)
+    logging.exception('Build Exception:')
 
