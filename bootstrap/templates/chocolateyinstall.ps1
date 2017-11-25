d79186d318bb51c078f1f91bb2d0134bdde56e38
EC2 Default User <ec2-user@ip-10-92-15-121.ec2.internal>
2017-11-25 02:07:12 +0000

    Add traces to chocolateyinstall

--- bootstrap/templates/chocolateyinstall.ps1
+++ bootstrap/templates/chocolateyinstall.ps1
@@ -7,10 +7,15 @@ $FILETYPE =     "{FileType}"
 $ARGUMENTS =    "{Arguments}"
 $VALID_CODES =  "{ValidCodes}"
 
-$TEMP_DIR =     Join-Path $env:SYSTEMROOT   "Temp\choco-bootstrap"
-$INSTALL_DIR =  Join-Path $TEMP_DIR         "choco-packages\$PACKAGE"
-$PRE_SCRIPT =   Join-Path $INSTALL_DIR      "pre_install.ps1"
-$POST_SCRIPT =  Join-Path $INSTALL_DIR      "post_install.ps1"
+$TEMP_DIR =     Join-Path $env:SYSTEMROOT "Temp\choco-bootstrap"
+$INSTALL_DIR =  Join-Path $TEMP_DIR "choco-packages\$PACKAGE"
+$PRE_SCRIPT =   Join-Path $INSTALL_DIR "pre_install.ps1"
+$POST_SCRIPT =  Join-Path $INSTALL_DIR "post_install.ps1"
+
+Write-Output "TEMP_DIR: $TEMP_DIR"
+Write-Output "INSTALL_DIR: $INSTALL_DIR"
+Write-Output "PRE_SCRIPT: $PRE_SCRIPT"
+Write-Output "POST_SCRIPT: $POST_SCRIPT"
 
 $S3_URI = "https://s3.amazonaws.com/$BUCKET/packages/$PACKAGE.zip"
 Install-ChocolateyZipPackage $PACKAGE $S3_URI $INSTALL_DIR
