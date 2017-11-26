a6bbf74843caf8ad2fe32de739d2a0c9a52cc835
EC2 Default User <ec2-user@ip-10-92-15-121.ec2.internal>
2017-11-26 04:37:14 +0000

    Fix space paths in msoffice

--- packages/msoffice-cornell/tools/postinstall.ps1
+++ packages/msoffice-cornell/tools/postinstall.ps1
@@ -4,5 +4,5 @@ $CSCRIPT =  Join-Path $env:SYSTEMROOT   'System32\cscript.exe'
 $OSPP_VBS = Join-Path $env:PROGRAMFILES 'Microsoft Office\Office16\OSPP.VBS'
 $KMS_HOST = 'kms02.cit.cornell.edu'
 
-Start-Process -NoNewWindow -Wait -FilePath $CSCRIPT -ArgumentList "$OSPP_VBS /sethst:$KMS_HOST"
-Start-Process -NoNewWindow -Wait -FilePath $CSCRIPT -ArgumentList "$OSPP_VBS /act"
+Start-Process -NoNewWindow -Wait -FilePath $CSCRIPT -ArgumentList "`"$OSPP_VBS`" /sethst:$KMS_HOST"
+Start-Process -NoNewWindow -Wait -FilePath $CSCRIPT -ArgumentList "`"$OSPP_VBS`" /act"
