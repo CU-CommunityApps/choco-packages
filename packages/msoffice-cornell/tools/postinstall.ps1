61438df98c4ff7d8e3ad533a1596899b02027653
EC2 Default User <ec2-user@ip-10-92-15-121.ec2.internal>
2018-01-18 23:46:10 +0000

    guess just need to activate via script

--- packages/msoffice-cornell/tools/postinstall.ps1
+++ packages/msoffice-cornell/tools/postinstall.ps1
@@ -1,8 +1,8 @@
 # Runs after the choco package is installed
 
-#$CSCRIPT =  Join-Path $env:SYSTEMROOT   'System32\cscript.exe'
-#$OSPP_VBS = Join-Path $env:PROGRAMFILES 'Microsoft Office\Office16\OSPP.VBS'
-#$KMS_HOST = 'kms02.cit.cornell.edu'
+$CSCRIPT =  Join-Path $env:SYSTEMROOT   'System32\cscript.exe'
+$OSPP_VBS = Join-Path $env:PROGRAMFILES 'Microsoft Office\Office16\OSPP.VBS'
+$KMS_HOST = 'kms02.cit.cornell.edu'
 
-#Start-Process -NoNewWindow -Wait -FilePath $CSCRIPT -ArgumentList "`"$OSPP_VBS`" /sethst:$KMS_HOST"
-#Start-Process -NoNewWindow -Wait -FilePath $CSCRIPT -ArgumentList "`"$OSPP_VBS`" /act"
+Start-Process -NoNewWindow -Wait -FilePath $CSCRIPT -ArgumentList "`"$OSPP_VBS`" /sethst:$KMS_HOST"
+Start-Process -NoNewWindow -Wait -FilePath $CSCRIPT -ArgumentList "`"$OSPP_VBS`" /act"
