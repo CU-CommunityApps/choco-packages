374a41b2fbd1586f7c917793e8e713569fbd1144
lb30 <39345501+lb30@users.noreply.github.com>
2018-07-19 14:19:09 -0400

    Update postinstall.ps1

--- packages/spss-ciser/tools/postinstall.ps1
+++ packages/spss-ciser/tools/postinstall.ps1
@@ -1,4 +1,4 @@
 # Runs after the choco package is installed
 # Runs after the choco package is installed
-Push-Location "$(${env:ProgramFiles})\IBM\SPSS\Statistics\25"; Invoke-Expression "& .\licenseactivator.exe %SPSS_LICENSE_KEY%"
+start-process "$env:programfiles\IBM\SPSS\Statistics\25\licenseactivator.exe" -ArgumentList "$env:SPSS_LICENSE_KEY"
 
