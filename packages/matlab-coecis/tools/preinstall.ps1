716dd9eae38bdd6c5d393130952cf8d3f5eba3e0
lb30 <39345501+lb30@users.noreply.github.com>
2018-08-01 08:05:25 -0400

    Update preinstall.ps1

--- packages/matlab-coecis/tools/preinstall.ps1
+++ packages/matlab-coecis/tools/preinstall.ps1
@@ -1,3 +1,3 @@
 # Runs before the choco package is installed
 
-Copy-Item "$INSTALL_DIR\network.lic" "%PROGRAMFILES%\MATLAB\R2018a\licenses\network.lic"
+Copy-Item "$INSTALL_DIR\network.lic" "C:\ProgramData\network.lic"
