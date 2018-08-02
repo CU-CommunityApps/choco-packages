de9cbe306e47640984bf0ef098c5a7aabd9ae831
Dan Klinger <dpk25@cornell.edu>
2018-08-02 09:32:31 -0400

    to %INSTALL_DIR% and Inventor NO REBOOT

--- packages/matlab-coecis/tools/preinstall.ps1
+++ packages/matlab-coecis/tools/preinstall.ps1
@@ -1,4 +1,4 @@
 # Runs before the choco package is installed
 
-Copy-Item "$INSTALL_DIR\network.lic" "C:\ProgramData\network.lic"
+Copy-Item "%INSTALL_DIR%\network.lic" "%ALLUSERSPROFILE%\network.lic"
 
