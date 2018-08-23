321332ba9d6e6c95b5df2abb39f4514184d89554
lb30 <39345501+lb30@users.noreply.github.com>
2018-08-23 13:49:29 -0400

    Update solidworks-accept-eula.ps1

--- packages/solidworks-coecis/tools/solidworks-accept-eula.ps1
+++ packages/solidworks-coecis/tools/solidworks-accept-eula.ps1
@@ -7,9 +7,9 @@ If (!(Test-Path $solid)){New-Item -Path $solid -force}
 New-ItemProperty -Path $solid -Name "EULA Accepted 2018 SP4.0 $env:ComputerName $env:UserName" -Value "Yes" -PropertyType "string" -force
 
 # Composer
-$3dComposer = "Registry::\HKCU\Software\Dassault Systemes\3DVIAComposer\7.4\Preferences"
+$3dComposer = "Registry::\HKCU\Software\Dassault Systemes\7.5\Preferences"
 If (!(Test-Path $3dComposer)){New-Item -Path $3dComposer -force}
-New-ItemProperty -Path $3dComposer -Name "EulaAccepted 2017 SP3.0 $env:ComputerName $env:UserName" -Value "1" -PropertyType "DWORD" -Force
+New-ItemProperty -Path $3dComposer -Name "EulaAccepted 2018 SP4.0 $env:ComputerName $env:UserName" -Value "1" -PropertyType "DWORD" -Force
 
 $composer = "Registry::\HKCU\Software\Dassault Systemes\Composer\7.5\Preferences"
 If (!(Test-Path $composer)){New-Item -Path $composer -force}
