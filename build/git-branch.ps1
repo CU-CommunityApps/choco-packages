2a97bded553fad039feedbf574e844c7d7d52b0f
Marty Sullivan <marty@martysullivan.com>
2018-09-03 16:18:41 +0000

    try no select

--- build/git-branch.ps1
+++ build/git-branch.ps1
@@ -4,6 +4,7 @@ Start-Process -FilePath $git `
     -ArgumentList "branch -a --contains $Env:CODEBUILD_SOURCE_VERSION" `
     -RedirectStandardOutput "$Env:TEMP\git-branch.txt"
     
-$branch = Get-Content "$Env:TEMP\git-branch.txt" | Select -Index 4
+#$branch = Get-Content "$Env:TEMP\git-branch.txt" | Select -Index 4
+$branch = Get-Content "$Env:TEMP\git-branch.txt"
 $Env:CODEBUILD_SOURCE_BRANCH = $branch
 Write-Output $Env:CODEBUILD_SOURCE_BRANCH
