06a257c29ad353761437b72c197d22c6a5068fc8
Marty Sullivan <marty@martysullivan.com>
2018-09-03 15:41:45 +0000

    contains only

--- build/git-branch.ps1
+++ build/git-branch.ps1
@@ -1,18 +1,9 @@
 $git = "$Env:TEMP\ChocoPackerBuild\packages\Git-Windows-Minimal.2.18.0\tools\cmd\git.exe"
 
 Start-Process -FilePath $git `
-    -ArgumentList "symbolic-ref HEAD --short" `
+    -ArgumentList "branch -a --contains $Env:CODEBUILD_SOURCE_VERSION" `
     -RedirectStandardOutput "$Env:TEMP\git-branch.txt"
-
-$branch = Get-Content "$Env:TEMP\git-branch.txt"
-
-if (-Not $branch) {
-    Start-Process -FilePath $git `
-        -ArgumentList "branch -a --contains HEAD" `
-        -RedirectStandardOutput "$Env:TEMP\git-branch.txt"
-        
-    $branch = (Get-Content "$Env:TEMP\git-branch.txt")[1]
-}
-
+    
+$branch = Get-Content "$Env:TEMP\git-branch.txt" | Select -Index 4
 $Env:CODEBUILD_SOURCE_BRANCH = $branch
 Write-Output $Env:CODEBUILD_SOURCE_BRANCH
