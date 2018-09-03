49725da286c683f0c3a7290a660805d5583e0dd1
Marty Sullivan <marty@martysullivan.com>
2018-09-03 15:06:22 +0000

    fix branch

--- build/git-branch.ps1
+++ build/git-branch.ps1
@@ -1,8 +1,14 @@
 $git = "$Env:TEMP\ChocoPackerBuild\packages\Git-Windows-Minimal.2.18.0\tools\cmd\git.exe"
-$branch = $(Start-Process -FilePath $git -ArgumentList "symbolic-ref HEAD --short" -NoNewWindow)
 
-if (-Not $branch) {
-    $branch = $(Start-Process -FilePath $git -ArgumentList "branch -a --contains HEAD" -NoNewWindow)
-}
+Start-Process -FilePath $git `
+    -ArgumentList "symbolic-ref $Env:CODEBUILD_SOURCE_VERSION --short" `
+    -RedirectStandardOutput "$Env:TEMP\git-branch.txt"
 
+$branch = Get-Content "$Env:TEMP\git-branch.txt"
+
+#if (-Not $branch) {
+#    $branch = $(Start-Process -FilePath $git -ArgumentList "branch -a --contains HEAD" -NoNewWindow)
+#}
+
+Write-Output $Env:CODEBUILD_SOURCE_BRANCH
 $Env:CODEBUILD_SOURCE_BRANCH = $branch
