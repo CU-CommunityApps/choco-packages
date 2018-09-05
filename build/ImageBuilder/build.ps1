efb1622357ff3553b17a6c076d4728f26e65de03
Marty Sullivan <marty@martysullivan.com>
2018-09-05 16:14:48 +0000

    rename image-builder-cornell

--- build/ImageBuilder/build.ps1
+++ build/ImageBuilder/build.ps1
@@ -1,5 +1,5 @@
 New-Item -ItemType Junction -Path $Env:TEMP\ImageBuilderBuild -Value $Env:CODEBUILD_SRC_DIR\build\ImageBuilder
 cd $Env:TEMP\ImageBuilderBuild
 nuget.exe restore
-msbuild /p:OutputPath=$Env:CODEBUILD_SRC_DIR\packages\_image-builder\tools\ImageBuilder
+msbuild /p:OutputPath=$Env:CODEBUILD_SRC_DIR\packages\image-builder\tools\ImageBuilder
 cd $Env:CODEBUILD_SRC_DIR
