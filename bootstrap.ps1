1849a398381ff0becfa486690ec8d75aaad35806
Marty Sullivan <marty@martysullivan.com>
2018-09-10 03:21:20 +0000

    make sure location is BUILD_DIR

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -48,6 +48,11 @@ if (-Not (Test-Path $BUILD_DIR)) {
     Write-Output "Installing ImageBuilder Package"
     Start-Process -FilePath "choco.exe" -ArgumentList "install $BUILDER_PACKAGE -s $PACKAGE_DIR;$CHOCO_REPO --no-progress -r -y" -NoNewWindow -Wait
 }
+else {
+    
+    # No Path set for SYSTEM so move to BUILD_DIR
+    Set-Location $BUILD_DIR
+}
 
 # Run ImageBuilder
 Write-Output "Running ImageBuilder"
