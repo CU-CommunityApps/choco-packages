3b659794becac19b065c5f5243a18ada8d3acae2
Dan Klinger <dpk25@cornell.edu>
2018-11-08 09:33:23 -0500

    Uninstall Firefox

--- packages/base-cornellstream/tools/postinstall.ps1
+++ packages/base-cornellstream/tools/postinstall.ps1
@@ -1,2 +1,14 @@
 # Runs after the choco package is installed
+# List of applications to remove
+$app = "Firefox"
 
+# Get uninstall info for each possible application
+If (gci -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall  | `
+gp | where {$_.DisplayName -match $app}){
+
+    $install = gci -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall  | `
+    gp | where {$_.DisplayName -match $app} | select DisplayName, InstallLocation, UninstallString
+    
+    Start-Process $install.UninstallString -ArgumentList "/S" -WorkingDirectory $install.InstallLocation -WindowStyle Hidden
+
+}
