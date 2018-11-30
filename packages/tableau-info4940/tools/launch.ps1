84f006a185a4a4231583421f9731cbe4bb709a4b
Dan Klinger <dpk25@cornell.edu>
2018-11-30 12:36:33 -0500

    Script consolidation and friendly launcher

--- packages/tableau-info4940/tools/launch.ps1
+++
@@ -1,6 +0,0 @@
-﻿Do {write-host "Activating Tableau please wait..." -ForegroundColor Yellow;Start-Sleep -Seconds 5}
-Until((Get-ScheduledTask -TaskName Tableau_Activation).State -ne "Running")
-
-If ((Get-ScheduledTask -TaskName Tableau_Activation | Get-ScheduledTaskInfo).LastTaskResult -eq 0 -or `
-(Get-ScheduledTask -TaskName Tableau_Activation | Get-ScheduledTaskInfo).LastRunTime -eq $null){
-Start-Process -FilePath "$env:ProgramFiles\Tableau\Tableau 2018.3\bin\tableau.exe"}
--- packages/tableau-info4940/tools/logon_tasks.ps1
+++
@@ -1,8 +0,0 @@
-﻿# Tableau licensing process
-
-# Make sure time is set properly
-If ((gp HKLM:\SYSTEM\CurrentControlSet\Control\TimeZoneInformation).TimeZoneKeyName -match "Eastern Standard Time"){Restart-Service -Name W32Time}
-Else {New-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Control\TimeZoneInformation -Name "TimeZoneKeyName" -PropertyType "String" -Value "Eastern Standard Time" -Force; Restart-Service -Name W32Time}
-
-# Check Licensing service status
-If ((Get-Service -Name 'FlexNet Licensing Service 64').Status -ne "Running"){Start-Service -Name 'FlexNet Licensing Service 64'}
