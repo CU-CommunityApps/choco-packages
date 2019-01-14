ef8ee0b69fd8da93b86e869c0df5eef158288247
lb30 <39345501+lb30@users.noreply.github.com>
2019-01-14 16:29:31 -0500

    Update postinstall.ps1

--- packages/arcgispro-cals/tools/postinstall.ps1
+++ packages/arcgispro-cals/tools/postinstall.ps1
@@ -1,2 +1,5 @@
 # Runs after the choco package is installed
-
+  - File:       '%INSTALL_DIR%\ArcGISd5\ArcGIS_Pro_221_164752d5.msp'
+    FileType:   'msp'
+    Arguments:  '/qb'
+    ExitCodes:  [0]
