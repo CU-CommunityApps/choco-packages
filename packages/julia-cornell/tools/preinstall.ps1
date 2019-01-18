d5fe06341fa04d620a5a1713ccf7af9bd2eb54d5
Dan Klinger <dpk25@cornell.edu>
2019-01-18 01:47:28 -0500

    Update preinstall.ps1

--- packages/julia-cornell/tools/preinstall.ps1
+++ packages/julia-cornell/tools/preinstall.ps1
@@ -1,2 +1,2 @@
 # Runs before the choco package is installed
-Start-Process "$env:ALLUSERSPROFILE\chocolatey\choco.exe" -ArgumentList "install julia --install-arguments=/D=C:\ProgramData\Julia"
+Start-Process "$env:ALLUSERSPROFILE\chocolatey\choco.exe" -ArgumentList "install julia --confirm --install-arguments=/D=C:\ProgramData\Julia"
