bf7a9db155769d3323c3d234717395de352060d5
Dan Klinger <dpk25@cornell.edu>
2019-01-23 18:31:32 -0500

    Update postinstall.ps1

--- packages/julia-cornell/tools/postinstall.ps1
+++ packages/julia-cornell/tools/postinstall.ps1
@@ -7,6 +7,7 @@ $Ar = New-Object System.Security.AccessControl.FileSystemAccessRule("Everyone", 
 $Acl.SetAccessRule($Ar)
 Set-Acl "C:\Work" $Acl
 
+C:\users\username\.julia\config\startup.jl
 push!(LOAD_PATH, "C:\\Work")
 empty!(DEPOT_PATH)
 push!(DEPOT_PATH, "C:\\Work")
