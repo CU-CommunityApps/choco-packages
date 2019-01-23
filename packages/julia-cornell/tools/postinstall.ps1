f17b81bb941848e65cd6c9bcd299e969f22936ae
Dan Klinger <dpk25@cornell.edu>
2019-01-23 18:28:41 -0500

    Update postinstall.ps1

--- packages/julia-cornell/tools/postinstall.ps1
+++ packages/julia-cornell/tools/postinstall.ps1
@@ -1,2 +1,8 @@
 # Runs after the choco package is installed
 MD C:\Work
+$Acl = Get-Acl "C:\Work"
+
+$Ar = New-Object System.Security.AccessControl.FileSystemAccessRule("Everyone", "FullControl", "ContainerInherit,ObjectInherit", "None", "Allow")
+
+$Acl.SetAccessRule($Ar)
+Set-Acl "C:\Work" $Acl
