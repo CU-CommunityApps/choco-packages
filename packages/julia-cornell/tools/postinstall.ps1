553afb33ba6946d9876350ea12cee6bb8f2ea5ea
Dan Klinger <dpk25@cornell.edu>
2019-01-24 16:55:46 -0500

    Update postinstall.ps1

--- packages/julia-cornell/tools/postinstall.ps1
+++ packages/julia-cornell/tools/postinstall.ps1
@@ -1,11 +1,6 @@
 # Runs after the choco package is installed
-MD C:\Work
-$Acl = Get-Acl "C:\Work"
 
-$Ar = New-Object System.Security.AccessControl.FileSystemAccessRule("Everyone", "FullControl", "ContainerInherit,ObjectInherit", "None", "Allow")
 
-$Acl.SetAccessRule($Ar)
-Set-Acl "C:\Work" $Acl
 
 C:\users\username\.julia\config\startup.jl
 push!(LOAD_PATH, "C:\\Work")
