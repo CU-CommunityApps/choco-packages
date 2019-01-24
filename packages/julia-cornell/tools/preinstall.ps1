9f77d70c66ead0953ddf901160322c2fe638b523
Dan Klinger <dpk25@cornell.edu>
2019-01-24 16:56:24 -0500

    Working dir for Julia

--- packages/julia-cornell/tools/preinstall.ps1
+++ packages/julia-cornell/tools/preinstall.ps1
@@ -1,2 +1,9 @@
 # Runs before the choco package is installed
 Start-Process "$env:ALLUSERSPROFILE\chocolatey\choco.exe" -ArgumentList "install julia --confirm --install-arguments=/D=C:\ProgramData\Julia" -NoNewWindow
+
+# Working dir for Julia
+MD C:\Work
+$Acl = Get-Acl "C:\Work"
+$Ar = New-Object System.Security.AccessControl.FileSystemAccessRule("Everyone", "FullControl", "ContainerInherit,ObjectInherit", "None", "Allow")
+$Acl.SetAccessRule($Ar)
+Set-Acl "C:\Work" $Acl
