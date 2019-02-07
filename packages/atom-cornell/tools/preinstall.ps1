31ad572b5b2ebfea92d5d054082b97085ea25760
lb30 <39345501+lb30@users.noreply.github.com>
2019-02-07 13:00:16 -0500

    added install location

--- packages/atom-cornell/tools/preinstall.ps1
+++ packages/atom-cornell/tools/preinstall.ps1
@@ -1,2 +1,3 @@
 # Runs before the choco package is installed
-
+# Install Julia.exe in specified location
+Start-Process "$env:ALLUSERSPROFILE\chocolatey\choco.exe" -ArgumentList "install atom --confirm --install-arguments=/D=C:\ProgramData\atom" -NoNewWindow
