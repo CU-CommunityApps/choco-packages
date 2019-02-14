78a143dcb650b3747640f9a4295b872e30866188
Dan Klinger <dpk25@cornell.edu>
2019-02-14 11:44:36 -0500

    SAS installation param adjustments

--- packages/sas-ciser/tools/postinstall.ps1
+++ packages/sas-ciser/tools/postinstall.ps1
@@ -1,2 +1,4 @@
 # Runs after the choco package is installed
-
+# Remove temporary installation files
+rm "$env:windir\Temp\sdwresponse.properties" -Force
+rm "$env:windir\Temp\SAS94_9BYDPR_70084772_Win_X64_Srv.txt" -Force
