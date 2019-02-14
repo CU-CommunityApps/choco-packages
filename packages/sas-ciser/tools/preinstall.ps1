511e67d37f91896ad26f1fa318f5fdb4e1e7b77e
Dan Klinger <dpk25@cornell.edu>
2019-02-14 11:59:33 -0500

    Copy files before install

--- packages/sas-ciser/tools/preinstall.ps1
+++ packages/sas-ciser/tools/preinstall.ps1
@@ -1,2 +1,4 @@
 # Runs before the choco package is installed
-
+# Copy pre installation files
+cp $env:TOOLS_DIR\sdwresponse.properties $env:windir\temp\sdwresponse.properties -force
+cp $env:INSTALL_DIR\sid_files\SAS94_9BYDPR_70084772_Win_X64_Srv.txt $env:windir\temp\SAS94_9BYDPR_70084772_Win_X64_Srv.txt -force
