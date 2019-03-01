20e70ab63194357e417198dc49867cd2ca59c8c1
lb30 <39345501+lb30@users.noreply.github.com>
2019-03-01 12:44:34 -0500

    reverted back

--- packages/arcgispro-cals/tools/postinstall.ps1
+++ packages/arcgispro-cals/tools/postinstall.ps1
@@ -1,2 +1,2 @@
 # Runs after the choco package is installed
-Start-Process msiexec.exe -ArgumentList "/p $env:INSTALL_DIR\ArcGISd5\ArcGIS_Pro_231_167399.msp /q"
+Start-Process msiexec.exe -ArgumentList "/p $env:INSTALL_DIR\ArcGISd5\ArcGIS_Pro_224_165832.msp /q"
