18731f8d8385998ed6be558a6df1b6020f4b8680
Dan Klinger <dpk25@cornell.edu>
2019-06-06 11:03:42 -0400

    New Composer Version Update

--- packages/solidworks-coecis/tools/solidworks-accept-eula.ps1
+++ packages/solidworks-coecis/tools/solidworks-accept-eula.ps1
@@ -6,11 +6,11 @@ $solid = "Registry::\HKCU\Software\SolidWorks\SolidWorks 2019\Security"
 If (!(Test-Path $solid)){New-Item -Path $solid -force}
 New-ItemProperty -Path $solid -Name "EULA Accepted 2019 SP3.0 $env:ComputerName $env:UserName" -Value "Yes" -PropertyType "string" -force
 
-$composer = "Registry::\HKCU\Software\Dassault Systemes\Composer\7.5\Preferences"
+$composer = "Registry::\HKCU\Software\Dassault Systemes\Composer\7.6\Preferences"
 If (!(Test-Path $composer)){New-Item -Path $composer -force}
 New-ItemProperty -Path $composer -Name "Composer.EulaAccepted" -Value "0" -PropertyType "DWORD" -Force
 New-ItemProperty -Path $composer -Name "EulaAccepted" -Value "0" -PropertyType "DWORD" -Force
-New-ItemProperty -Path $composer -Name "EulaAccepted 2019 7.5.5.1346 $env:ComputerName $env:Username" -Value "1" -PropertyType "DWORD" -Force
+New-ItemProperty -Path $composer -Name "EulaAccepted 2019 7.6.5.1481 $env:ComputerName $env:Username" -Value "1" -PropertyType "DWORD" -Force
 New-ItemProperty -Path $composer -Name "RefreshProgressUI" -Value "1" -PropertyType "DWORD" -Force
 New-ItemProperty -Path $composer -Name "Sync.EulaAccepted" -Value "0" -PropertyType "DWORD" -Force
 
