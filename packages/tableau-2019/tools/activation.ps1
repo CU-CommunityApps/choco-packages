0557f4f9e5c9fac2e5b0138cd94ebaca0e3b8791
lb30 <39345501+lb30@users.noreply.github.com>
2019-06-13 14:02:08 -0400

    Update activation.ps1

--- packages/tableau-2019/tools/activation.ps1
+++ packages/tableau-2019/tools/activation.ps1
@@ -1,4 +1,4 @@
-If ((gwmi win32_computersystem).username -match "CORNELL"){
+If ($env:AppStream_UserName -match "cornell.edu"){
     
     write-host "Activating Tableau please wait..." -ForegroundColor Yellow
     Start-Process "$env:ProgramFiles\Tableau\Tableau 2019.2\bin\tableau.exe" -ArgumentList "-activate $env:STATIC_SYSTEM_TABLEAU_ACTIVATE_KEY" -Wait
