78af66797869e7caf0a5df465a146c2531306c3d
lb30 <39345501+lb30@users.noreply.github.com>
2019-06-13 13:49:09 -0400

    Update activation.ps1

--- packages/tableau-2019/tools/activation.ps1
+++ packages/tableau-2019/tools/activation.ps1
@@ -1,14 +1,10 @@
-Function Launch{
-
-    If ((gwmi win32_computersystem).username -match "CORNELL"){
+If ((gwmi win32_computersystem).username -match "CORNELL"){
     
-        write-host "Activating Tableau please wait..." -ForegroundColor Yellow
-        Start-Process "$env:ProgramFiles\Tableau\Tableau 2019.2\bin\tableau.exe" -ArgumentList "-activate $env:STATIC_SYSTEM_TABLEAU_ACTIVATE_KEY" -Wait
-        Start-Process "$env:ProgramFiles\Tableau\Tableau 2019.2\bin\tableau.exe" -ArgumentList "-register" -Wait
-        write-host "Launching Tableau" -ForegroundColor Green
-        Start-Process -FilePath "$env:ProgramFiles\Tableau\Tableau 2019.2\bin\tableau.exe"
-
-    }
-    Else {Start-Process -FilePath "$env:ProgramFiles\Tableau\Tableau 2019.2\bin\tableau.exe"}
+    write-host "Activating Tableau please wait..." -ForegroundColor Yellow
+    Start-Process "$env:ProgramFiles\Tableau\Tableau 2019.2\bin\tableau.exe" -ArgumentList "-activate $env:STATIC_SYSTEM_TABLEAU_ACTIVATE_KEY" -Wait
+    Start-Process "$env:ProgramFiles\Tableau\Tableau 2019.2\bin\tableau.exe" -ArgumentList "-register" -Wait
+    write-host "Launching Tableau" -ForegroundColor Green
+    Start-Process -FilePath "$env:ProgramFiles\Tableau\Tableau 2019.2\bin\tableau.exe"
 
 }
+Else {Start-Process -FilePath "$env:ProgramFiles\Tableau\Tableau 2019.2\bin\tableau.exe"}
