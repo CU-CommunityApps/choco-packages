fcde492bd04e7fc7dbc133a00b68e3ade088006b
lb30 <39345501+lb30@users.noreply.github.com>
2019-06-14 09:15:30 -0400

    Update activation.ps1

--- packages/tableau-2019/tools/activation.ps1
+++ packages/tableau-2019/tools/activation.ps1
@@ -1,4 +1,3 @@
-    
 If (((gwmi win32_computersystem).username -match "CORNELL") -or ((gwmi win32_computersystem).username -match "PhotonUser")){
     write-output "Activating..."
     Start-Process "$env:ProgramFiles\Tableau\Tableau 2019.2\bin\tableau.exe" -ArgumentList "-activate $env:STATIC_SYSTEM_TABLEAU_ACTIVATE_KEY"
