01e8d80586647cea11ecb963083838318064ecdd
lb30 <39345501+lb30@users.noreply.github.com>
2019-06-14 09:14:17 -0400

    Update activation.ps1

--- packages/tableau-2019/tools/activation.ps1
+++ packages/tableau-2019/tools/activation.ps1
@@ -1,10 +1,7 @@
-If ($env:AppStream_UserName -match "cornell.edu"){
     
-    write-host "Activating Tableau please wait..." -ForegroundColor Yellow
-    Start-Process "$env:ProgramFiles\Tableau\Tableau 2019.2\bin\tableau.exe" -ArgumentList "-activate $env:STATIC_SYSTEM_TABLEAU_ACTIVATE_KEY" -Wait
-    Start-Process "$env:ProgramFiles\Tableau\Tableau 2019.2\bin\tableau.exe" -ArgumentList "-register" -Wait
-    write-host "Launching Tableau" -ForegroundColor Green
-    Start-Process -FilePath "$env:ProgramFiles\Tableau\Tableau 2019.2\bin\tableau.exe"
-
+If (((gwmi win32_computersystem).username -match "CORNELL") -or ((gwmi win32_computersystem).username -match "PhotonUser")){
+    write-output "Activating..."
+    Start-Process "$env:ProgramFiles\Tableau\Tableau 2019.2\bin\tableau.exe" -ArgumentList "-activate $env:STATIC_SYSTEM_TABLEAU_ACTIVATE_KEY"
+    write-output "Registering..."
+    Start-Process "$env:ProgramFiles\Tableau\Tableau 2019.2\bin\tableau.exe" -ArgumentList "-register"
 }
-Else {Start-Process -FilePath "$env:ProgramFiles\Tableau\Tableau 2019.2\bin\tableau.exe"}
