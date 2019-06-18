f51ecb5ae1a3f64e8e2e46e815f6720dd44d0f4c
Dan Klinger <dpk25@cornell.edu>
2019-06-18 16:15:10 -0400

    Removing -register step

--- packages/tableau-2019/tools/activation.ps1
+++ packages/tableau-2019/tools/activation.ps1
@@ -1,6 +1,4 @@
 If (((gwmi win32_computersystem).username -match "CORNELL") -or ((gwmi win32_computersystem).username -match "PhotonUser")){
     write-output "Activating..."
     Start-Process "$env:ProgramFiles\Tableau\Tableau 2019.2\bin\tableau.exe" -ArgumentList "-activate $env:STATIC_SYSTEM_TABLEAU_ACTIVATE_KEY"
-    write-output "Registering..."
-    Start-Process "$env:ProgramFiles\Tableau\Tableau 2019.2\bin\tableau.exe" -ArgumentList "-register"
 }
