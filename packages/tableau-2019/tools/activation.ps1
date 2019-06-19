2e5e6694dcbedc0c3184b6f11187c0a5075bd2e9
Dan Klinger <dpk25@cornell.edu>
2019-06-19 12:31:17 -0400

    Update activation.ps1

--- packages/tableau-2019/tools/activation.ps1
+++ packages/tableau-2019/tools/activation.ps1
@@ -1,5 +1,3 @@
 If (((gwmi win32_computersystem).username -match "CORNELL") -or ((gwmi win32_computersystem).username -match "PhotonUser")){
-    Start-Sleep -s 5
-    write-output "Activating..."
-    Start-Process "$env:ProgramFiles\Tableau\Tableau 2019.2\bin\tableau.exe" -ArgumentList "-activate $env:STATIC_SYSTEM_TABLEAU_ACTIVATE_KEY" -Wait
+    Start-Process "$env:ProgramFiles\Tableau\Tableau 2019.2\bin\tableau.exe" -ArgumentList "-activate $env:STATIC_SYSTEM_TABLEAU_ACTIVATE_KEY"
 }
