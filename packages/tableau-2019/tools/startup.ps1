744ac34d623bb6a9bbb9121203b97db89f309055
Dan Klinger <dpk25@cornell.edu>
2019-06-24 09:59:06 -0400

    Update and rename activation.ps1 to startup.ps1

---
+++ packages/tableau-2019/tools/startup.ps1
@@ -0,0 +1,2 @@
+Start-Process "$env:ProgramFiles\Tableau\Tableau 2019.2\bin\atrdiag.exe" -ArgumentList "-enableATRFeature"
+Start-Process "$env:ProgramFiles\Tableau\Tableau 2019.2\bin\atrdiag.exe" -ArgumentList "-setDuration 57600"
