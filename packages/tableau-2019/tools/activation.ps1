1d85e82ed1620d8dd869f7a3c3eaf31ca590fd0b
Dan Klinger <dpk25@cornell.edu>
2019-06-24 14:14:45 -0400

    Update activation.ps1

--- packages/tableau-2019/tools/activation.ps1
+++ packages/tableau-2019/tools/activation.ps1
@@ -1,1 +1,19 @@
-Start-Process "$env:ProgramFiles\Tableau\Tableau 2019.2\bin\tableau.exe" -ArgumentList "-activate $env:STATIC_SYSTEM_TABLEAU_ACTIVATE_KEY"
+# .Net methods for hiding/showing the console in the background
+Add-Type -Name Window -Namespace Console -MemberDefinition '
+[DllImport("Kernel32.dll")]
+public static extern IntPtr GetConsoleWindow();
+
+[DllImport("user32.dll")]
+public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
+'
+function Hide-Console
+{
+    $consolePtr = [Console.Window]::GetConsoleWindow()
+    #0 hide
+    [Console.Window]::ShowWindow($consolePtr, 0)
+}
+Hide-Console
+
+# Activate product only if the trust store is broken
+if (& 'C:\Program Files\Tableau\Tableau 2019.2\bin\custactutil.exe' -view | Select-String -Pattern 'FULLY TRUSTED'){Start-Process "$env:ProgramFiles\Tableau\Tableau 2019.2\bin\tableau.exe"}
+else {Start-Process "$env:ProgramFiles\Tableau\Tableau 2019.2\bin\tableau.exe" -ArgumentList "-activate $env:STATIC_SYSTEM_TABLEAU_ACTIVATE_KEY";Start-Process "$env:ProgramFiles\Tableau\Tableau 2019.2\bin\tableau.exe"}
