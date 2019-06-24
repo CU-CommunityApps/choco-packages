d5a960a35f71be759c6c67c38c60a28c79676ad7
Dan Klinger <dpk25@cornell.edu>
2019-06-24 09:59:06 -0400

    Update and rename activation.ps1 to startup.ps1

--- packages/tableau-2019/tools/activation.ps1
+++
@@ -1,5 +0,0 @@
-If (((gwmi win32_computersystem).username -match "CORNELL") -or ((gwmi win32_computersystem).username -match "PhotonUser")){
-    Start-Process "$env:ProgramFiles\Tableau\Tableau 2019.2\bin\atrdiag.exe" -ArgumentList "-enableATRFeature"
-    Start-Process "$env:ProgramFiles\Tableau\Tableau 2019.2\bin\atrdiag.exe" -ArgumentList "-setDuration 57600"
-    Start-Process "$env:ProgramFiles\Tableau\Tableau 2019.2\bin\tableau.exe" -ArgumentList "-activate $env:STATIC_SYSTEM_TABLEAU_ACTIVATE_KEY"
-}
