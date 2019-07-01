358ec0ea54014ecf4bbbc29a5b5444b58c17f2d9
Dan Klinger <dpk25@cornell.edu>
2019-07-01 15:23:48 -0400

    Update activation.ps1

--- packages/tableau-2019/tools/activation.ps1
+++ packages/tableau-2019/tools/activation.ps1
@@ -1,2 +1,3 @@
 # Activate product
-Start-Process "$env:ProgramFiles\Tableau\Tableau 2019.2\bin\tableau.exe" -ArgumentList "-activate $env:STATIC_SYSTEM_TABLEAU_ACTIVATE_KEY";Start-Sleep -s 1;Start-Process "$env:ProgramFiles\Tableau\Tableau 2019.2\bin\tableau.exe"
+Write-Output "Activating..."
+Start-Process "$env:ProgramFiles\Tableau\Tableau 2019.2\bin\tableau.exe" -ArgumentList "-activate $env:STATIC_SYSTEM_TABLEAU_ACTIVATE_KEY" -Wait
