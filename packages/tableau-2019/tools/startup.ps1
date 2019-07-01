516cb2e463e028d5569c727a4ad02415c530d314
Dan Klinger <dpk25@cornell.edu>
2019-07-01 15:25:01 -0400

    Update startup.ps1

--- packages/tableau-2019/tools/startup.ps1
+++ packages/tableau-2019/tools/startup.ps1
@@ -1,2 +1,3 @@
+Write-Output "Setting ATR settings..."
 Start-Process "$env:ProgramFiles\Tableau\Tableau 2019.2\bin\atrdiag.exe" -ArgumentList "-enableATRFeature"
 Start-Process "$env:ProgramFiles\Tableau\Tableau 2019.2\bin\atrdiag.exe" -ArgumentList "-setDuration 57600"
