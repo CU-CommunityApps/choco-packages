508b001351a4dad821338c233ef2ba2595048785
Dan Klinger <dpk25@cornell.edu>
2019-07-24 16:20:50 -0400

    Unzip Julia packages with 7zip, external to chocolatey
    This is to prevent the long file path issue with the Nuget.core codebase error as this is not OS related, but with nuget and it's own limitations.

--- packages/julia-cornell/tools/postinstall.ps1
+++ packages/julia-cornell/tools/postinstall.ps1
@@ -1,1 +1,28 @@
 # Runs after the choco package is installed
+$resourceArn = (invoke-restmethod http://169.254.169.254/latest/user-data).resourceArn
+$branch = $resourceArn.split(":")[5].split("/")[1].split(".")[1]
+$account = $resourceArn.split(":")[4]
+$region = $resourceArn.split(":")[3]
+$extractFile = "julia-cornell-postfiles.zip"
+$downloadOutput = "$env:ALLUSERSPROFILE\TEMP\packages\$extractFile"
+$extractOutput = "$env:SystemDrive\Work"
+
+# Download post install zip file
+While (!(test-path $downloadOutput)){
+    try{
+        $URI = "https://image-build-$account-$region.s3.amazonaws.com/installers/$branch/$extractFile"
+        Invoke-WebRequest -Uri $uri -OutFile $downloadOutput
+    }
+    catch{Write-Host "Error downloading, try again..."}
+}
+
+# Extract post install zip file
+If (test-path $extractOutput){
+    Invoke-Expression "7z x `"$downloadOutput`" -y -o`"$extractOutput`""
+    Write-Output "Files extracted to $extractOutput"
+}
+Else {Write-Output "$extractOutput does not exist"}
+
+# Post extraction file copy
+If (!(test-path "$env:SystemDrive\Users\Default\.julia\config\startup.jl")){mkdir "$env:SystemDrive\Users\Default\.julia\config" -Force}
+cp "$extractOutput\startup.jl" "$env:SystemDrive\Users\Default\.julia\config\startup.jl" -Force
