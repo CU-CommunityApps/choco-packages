a2a39bee90fc897547912a5eafded700292d0d83
kb599 <34450561+kb599@users.noreply.github.com>
2019-09-19 16:53:45 -0400

    added windows defender settings on like 16

--- packages/base-cornellstream/tools/postinstall.ps1
+++ packages/base-cornellstream/tools/postinstall.ps1
@@ -12,3 +12,11 @@ gp | where {$_.DisplayName -match $app}){
     Start-Process $install.UninstallString -ArgumentList "/S" -WorkingDirectory $install.InstallLocation -WindowStyle Hidden
 
 }
+
+#Setup Windows Defender Preferences for Server 2016 or 2019 (not 2012)
+$OSVersion = (get-itemproperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name ProductName).ProductName
+
+If($OSVersion -notmatch "2012")
+{
+Set-MpPreference -UILockdown:$False -ExclusionPath "%USERPROFILE%\My Files" -SignatureFallbackOrder {MMPC | MicrosoftUpdateServer} -SignatureScheduleDay Everyday -SignatureScheduletime 480 -SignatureFirstAuGracePeriod 15 -SignatureAuGracePeriod 20 -SubmitSamplesConsent Never -CheckForSignaturesBeforeRunningScan $True -DisableRealTimeMonitoring $False -ExclusionProcess "StorageConnector.exe", "dcvserver.exe", "dcvagent.exe", "PhotonAgentWebServer.exe", "PhotonAgent.exe", "PhotonWindowsAppSwitcher.exe", "PhotonWindowsCustomerShell*.exe", "amazon-cloudwatch-agent.exe", "*amazon*.*", "*Photon*.*" -DisableRestorePoint $True -DisableScanningMappedNetworkDrivesForFullScan $True -DisableScanningNetworkFiles $True  -ThrottleLimit 10 -DisableBehaviorMonitoring $True -DisableCatchupFullScan $True -MAPSReporting Disabled -RealtimeScanDirection Incoming -RemediationScheduleDay Never -ScanAvgCPULoadFactor 10 -ScanOnlyIfIdleEnabled $True -ScanParameters QuickScan -ScanScheduleQuickScanTime 960 -SevereThreatDefaultAction Remove 
+}
