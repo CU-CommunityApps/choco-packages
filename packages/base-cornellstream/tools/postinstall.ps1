c5d37861de7cc507ccc67d507aa024576b2c16f7
Dan Klinger <dpk25@cornell.edu>
2019-09-27 13:11:13 -0400

    SCEP and WinDefend updates

--- packages/base-cornellstream/tools/postinstall.ps1
+++ packages/base-cornellstream/tools/postinstall.ps1
@@ -13,10 +13,23 @@ gp | where {$_.DisplayName -match $app}){
 
 }
 
-#Setup Windows Defender Preferences for Server 2016 or 2019 (not 2012)
 $OSVersion = (get-itemproperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name ProductName).ProductName
 
-If($OSVersion -notmatch "2012")
+# Install and configure SCEP on Server 2012
+If($OSVersion -match "2012")
 {
-Set-MpPreference -UILockdown:$False -ExclusionPath "%USERPROFILE%\My Files" -SignatureFallbackOrder {MMPC | MicrosoftUpdateServer} -SignatureScheduleDay Everyday -SignatureScheduletime 480 -SignatureFirstAuGracePeriod 15 -SignatureAuGracePeriod 20 -SubmitSamplesConsent Never -CheckForSignaturesBeforeRunningScan $True -DisableRealTimeMonitoring $False -ExclusionProcess "StorageConnector.exe", "dcvserver.exe", "dcvagent.exe", "PhotonAgentWebServer.exe", "PhotonAgent.exe", "PhotonWindowsAppSwitcher.exe", "PhotonWindowsCustomerShell*.exe", "amazon-cloudwatch-agent.exe", "*amazon*.*", "*Photon*.*" -DisableRestorePoint $True -DisableScanningMappedNetworkDrivesForFullScan $True -DisableScanningNetworkFiles $True  -ThrottleLimit 10 -DisableBehaviorMonitoring $True -DisableCatchupFullScan $True -MAPSReporting Disabled -RealtimeScanDirection Incoming -RemediationScheduleDay Never -ScanAvgCPULoadFactor 10 -ScanOnlyIfIdleEnabled $True -ScanParameters QuickScan -ScanScheduleQuickScanTime 960 -SevereThreatDefaultAction Remove 
+    Start-Process "$env:INSTALL_DIR\SCEPInstall.exe" -ArgumentList "/s /q /policy $env:INSTALL_DIR\SCEPpolicy.xml"
+}
+
+#Setup Windows Defender Preferences for Server 2016 or 2019 (not 2012)
+If($OSVersion -notmatch "2012")
+{   
+    # Enable WinDefend
+    Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name DisableAntiSpyware -force
+    
+    # Start WindDefend
+    Start-Service -Name WinDefend
+    
+    # Apply custom policies
+    Set-MpPreference -UILockdown:$False -ExclusionPath "%USERPROFILE%\My Files" -SignatureFallbackOrder {MMPC | MicrosoftUpdateServer} -SignatureScheduleDay Everyday -SignatureScheduletime 480 -SignatureFirstAuGracePeriod 15 -SignatureAuGracePeriod 20 -SubmitSamplesConsent Never -CheckForSignaturesBeforeRunningScan $True -DisableRealTimeMonitoring $False -ExclusionProcess "StorageConnector.exe", "dcvserver.exe", "dcvagent.exe", "PhotonAgentWebServer.exe", "PhotonAgent.exe", "PhotonWindowsAppSwitcher.exe", "PhotonWindowsCustomerShell*.exe", "amazon-cloudwatch-agent.exe", "*amazon*.*", "*Photon*.*" -DisableRestorePoint $True -DisableScanningMappedNetworkDrivesForFullScan $True -DisableScanningNetworkFiles $True  -ThrottleLimit 10 -DisableBehaviorMonitoring $True -DisableCatchupFullScan $True -MAPSReporting Disabled -RealtimeScanDirection Incoming -RemediationScheduleDay Never -ScanAvgCPULoadFactor 10 -ScanOnlyIfIdleEnabled $True -ScanParameters QuickScan -ScanScheduleQuickScanTime 960 -SevereThreatDefaultAction Remove 
 }
