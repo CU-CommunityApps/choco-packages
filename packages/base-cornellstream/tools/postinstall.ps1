8f0524c60b6d6af47bb97ac662257fea65ebcecd
Dan Klinger <dpk25@cornell.edu>
2019-10-01 17:43:22 -0400

    Update postinstall.ps1

--- packages/base-cornellstream/tools/postinstall.ps1
+++ packages/base-cornellstream/tools/postinstall.ps1
@@ -20,19 +20,12 @@ $OSVersion = (get-itemproperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\Curren
 # Install and configure SCEP on Server 2012
 If($OSVersion -match "2012")
 {
-    Start-Process "$INSTALL_DIR\SCEPInstall.exe" -ArgumentList "/s /q /policy $INSTALL_DIR\SCEPpolicy.xml"
+    Start-Process "$INSTALL_DIR\SCEPInstall.exe" -ArgumentList "/s /q /policy $INSTALL_DIR\SCEPpolicy.xml" -Wait
+    Register-ScheduledTask -TaskName "Update_SCEP" -Xml (Get-Content "$PSScriptRoot\Update_SCEP.xml" | Out-String)
 }
 
 #Setup Windows Defender Preferences for Server 2016 or 2019 (not 2012)
 If($OSVersion -notmatch "2012")
-{   
-    # Enable WinDefend
-    Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name DisableAntiSpyware -force
-    
-    # Start WindDefend
-    Start-Service -Name WinDefend
-    
-    # Apply custom policies
-    ## https://docs.microsoft.com/en-us/powershell/module/defender/Set-MpPreference?view=win10-ps ##
-    Set-MpPreference -UILockdown:$False -ExclusionPath "$env:USERPROFILE\My Files" -SignatureFallbackOrder {MMPC | MicrosoftUpdateServer} -SignatureUpdateInterval 1 -SignatureFirstAuGracePeriod 5 -SignatureAuGracePeriod 5 -SubmitSamplesConsent Never -CheckForSignaturesBeforeRunningScan $True -DisableRealTimeMonitoring $False -ExclusionProcess "StorageConnector.exe", "dcvserver.exe", "dcvagent.exe", "PhotonAgentWebServer.exe", "PhotonAgent.exe", "PhotonWindowsAppSwitcher.exe", "PhotonWindowsCustomerShell*.exe", "amazon-cloudwatch-agent.exe", "*amazon*.*", "*Photon*.*" -DisableRestorePoint $True -DisableScanningMappedNetworkDrivesForFullScan $True -DisableScanningNetworkFiles $True  -ThrottleLimit 10 -DisableBehaviorMonitoring $True -DisableCatchupFullScan $True -MAPSReporting Disabled -RealtimeScanDirection Incoming -RemediationScheduleDay Never -ScanAvgCPULoadFactor 10 -ScanOnlyIfIdleEnabled $True -ScanParameters QuickScan -ScanScheduleQuickScanTime 960 -SevereThreatDefaultAction Remove 
+{
+    Register-ScheduledTask -TaskName "Update_WinDefend" -Xml (Get-Content "$PSScriptRoot\Update_WinDefend.xml" | Out-String)
 }
