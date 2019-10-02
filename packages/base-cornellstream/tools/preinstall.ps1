7c564ad9ed1aba9858a88d4acc6e43f3c7819a93
Dan Klinger <dpk25@cornell.edu>
2019-10-02 18:04:09 -0400

    Update preinstall.ps1

--- packages/base-cornellstream/tools/preinstall.ps1
+++ packages/base-cornellstream/tools/preinstall.ps1
@@ -1,4 +1,12 @@
 # Runs before the choco package is installed
 
-# Set Time Zone Properly
-Set-TimeZone -Name "Eastern Standard Time"
+# Enable Windows Defender
+If($OSVersion -notmatch "2012")
+{
+    # Enable WinDefend
+    Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name DisableAntiSpyware -force
+
+    # Start WindDefend
+    Start-Service -Name WinDefend
+    
+}
