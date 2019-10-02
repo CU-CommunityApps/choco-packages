1a5cf55f1e480e667a6f7a87b27455165800cc54
Dan Klinger <dpk25@cornell.edu>
2019-10-01 21:44:03 -0400

    Update postinstall.ps1

--- packages/base-cornellstream/tools/postinstall.ps1
+++ packages/base-cornellstream/tools/postinstall.ps1
@@ -27,5 +27,15 @@ If($OSVersion -match "2012")
 #Setup Windows Defender Preferences for Server 2016 or 2019 (not 2012)
 If($OSVersion -notmatch "2012")
 {
+    # Enable WinDefend
+    Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name DisableAntiSpyware -force
+
+    # Start WindDefend
+    Start-Service -Name WinDefend
+
+    # Apply custom policies
+    ## https://docs.microsoft.com/en-us/powershell/module/defender/Set-MpPreference?view=win10-ps ##
+    Set-MpPreference -UILockdown:$False -ExclusionPath "$env:USERPROFILE\My Files" -SignatureFallbackOrder {MMPC | MicrosoftUpdateServer} -SignatureUpdateInterval 1 -SignatureFirstAuGracePeriod 5 -SignatureAuGracePeriod 5 -SubmitSamplesConsent Never -CheckForSignaturesBeforeRunningScan $True -DisableRealTimeMonitoring $False -ExclusionProcess "StorageConnector.exe", "dcvserver.exe", "dcvagent.exe", "PhotonAgentWebServer.exe", "PhotonAgent.exe", "PhotonWindowsAppSwitcher.exe", "PhotonWindowsCustomerShell*.exe", "amazon-cloudwatch-agent.exe", "*amazon*.*", "*Photon*.*" -DisableRestorePoint $True -DisableScanningMappedNetworkDrivesForFullScan $True -DisableScanningNetworkFiles $True  -ThrottleLimit 10 -DisableBehaviorMonitoring $True -DisableCatchupFullScan $True -MAPSReporting Disabled -RealtimeScanDirection Incoming -RemediationScheduleDay Never -ScanAvgCPULoadFactor 10 -ScanOnlyIfIdleEnabled $True -ScanParameters QuickScan -ScanScheduleQuickScanTime 960 -SevereThreatDefaultAction Remove 
+
     Register-ScheduledTask -TaskName "Update_WinDefend" -Xml (Get-Content "$PSScriptRoot\Update_WinDefend.xml" | Out-String)
 }
