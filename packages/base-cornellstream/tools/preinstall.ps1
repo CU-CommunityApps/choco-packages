401a55a75eee50f14d7e4d16c6349dad43c8f271
Dan Klinger <dpk25@cornell.edu>
2019-10-09 09:04:52 -0400

    Added missing OS check

--- packages/base-cornellstream/tools/preinstall.ps1
+++ packages/base-cornellstream/tools/preinstall.ps1
@@ -1,5 +1,8 @@
 # Runs before the choco package is installed
 
+# Check OS Version
+$OSVersion = (get-itemproperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name ProductName).ProductName
+
 # Enable Windows Defender
 If($OSVersion -notmatch "2012")
 {
