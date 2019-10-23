1531627b429e5aa90d310144e0ef0404876f5586
Dan Klinger <dpk25@cornell.edu>
2019-10-23 15:21:24 -0400

    A bit smarter

--- packages/troubleshooting.ps1
+++ packages/troubleshooting.ps1
@@ -3,32 +3,34 @@
     Chocolatey .nupkg troubleshooting script
 .DESCRIPTION
     Test custom .nupkg installers on Windows OS' prior to building an AppStream image
-.PARAMETER S3
+.PARAMETER s3
     S3 bucket name for installer files. Bucket name only, do not include entire path. i.e. if S3 bucket path is https://s3.amazonaws.com/mybucket input 'mybucket'
-.PARAMETER App
+.PARAMETER package
     Application name for testing (ie. adobedcreader-cornell)
-.PARAMETER Branch
+.PARAMETER branch
     Test branch name (ie. adobedcreader)
 .EXAMPLE
     PS C:\Users\test\Desktop> 
-    .\troubleshooting.ps1 -S3 somebucketname -App adobedcreader-cornell -Branch adobedcreader
+    .\troubleshooting.ps1 -S3 somebucketname -App_Version adobedcreader-cornell.2018.011.20055 -Branch adobedcreader
 .LINK
     https://github.com/CU-CommunityApps/choco-packages
     https://confluence.cornell.edu/display/CLOUD/Cornell+Stream
+    https://developer.github.com/v3/
 .NOTES
     Testing should occur on same OS as target OS.
+    irm -uri "https://api.github.com/repos/CU-CommunityApps/choco-packages" to see available github parameters
 #>
 [CmdletBinding()]
 Param(
     [Parameter(Mandatory=$true,Position=1,HelpMessage="S3 bucket name containing .nupkg files")]
     [ValidateNotNullOrEmpty()]
-    [string]$S3,
-    [Parameter(Mandatory=$true,Position=2,HelpMessage="Application name")]
+    [string]$s3,
+    [Parameter(Mandatory=$false,Position=2,HelpMessage="Application name")]
     [ValidateNotNullOrEmpty()]
-    [string]$App,
-    [Parameter(Mandatory=$true,Position=3,HelpMessage="Test branch name")]
+    [string]$package,
+    [Parameter(Mandatory=$false,Position=3,HelpMessage="Test branch name")]
     [ValidateNotNullOrEmpty()]
-    [string]$Branch
+    [string]$branch
 )
 
 # Must run in Administrator Mode
@@ -44,42 +46,102 @@ Invoke-Expression "choco.exe upgrade -y chocolatey"
 # Install 7-zip
 Invoke-Expression "choco.exe install -y 7zip"
 # Install required powershell modules
-Install-Module powershell-yaml -Force
-Install-Module pssqlite -Force
-# Import required powershell modules
-Import-Module powershell-yaml -Force
-Import-Module pssqlite -Force
+If ((Get-Module powershell-yaml, pssqlite).Count -eq 2){Write-Host "Powershell modules already installed" -ForegroundColor Green}
+Else {Install-Module powershell-yaml -Force; Install-Module pssqlite -Force; Import-Module powershell-yaml -Force; Import-Module pssqlite -Force}
 
-# Variables
-$repo = "https://raw.githubusercontent.com/CU-CommunityApps/choco-packages"
-$version = ((iwr -URI "$repo/$branch/packages/$App/config.yml").Content | ConvertFrom-Yaml).Version
-$output = "$env:USERPROFILE\Desktop\$App.$version.nupkg"
-$extract = "$env:USERPROFILE\Desktop\$App.$version"
+$repo = "CU-CommunityApps/choco-packages"
+$apiURI = "https://api.github.com/repos/$repo"
+$rawURI = "https://raw.githubusercontent.com/$repo"
 
-# If the file doesn't exist, download from S3 bucket
-If (!(test-path $output)){
-    try{
-        $URI = "https://$S3.s3.amazonaws.com/packages/$branch/$App.$version.nupkg"
-        Start-BitsTransfer -Source $uri -Destination $output
+Function branches {
+
+    # List current branches
+    Write-Host "`nCurrent branches for $repo`n" -ForegroundColor Yellow
+    $branches = (irm -uri "$apiURI/branches").name
+    $branches | select | where {$_ -NE "master"}
+    
+    Do {
+        $branch = Read-Host "`nSelect branch"
+    }Until ($branches -contains $branch)
+
+    packages $branch
+}
+
+Function packages($branch) {
+
+    # List packages
+    Write-Host "`nCurrent packages on $branch branch`n" -ForegroundColor Yellow
+    $packages = (irm -Uri "$apiURI/contents/packages?ref=$branch")
+    $packages.name | select | where {$_ -match "-"}
+
+    Do {
+        $package = Read-Host "`nSelect package"
+    }Until ($packages.name -contains $package)
+
+    $package
+
+    version $package $branch   
+
+}
+
+Function version($package, $branch){
+
+    # Get file version
+    $config = irm -Uri "$rawURI/$branch/packages/$package/config.yml"
+    $config
+    $version = ($config | ConvertFrom-Yaml).version
+
+    troubleshoot $package $branch $version
+
+}
+
+Function troubleshoot($package, $branch, $version) {
+
+    # Download and extract locations on test machine
+    $output = "$env:USERPROFILE\Desktop\$package.$version.nupkg"
+    $extract = "$env:USERPROFILE\Desktop\$package.$version"
+
+    # If the file doesn't exist, download from S3 bucket
+    If (!(test-path $output)){
+        try{
+            $URI = "https://$s3.s3.amazonaws.com/packages/$branch/$package.$version.nupkg"
+            Start-BitsTransfer -Source $URI -Destination $output -ErrorAction Stop
+        }
+        catch{Write-Host "$package.$version has not been built yet or does not exist, commit to github and wait for successful build, then try again..." -ForegroundColor Red;branches}
     }
-    catch{Write-Host "Error downloading, try again..."}
+
+    # Cache process to disk if file is greater than 2GB
+    If ((get-item $output).Length -gt 2gb){Invoke-Expression "choco.exe install -y $output --force --debug --cache-location=C:\Temp"}
+    Else{Invoke-Expression "choco.exe install -y $output --force --debug --cache-location="}
+
+    Do{$ans = Read-Host "Extract and troubleshoot $package.$version ?"}Until(($ans.ToLower() -eq "y") -or ($ans.ToLower() -eq "n"))
+
+    If ($ans.ToLower() -eq "y"){
+        Invoke-Expression "7z e $output -y -o$extract -r"
+        Write-Host "Files extracted to $extract, edit config.yml and others as needed" -ForegroundColor Yellow
+        
+        pause
+
+        Do{$ans = Read-Host "Test $package.$version ?"}Until(($ans.ToLower() -eq "y") -or ($ans.ToLower() -eq "n"))
+
+        If ($ans.ToLower() -eq "y"){
+            Invoke-Expression "$extract\chocolateyinstall.ps1 -Mode T -S3 $s3 -App $package" 
+        }
+        Else {
+            Do{$ans = Read-Host "Test another application?"}Until(($ans.ToLower() -eq "y") -or ($ans.ToLower() -eq "n"))
+
+            If ($ans.ToLower() -eq "y"){branches}
+            Else {Write-host "Happy testing!!" -ForegroundColor Green; exit 0}
+        }
+    }
+    Else {
+        Do{$ans = Read-Host "Test another application?"}Until(($ans.ToLower() -eq "y") -or ($ans.ToLower() -eq "n"))
+
+        If ($ans.ToLower() -eq "y"){branches}
+        Else {Write-host "Happy testing!!" -ForegroundColor Green; exit 0}
+    }
+
 }
 
-# Cache process to disk if file is greater than 2GB
-If ((get-item $output).Length -gt 2gb){Invoke-Expression "choco.exe install -y $output --force --debug --cache-location=C:\Temp"}
-Else{Invoke-Expression "choco.exe install -y $output --force --debug --cache-location="}
-
-Do{$ans = Read-Host "Extract and troubleshoot $App.$version ?"}Until(($ans.ToLower() -eq "y") -or ($ans.ToLower() -eq "n"))
-
-If ($ans.ToLower() -eq "y"){
-    Invoke-Expression "7z e $output -y -o$extract -r"
-    Write-Host "Files extracted to $extract, edit config.yml and others as needed" -ForegroundColor Yellow
-}
-Else {Write-host "Happy testing!!" -ForegroundColor Green}
-
-Do{$ans = Read-Host "Test $App.$version ?"}Until(($ans.ToLower() -eq "y") -or ($ans.ToLower() -eq "n"))
-
-If ($ans.ToLower() -eq "y"){
-    Invoke-Expression "$extract\chocolateyinstall.ps1 -Mode T -S3 $S3 -App $App" 
-}
-Else {Write-host "Happy testing!!" -ForegroundColor Green}
+If ($package -and $branch){version $package $branch}
+Else {branches}
