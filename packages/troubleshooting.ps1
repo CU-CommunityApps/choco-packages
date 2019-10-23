7a23cfa7213806e9ba7fa26f3c5963f0945086f0
Dan Klinger <dpk25@cornell.edu>
2019-10-23 12:51:43 -0400

    Get version number from git repo

--- packages/troubleshooting.ps1
+++ packages/troubleshooting.ps1
@@ -5,13 +5,13 @@
     Test custom .nupkg installers on Windows OS' prior to building an AppStream image
 .PARAMETER S3
     S3 bucket name for installer files. Bucket name only, do not include entire path. i.e. if S3 bucket path is https://s3.amazonaws.com/mybucket input 'mybucket'
-.PARAMETER App_Version
-    Application name and version number for testing (ie. adobedcreader-cornell.2018.011.20055)
+.PARAMETER App
+    Application name for testing (ie. adobedcreader-cornell)
 .PARAMETER Branch
     Test branch name (ie. adobedcreader)
 .EXAMPLE
     PS C:\Users\test\Desktop> 
-    .\troubleshooting.ps1 -S3 somebucketname -App_Version adobedcreader-cornell.2018.011.20055 -Branch adobedcreader
+    .\troubleshooting.ps1 -S3 somebucketname -App adobedcreader-cornell -Branch adobedcreader
 .LINK
     https://github.com/CU-CommunityApps/choco-packages
     https://confluence.cornell.edu/display/CLOUD/Cornell+Stream
@@ -23,9 +23,9 @@ Param(
     [Parameter(Mandatory=$true,Position=1,HelpMessage="S3 bucket name containing .nupkg files")]
     [ValidateNotNullOrEmpty()]
     [string]$S3,
-    [Parameter(Mandatory=$true,Position=2,HelpMessage="Application name and version number")]
+    [Parameter(Mandatory=$true,Position=2,HelpMessage="Application name")]
     [ValidateNotNullOrEmpty()]
-    [string]$App_Version,
+    [string]$App,
     [Parameter(Mandatory=$true,Position=3,HelpMessage="Test branch name")]
     [ValidateNotNullOrEmpty()]
     [string]$Branch
@@ -51,14 +51,15 @@ Import-Module powershell-yaml -Force
 Import-Module pssqlite -Force
 
 # Variables
-$appName = $App_Version.Split(".")[0]
-$output = "$env:USERPROFILE\Desktop\$App_Version.nupkg"
-$extract = "$env:USERPROFILE\Desktop\$App_Version"
+$repo = "https://raw.githubusercontent.com/CU-CommunityApps/choco-packages"
+$version = ((iwr -URI "$repo/$branch/packages/$App/config.yml").Content | ConvertFrom-Yaml).Version
+$output = "$env:USERPROFILE\Desktop\$App.$version.nupkg"
+$extract = "$env:USERPROFILE\Desktop\$App.$version"
 
 # If the file doesn't exist, download from S3 bucket
 If (!(test-path $output)){
     try{
-        $URI = "https://$S3.s3.amazonaws.com/packages/$branch/$App_Version.nupkg"
+        $URI = "https://$S3.s3.amazonaws.com/packages/$branch/$App.$version.nupkg"
         Start-BitsTransfer -Source $uri -Destination $output
     }
     catch{Write-Host "Error downloading, try again..."}
@@ -68,7 +69,7 @@ If (!(test-path $output)){
 If ((get-item $output).Length -gt 2gb){Invoke-Expression "choco.exe install -y $output --force --debug --cache-location=C:\Temp"}
 Else{Invoke-Expression "choco.exe install -y $output --force --debug --cache-location="}
 
-Do{$ans = Read-Host "Extract and troubleshoot $App_Version ?"}Until(($ans.ToLower() -eq "y") -or ($ans.ToLower() -eq "n"))
+Do{$ans = Read-Host "Extract and troubleshoot $App.$version ?"}Until(($ans.ToLower() -eq "y") -or ($ans.ToLower() -eq "n"))
 
 If ($ans.ToLower() -eq "y"){
     Invoke-Expression "7z e $output -y -o$extract -r"
@@ -76,9 +77,9 @@ If ($ans.ToLower() -eq "y"){
 }
 Else {Write-host "Happy testing!!" -ForegroundColor Green}
 
-Do{$ans = Read-Host "Test $App_Version ?"}Until(($ans.ToLower() -eq "y") -or ($ans.ToLower() -eq "n"))
+Do{$ans = Read-Host "Test $App.$version ?"}Until(($ans.ToLower() -eq "y") -or ($ans.ToLower() -eq "n"))
 
 If ($ans.ToLower() -eq "y"){
-    Invoke-Expression "$extract\chocolateyinstall.ps1 -Mode T -S3 $S3 -App $appName" 
+    Invoke-Expression "$extract\chocolateyinstall.ps1 -Mode T -S3 $S3 -App $App" 
 }
 Else {Write-host "Happy testing!!" -ForegroundColor Green}
