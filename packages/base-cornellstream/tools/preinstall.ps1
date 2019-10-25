833a2341d386cce1f4a69fc4c0857fdafb4fe53a
Dan Klinger <dpk25@cornell.edu>
2019-10-25 15:24:36 -0400

    Reinstall WinDefend

--- packages/base-cornellstream/tools/preinstall.ps1
+++ packages/base-cornellstream/tools/preinstall.ps1
@@ -3,13 +3,15 @@
 # Check OS Version
 $OSVersion = (get-itemproperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name ProductName).ProductName
 
+# Reinstall Windows Defender on Server 2016
+If($OSVersion -match "2016"){Install-WindowsFeature -Name Windows-Defender}
+
 # Enable Windows Defender
-If($OSVersion -notmatch "2012")
+If($OSVersion -match "2019")
 {
     # Enable WinDefend
     Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name DisableAntiSpyware -force -ErrorAction SilentlyContinue
 
     # Start WindDefend
     Start-Service -Name WinDefend
-    
 }
