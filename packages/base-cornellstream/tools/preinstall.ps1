67a53d6a9776b13305630f4b9931a30a8878e02d
Dan Klinger <dpk25@cornell.edu>
2019-11-05 18:13:55 -0500

    Update preinstall.ps1

--- packages/base-cornellstream/tools/preinstall.ps1
+++ packages/base-cornellstream/tools/preinstall.ps1
@@ -4,11 +4,11 @@
 $OSVersion = (get-itemproperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name ProductName).ProductName
 
 # Enable Windows Defender
-If($OSVersion -ge "2019")
+If($OSVersion -notmatch "2012")
 {
     # Enable WinDefend
     Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name DisableAntiSpyware -force -ErrorAction SilentlyContinue
 
     # Start WindDefend
-    Start-Service -Name WinDefend
+    Start-Service -Name WinDefend -ErrorAction SilentlyContinue
 }
