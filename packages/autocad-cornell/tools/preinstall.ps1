e292108abe1b9a4f225d6b8a0e6f04df825b6cc8
Dan Klinger <dpk25@cornell.edu>
2019-11-12 20:32:03 -0500

    Update preinstall.ps1

--- packages/autocad-cornell/tools/preinstall.ps1
+++ packages/autocad-cornell/tools/preinstall.ps1
@@ -2,6 +2,6 @@
 $INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
 
 # https://forums.autodesk.com/t5/installation-licensing/installatoin-hangs-at-adsklicensing-installer-exe/m-p/8842099#M223832
-Start-Process "AdskLicensing-installer.exe" -ArgumentList "--mode unattended --unattendedmodeui none" -WorkingDirectory $INSTALL_DIR
+Start-Process "AdskLicensing-installer.exe" -WorkingDirectory $INSTALL_DIR
 
 Start-Sleep -s 120
