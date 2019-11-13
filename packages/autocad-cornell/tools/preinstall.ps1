09cd7a3cfee6ee76ba206e34625198306051506a
Dan Klinger <dpk25@cornell.edu>
2019-11-13 17:54:30 -0500

    Update preinstall.ps1

--- packages/autocad-cornell/tools/preinstall.ps1
+++ packages/autocad-cornell/tools/preinstall.ps1
@@ -1,7 +1,7 @@
 # Runs before the choco package is installed
 $INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
 
-# Install prereqs
+# Install prereq's
 Start-Process "$INSTALL_DIR\VC_redist.x86.exe" -ArgumentList "/quiet /norestart" -Wait
 Start-Process "$INSTALL_DIR\VC_redist.x64.exe" -ArgumentList "/quiet /norestart" -Wait
 
