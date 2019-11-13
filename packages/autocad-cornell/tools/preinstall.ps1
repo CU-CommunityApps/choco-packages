3667294f7574a4c6c7891094b73536032d3cbde8
Dan Klinger <dpk25@cornell.edu>
2019-11-13 17:29:31 -0500

    Update preinstall.ps1

--- packages/autocad-cornell/tools/preinstall.ps1
+++ packages/autocad-cornell/tools/preinstall.ps1
@@ -1,7 +1,9 @@
 # Runs before the choco package is installed
 $INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
 
-# https://forums.autodesk.com/t5/installation-licensing/installatoin-hangs-at-adsklicensing-installer-exe/m-p/8842099#M223832
-Start-Process "AdskLicensing-installer.exe" -WorkingDirectory $INSTALL_DIR
+# Install prereqs
+Start-Process "$INSTALL_DIR\VC_redist.x86.exe" -ArgumentList "/quiet /norestart" -Wait
+Start-Process "$INSTALL_DIR\VC_redist.x64.exe" -ArgumentList "/quiet /norestart" -Wait
 
-Start-Sleep -s 120
+# https://forums.autodesk.com/t5/installation-licensing/installatoin-hangs-at-adsklicensing-installer-exe/m-p/8842099#M223832
+Start-Process "$INSTALL_DIR\AdskLicensing-installer.exe" -ArgumentList "--mode unattended --unattendedmodeui none" -Wait
