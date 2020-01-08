439e617f4d6c56a31d8129295bdecbae958e6a7d
lb30 <39345501+lb30@users.noreply.github.com>
2020-01-08 09:24:39 -0500

    Update preinstall.ps1

--- packages/mstar-eng/tools/preinstall.ps1
+++ packages/mstar-eng/tools/preinstall.ps1
@@ -4,3 +4,4 @@ $INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
 # Install prereq's
 Start-Process "$INSTALL_DIR\VC_redist.x86.exe" -ArgumentList "/quiet /norestart" -Wait
 Start-Process "$INSTALL_DIR\VC_redist.x64.exe" -ArgumentList "/quiet /norestart" -Wait
+Start-Process "$INSTALL_DIR\msmpisetup.exe" -ArgumentList "-unattend -minimal" -Wait
