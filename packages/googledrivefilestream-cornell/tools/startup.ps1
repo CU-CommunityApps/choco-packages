3b1b4ee55e05fcd88895196187b0318979648dc8
Lori Beebe <lb30@cornell.edu>
2020-01-17 09:47:56 -0500

    chgtoschtask

--- packages/googledrivefilestream-cornell/tools/startup.ps1
+++
@@ -1,3 +0,0 @@
-Write-Output "Launching Google Drive File Stream"
-$path = (gci "${env:ProgramFiles}\Google\Drive File Stream\35.0.13.0" -Include GoogleDriveFS.exe -Recurse -ErrorAction SilentlyContinue).DirectoryName
-Start-Process "$path\GoogleDriveFS.exe"
