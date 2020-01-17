50622139cf41e2f54187dc6af87dfed574dc52e4
lb30 <39345501+lb30@users.noreply.github.com>
2020-01-17 09:04:19 -0500

    testing

--- packages/googledrivefilestream-cornell/tools/startup.ps1
+++ packages/googledrivefilestream-cornell/tools/startup.ps1
@@ -1,1 +1,3 @@
-Start-Process -FilePath "C:\Program Files\Google\Drive File Stream\35.0.13.0\GoogleDriveFS.exe"
+Write-Output "Launching Google Drive File Stream"
+$path = (gci "${env:ProgramFiles}\Google\Drive File Stream\35.0.13.0" -Include GoogleDriveFS.exe -Recurse -ErrorAction SilentlyContinue).DirectoryName
+Start-Process "$path\GoogleDriveFS.exe"
