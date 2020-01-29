7f568a4b2c2ef38f8272ecbbf2ea22e367bbeccf
kb599 <kb599@cornell.edu>
2020-01-29 10:05:32 -0500

    updatedgoogledrive

--- packages/googledrivefilestream-cornell/tools/postinstall.ps1
+++ packages/googledrivefilestream-cornell/tools/postinstall.ps1
@@ -1,1 +1,6 @@
 # Runs after the choco package is installed
+SCHTASKS /DELETE /TN "GoogleUpdateTaskMachineUA" /F
+SCHTASKS /DELETE /TN "GoogleUpdateTaskMachineCore" /F
+SCHTASKS /DELETE /TN "GoogleDriveFileStream" /F
+"C:\Program Files (x86)\Google\Update\GoogleUpdate.exe" /uninstall
+Rename-Item "C:\Program Files (x86)\Google\Update" -NewName noup
