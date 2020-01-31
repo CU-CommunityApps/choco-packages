e6e6719e093a9c865c61020805ef75632b13d1f3
kb599 <34450561+kb599@users.noreply.github.com>
2020-01-31 17:29:21 -0500

    Update postinstall.ps1
    moved uninstall

--- packages/googledrivefilestream-cornell/tools/postinstall.ps1
+++ packages/googledrivefilestream-cornell/tools/postinstall.ps1
@@ -2,5 +2,5 @@
 SCHTASKS /DELETE /TN "GoogleUpdateTaskMachineUA" /F
 SCHTASKS /DELETE /TN "GoogleUpdateTaskMachineCore" /F
 SCHTASKS /DELETE /TN "GoogleDriveFileStream" /F
-"C:\Program Files (x86)\Google\Update\GoogleUpdate.exe" /uninstall
+"C:\Program Files (x86)\Google\Update\GoogleUpdate.exe /uninstall" 
 Rename-Item "C:\Program Files (x86)\Google\Update" -NewName noup
