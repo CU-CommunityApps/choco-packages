f3cf08f9fda7a8bc8172d8a03243908ab8269b98
kb599 <34450561+kb599@users.noreply.github.com>
2020-02-12 10:38:31 -0500

    Update postinstall.ps1
    removed the stop process

--- packages/googledrivefilestream-cornell/tools/postinstall.ps1
+++ packages/googledrivefilestream-cornell/tools/postinstall.ps1
@@ -1,8 +1,6 @@
 # Runs after the choco package is installed
 SCHTASKS /DELETE /TN "GoogleUpdateTaskMachineUA" /F
 SCHTASKS /DELETE /TN "GoogleUpdateTaskMachineCore" /F
-Stop-process -processname GoogleUpdate
-Rename-Item "C:\Program Files (x86)\Google\Update" -NewName noup
 Stop-Service gupdate
 Set-Service gupdate -StartupType Disabled
 Stop-Service gupdatem
