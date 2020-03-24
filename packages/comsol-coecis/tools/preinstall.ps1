15f8dcffb07515928495a0a8c4e590643c9ff401
lb30 <39345501+lb30@users.noreply.github.com>
2020-03-24 13:39:07 -0400

    Update preinstall.ps1

--- packages/comsol-coecis/tools/preinstall.ps1
+++ packages/comsol-coecis/tools/preinstall.ps1
@@ -1,4 +1,4 @@
 # Runs before the choco package is installed
 
 # Microsoft.NET Framework 4.6 (REQUIRED)
-Start-Process "$INSTALL_DIR\ndp48-x86-x64-allos-enu.exe" -ArgumentList "/q /lang:ENU /norestart" -Wait
+Start-Process "$env:INSTALL_DIR\ndp48-x86-x64-allos-enu.exe" -ArgumentList "/q /lang:ENU /norestart" -Wait
