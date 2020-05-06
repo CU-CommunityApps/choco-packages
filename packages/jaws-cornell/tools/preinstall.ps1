b18f622e1d9052d81e55750fd5c80229610f4d05
lb30 <39345501+lb30@users.noreply.github.com>
2020-05-06 11:02:37 -0400

    added vcredist

--- packages/jaws-cornell/tools/preinstall.ps1
+++ packages/jaws-cornell/tools/preinstall.ps1
@@ -1,2 +1,9 @@
 # Runs before the choco package is installed
 
+# Install prereq's 2010
+Start-Process "$INSTALL_DIR\2010vcredist_x86.exe" -ArgumentList "/quiet /norestart" -Wait
+Start-Process "$INSTALL_DIR\2010vcredist_x64.exe" -ArgumentList "/quiet /norestart" -Wait
+
+# Install prereq's 2010
+Start-Process "$INSTALL_DIR\2013vcredist_x86.exe" -ArgumentList "/quiet /norestart" -Wait
+Start-Process "$INSTALL_DIR\2013vcredist_x64.exe" -ArgumentList "/quiet /norestart" -Wait
