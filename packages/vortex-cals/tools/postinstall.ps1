84ee2ec07ab180d38f8ae43f7a7b8f5a2fb11013
lb30 <39345501+lb30@users.noreply.github.com>
2020-06-01 13:30:02 -0400

    Update postinstall.ps1

--- packages/vortex-cals/tools/postinstall.ps1
+++ packages/vortex-cals/tools/postinstall.ps1
@@ -1,5 +1,5 @@
 # Runs after the choco package is installed
 
 #Move Project Folders to accessible location
-move-item $env:%SYSTEMDRIVE%\Vortex10Projects $env:%SYSTEMDRIVE%\Users\Default\Documents\Vortex10Projects
-move-item $env:%SYSTEMDRIVE%\VortexAMProjects $env:%SYSTEMDRIVE%\Users\Default\Documents\VortexAMProjects
+move-item $env:SYSTEMDRIVE\Vortex10Projects $env:SYSTEMDRIVE\Users\Default\Documents\Vortex10Projects
+move-item $env:SYSTEMDRIVE\VortexAMProjects $env:SYSTEMDRIVE\Users\Default\Documents\VortexAMProjects
