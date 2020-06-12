d24809713085b1b5592e06fcf8b283a98775d81a
lb30 <39345501+lb30@users.noreply.github.com>
2020-06-12 15:21:33 -0400

    Update preinstall.ps1

--- packages/inventor-coecis/tools/preinstall.ps1
+++ packages/inventor-coecis/tools/preinstall.ps1
@@ -1,6 +1,9 @@
 # Runs before the choco package is installed
 $INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
 
+# Install .NET
+Add-WindowsFeature NET-Framework-Core
+
 # Install prereq's
 Start-Process "$INSTALL_DIR\VC_redist.x86.exe" -ArgumentList "/quiet /norestart" -Wait
 Start-Process "$INSTALL_DIR\VC_redist.x64.exe" -ArgumentList "/quiet /norestart" -Wait
