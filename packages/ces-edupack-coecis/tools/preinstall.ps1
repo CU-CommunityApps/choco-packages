f77cf2a63aaeb69b2165113c9babbccca3e685ee
lb30 <39345501+lb30@users.noreply.github.com>
2020-07-09 11:08:49 -0400

    ces edupack coecis (#137)
    * version update
    
    * version update

--- packages/ces-edupack-coecis/tools/preinstall.ps1
+++ packages/ces-edupack-coecis/tools/preinstall.ps1
@@ -1,3 +1,3 @@
 # Runs before the choco package is installed
-If (!(Test-Path "$env:ALLUSERSPROFILE\Granta Design\CES EduPack 2019")){New-Item "$env:ALLUSERSPROFILE\Granta Design\CES EduPack 2019" -ItemType Directory -Force}
-Copy-Item "$env:INSTALL_DIR\license.lic" -Destination "$env:ALLUSERSPROFILE\Granta Design\CES EduPack 2019"
+If (!(Test-Path "$env:ALLUSERSPROFILE\Granta Design\CES EduPack 2020")){New-Item "$env:ALLUSERSPROFILE\Granta Design\CES EduPack 2020" -ItemType Directory -Force}
+Copy-Item "$env:INSTALL_DIR\license.lic" -Destination "$env:ALLUSERSPROFILE\Granta Design\CES EduPack 2020"
