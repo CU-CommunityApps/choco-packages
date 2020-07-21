1f82805ebd1aa1119d28cf76d58a6f16bb963973
lb30 <39345501+lb30@users.noreply.github.com>
2020-07-21 14:50:32 -0400

    Update preinstall.ps1

--- packages/sketchup-lib/tools/preinstall.ps1
+++ packages/sketchup-lib/tools/preinstall.ps1
@@ -1,2 +1,8 @@
 # Runs before the choco package is installed
+
+# Install .Net
 Add-WindowsFeature NET-Framework-Core
+
+# Install prereq's
+Start-Process "$INSTALL_DIR\VC_redist.x86.exe" -ArgumentList "/quiet /norestart" -Wait
+Start-Process "$INSTALL_DIR\VC_redist.x64.exe" -ArgumentList "/quiet /norestart" -Wait
