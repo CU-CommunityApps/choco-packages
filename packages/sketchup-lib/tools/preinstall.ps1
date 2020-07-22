db633254124cd8e5a93fe19940e6c9adcdb630f2
lb30 <39345501+lb30@users.noreply.github.com>
2020-07-22 08:52:00 -0400

    Update preinstall.ps1

--- packages/sketchup-lib/tools/preinstall.ps1
+++ packages/sketchup-lib/tools/preinstall.ps1
@@ -4,5 +4,6 @@
 Add-WindowsFeature NET-Framework-Core
 
 # Install prereq's
+Start-Process "$INSTALL_DIR\ndp48-x86-x64-allos-enu.exe" -ArgumentList "/quiet /norestart" -Wait
 Start-Process "$INSTALL_DIR\VC_redist.x86.exe" -ArgumentList "/quiet /norestart" -Wait
 Start-Process "$INSTALL_DIR\VC_redist.x64.exe" -ArgumentList "/quiet /norestart" -Wait
