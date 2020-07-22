eb99ca1a9b68b25525ff66a41a6d23eed53db25c
lb30 <39345501+lb30@users.noreply.github.com>
2020-07-22 15:05:00 -0400

    Update preinstall.ps1

--- packages/arcgispro-lib/tools/preinstall.ps1
+++ packages/arcgispro-lib/tools/preinstall.ps1
@@ -1,8 +1,2 @@
   
 # Runs before the choco package is installed
-
-# Install .Net
-Start-Process "$INSTALL_DIR\ndp48-x86-x64-allos-enu.exe" -ArgumentList "/quiet /norestart" -Wait
-# Install c++
-Start-Process "$INSTALL_DIR\VC_redistx64.exe" -ArgumentList "/quiet /norestart" -Wait
-Start-Process "$INSTALL_DIR\VC_redistx86.exe" -ArgumentList "/quiet /norestart" -Wait
