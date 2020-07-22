fc265d9761d1c218e1ae8af4d65004be8a9dd2d6
lb30 <39345501+lb30@users.noreply.github.com>
2020-07-22 08:52:11 -0400

    Update preinstall.ps1

--- packages/sketchup-lib/tools/preinstall.ps1
+++ packages/sketchup-lib/tools/preinstall.ps1
@@ -1,8 +1,5 @@
 # Runs before the choco package is installed
 
-# Install .Net
-Add-WindowsFeature NET-Framework-Core
-
 # Install prereq's
 Start-Process "$INSTALL_DIR\ndp48-x86-x64-allos-enu.exe" -ArgumentList "/quiet /norestart" -Wait
 Start-Process "$INSTALL_DIR\VC_redist.x86.exe" -ArgumentList "/quiet /norestart" -Wait
