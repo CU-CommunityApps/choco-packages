dbb0a93d6a8a470c32558b5280e100bc1e366bd8
lb30 <39345501+lb30@users.noreply.github.com>
2020-07-29 15:15:15 -0400

    patch updates

--- packages/arcgispro-cals/tools/postinstall.ps1
+++ packages/arcgispro-cals/tools/postinstall.ps1
@@ -1,2 +1,4 @@
+  
 # Runs after the choco package is installed
-Start-Process msiexec.exe -ArgumentList '/p INSTALL_DIR\ArcGISd1\ArcGIS_Pro_241_170796.msp /qn'
+Start-Process msiexec.exe -ArgumentList '/p INSTALL_DIR\ArcGISd1\ArcGIS_Pro_251_173949.msp /qn'
+Start-Process msiexec.exe -ArgumentList '/p INSTALL_DIR\ArcGISd1\ArcGIS_Pro_251_173950.msp /qn'
