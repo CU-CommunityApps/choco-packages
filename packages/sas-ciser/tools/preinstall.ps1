58b8ec90535d81c2cf15078f88f08f8429a38149
lb30 <39345501+lb30@users.noreply.github.com>
2020-07-30 11:08:02 -0400

    Update preinstall.ps1

--- packages/sas-ciser/tools/preinstall.ps1
+++ packages/sas-ciser/tools/preinstall.ps1
@@ -1,7 +1,7 @@
 # Runs before the choco package is installed
 # Copy pre installation files
 cp "$env:TOOLS_DIR\sdwresponse.properties" "$env:windir\temp\sdwresponse.properties" -Force
-cp "$env:INSTALL_DIR\sid_files\SAS94_9C5M7J_70084773_Win_X64_Srv.txt" "$env:windir\temp\SAS94_9C5M7J_70084773_Win_X64_Srv.txt" -Force
+cp "$env:INSTALL_DIR\sid_files\SAS94_9CCLNB_70084773_Win_X64_Srv.txt" "$env:windir\temp\SAS94_9CCLNB_70084773_Win_X64_Srv.txt" -Force
 <#
 :: --------------------------------------------------------------------------------
 :: This is a batch system requirements file created from order 9BVPLP at
