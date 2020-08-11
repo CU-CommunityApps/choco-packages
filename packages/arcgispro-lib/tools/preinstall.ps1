ad18476b4b1eb7c6a50bd1dcaf3c0da5524210e3
Dan Klinger <dpk25@cornell.edu>
2020-08-10 21:09:40 -0400

    Update preinstall.ps1

--- packages/arcgispro-lib/tools/preinstall.ps1
+++ packages/arcgispro-lib/tools/preinstall.ps1
@@ -1,5 +1,6 @@
-  
 # Runs before the choco package is installed
+
+$INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
 
 #Install .NET
 Start-Process "$INSTALL_DIR\ndp48-x86-x64-allos-enu.exe" -ArgumentList "/quiet /norestart" -Wait
