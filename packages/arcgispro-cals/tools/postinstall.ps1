aecc569cedd6c247580cad6ac0ae73d0a013bd31
Dan Klinger <dpk25@cornell.edu>
2020-08-10 21:22:44 -0400

    Adding in missing $ for variable and proper path

--- packages/arcgispro-cals/tools/postinstall.ps1
+++ packages/arcgispro-cals/tools/postinstall.ps1
@@ -1,4 +1,6 @@
-  
 # Runs after the choco package is installed
-Start-Process msiexec.exe -ArgumentList '/p INSTALL_DIR\ArcGISd1\ArcGIS_Pro_251_173949.msp /qn'
-Start-Process msiexec.exe -ArgumentList '/p INSTALL_DIR\ArcGISd1\ArcGIS_Pro_251_173950.msp /qn'
+
+$INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
+
+Start-Process msiexec.exe -ArgumentList '/p $INSTALL_DIR\ArcGISd1\ArcGIS_Pro_251_173949.msp /qn'
+Start-Process msiexec.exe -ArgumentList '/p $INSTALL_DIR\ArcGISd1\ArcGIS_Pro_251_173950.msp /qn'
