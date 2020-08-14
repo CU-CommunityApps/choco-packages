2d4c35868fb299020004e897db2807170a18a227
Dan Klinger <dpk25@cornell.edu>
2020-08-13 21:41:24 -0400

    Update preinstall.ps1

--- packages/minitab-ciser/tools/preinstall.ps1
+++ packages/minitab-ciser/tools/preinstall.ps1
@@ -1,3 +1,5 @@
 # Runs before the choco package is installed
 
-Copy-Item "$env:INSTALL_DIR\minitab.lic" "%PROGRAMFILES%\Minitab\Minitab 19\minitab.lic"
+$INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
+
+Copy-Item "$INSTALL_DIR\minitab.lic" "%PROGRAMFILES%\Minitab\Minitab 19\minitab.lic"
