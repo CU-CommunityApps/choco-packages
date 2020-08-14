3991eaecd6930479746fc05d5fb9bd655ff93a21
Dan Klinger <dpk25@cornell.edu>
2020-08-14 08:23:26 -0400

    Minitab (#190)
    * only chgd lic file
    
    * Update config.yml
    
    * Update preinstall.ps1
    
    * Update config.yml
    
    * Update preinstall.ps1
    
    * Wrong language!?
    
    * Update preinstall.ps1
    
    * Create directory
    
    * Updated command line parameters
    
    * Changed to programdata
    
    * 19.2020.1.0 updates
    
    * Update preinstall.ps1
    
    * Update config.yml
    
    * Update config.yml
    
    Co-authored-by: lb30 <39345501+lb30@users.noreply.github.com>

--- packages/minitab-ciser/tools/preinstall.ps1
+++ packages/minitab-ciser/tools/preinstall.ps1
@@ -1,2 +1,7 @@
 # Runs before the choco package is installed
 
+$INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
+
+New-Item -ItemType Directory "$env:allusersprofile\Minitab\Minitab 19" -Force
+Copy-Item "$INSTALL_DIR\minitab.lic" "$env:allusersprofile\Minitab\Minitab 19\minitab.lic" -Recurse -Force
+Copy-Item "$INSTALL_DIR\License.ini" "$env:ALLUSERSPROFILE\Minitab\License.ini" -Recurse -Force
