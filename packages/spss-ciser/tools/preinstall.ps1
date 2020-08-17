9b4b60790a0cb50aac7733672c23070d941c27e0
lb30 <39345501+lb30@users.noreply.github.com>
2020-08-17 10:31:22 -0400

    Update preinstall.ps1

--- packages/spss-ciser/tools/preinstall.ps1
+++ packages/spss-ciser/tools/preinstall.ps1
@@ -4,4 +4,3 @@ $INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
 
 #Install other pre-reqs
 Start-Process "$INSTALL_DIR\VC_redist.x64.exe" -ArgumentList "/quiet /norestart" -Wait
-Start-Process "$INSTALL_DIR\VC_redist.x86.exe" -ArgumentList "/quiet /norestart" -Wait
