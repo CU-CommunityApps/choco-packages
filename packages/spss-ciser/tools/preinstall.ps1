bd92943e8e348519865f177bc48bdf97fe3cc0e8
lb30 <39345501+lb30@users.noreply.github.com>
2020-08-17 10:03:12 -0400

    prereq installs

--- packages/spss-ciser/tools/preinstall.ps1
+++ packages/spss-ciser/tools/preinstall.ps1
@@ -1,2 +1,7 @@
 # Runs before the choco package is installed
-Start-Process "$env:INSTALL_DIR\vcredist_x64.exe" -ArgumentList "/q /norestart" -Wait
+
+$INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
+
+#Install other pre-reqs
+Start-Process "$INSTALL_DIR\VC_redist.x64.exe" -ArgumentList "/quiet /norestart" -Wait
+Start-Process "$INSTALL_DIR\VC_redist.x86.exe" -ArgumentList "/quiet /norestart" -Wait
