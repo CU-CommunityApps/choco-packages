c812088bc6a9c19ba1942b12f091792de8b38a1e
Dan Klinger <dpk25@cornell.edu>
2020-08-25 21:13:18 -0400

    Solidworks (#209)
    * Switch to .msi's
    
    * C++ prereqs
    
    * Session script to run at user login
    
    * Update config.yml
    
    * Update config.yml
    
    * Adding ""  around INSTALL_DIR
    
    * Update config.yml
    
    * Neither service exists for 2020
    
    * Visualize 2020 service version
    
    * Ugh
    
    Co-authored-by: unknown <ImageBuilderAdmin@85de23e5d53840e.cornell.edu>

--- packages/solidworks-coecis/tools/preinstall.ps1
+++ packages/solidworks-coecis/tools/preinstall.ps1
@@ -1,2 +1,6 @@
 # Runs before the choco package is installed
+$INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
+
 Add-WindowsFeature NET-Framework-Core
+
+gci -Path $INSTALL_DIR\Microsoft* -Filter *.exe -Recurse | % {Start-Process $_.FullName -ArgumentList "/q /norestart" -wait -Verbose}
