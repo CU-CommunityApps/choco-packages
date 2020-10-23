878c095b2f7a44fa47e9a585ab2231a92e94480f
lb30 <39345501+lb30@users.noreply.github.com>
2020-10-23 15:01:34 -0400

    Update postinstall.ps1

--- packages/qgis-cornell/tools/postinstall.ps1
+++ packages/qgis-cornell/tools/postinstall.ps1
@@ -1,4 +1,2 @@
 # Runs after the choco package is installed
 Invoke-WebRequest https://ubuntu.qgis.org/version.txt
-
-Expand-Archive -LiteralPath '$env:TOOLS_DIR\QGIS3.zip' -DestinationPath '$env:SYSTEMDRIVE\Users\Default\AppData\Roaming\QGIS'
