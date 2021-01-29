21ca37b665ca3461eeb13545a1c4bf2e433bd8f8
unknown <ImageBuilderAdmin@f20ef61755d842d.cornell.edu>
2021-01-29 17:49:05 -0500

    Copy file to install dir prior to installation

--- packages/revit-cornell/tools/preinstall.ps1
+++ packages/revit-cornell/tools/preinstall.ps1
@@ -1,3 +1,10 @@
 # Runs before the choco package is installed
 
+# Installation dir location
+$INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
 
+# Tools dir location
+$TOOLS_DIR = Join-Path $PSScriptRoot
+
+# File contains chars that chocolatey will not extract, copying manually before installation
+Copy-Item -Path "$TOOLS_DIR\ArchVision_C1_Tina[2].rpc" -Destination "$INSTALL_DIR\Img\x64\RVT\PF32\CF32\AS32\Materials\2021\assetlibrary_base.fbm\RPCs\ArchVision_C1_Tina[2].rpc" -Force
