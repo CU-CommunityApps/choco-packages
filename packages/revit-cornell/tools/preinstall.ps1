574ed3b65686e156ea09c29c7d72a63bf34fe823
Dan Klinger <dpk25@cornell.edu>
2021-02-01 14:13:22 -0500

    Revit cornell (#258)
    * revitcornell
    
    * Update config.yml
    
    * Update config.yml
    
    * Update config.yml
    
    * test1
    
    * Update config.yml
    
    * revit fixes
    
    * Removing Image Library installs
    
    * Unfriendly chars in file path file
    
    * Copy file to install dir prior to installation
    
    * Bad file name
    
    * File name syntax
    
    * File name mismatch
    
    * Update config.yml
    
    * Update preinstall.ps1
    
    Co-authored-by: Lori Beebe <lb30@cornell.edu>
    Co-authored-by: lb30 <39345501+lb30@users.noreply.github.com>
    Co-authored-by: unknown <ImageBuilderAdmin@f20ef61755d842d.cornell.edu>

--- packages/revit-cornell/tools/preinstall.ps1
+++ packages/revit-cornell/tools/preinstall.ps1
@@ -1,8 +1,7 @@
 # Runs before the choco package is installed
+
+# Installation dir location
 $INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
 
-# Install prereq's
-Start-Process "$INSTALL_DIR\VC_redist.x86.exe" -ArgumentList "/quiet /norestart" -Wait
-Start-Process "$INSTALL_DIR\VC_redist.x64.exe" -ArgumentList "/quiet /norestart" -Wait
-
-
+# File contains chars that chocolatey will not extract, copying manually before installation
+Copy-Item -Path "$INSTALL_DIR\ArchVisionC1Tina2.rpc" -Destination "$INSTALL_DIR\Img\x64\RVT\PF32\CF32\AS32\Materials\2021\assetlibrary_base.fbm\RPCs\ArchVision_C1_Tina[2].rpc" -Force
