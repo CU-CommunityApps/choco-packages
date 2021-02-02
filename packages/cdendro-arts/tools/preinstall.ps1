1a5f71bc53891f245698f3f8661d4604dbbd699a
kb599 <34450561+kb599@users.noreply.github.com>
2021-02-02 17:55:04 -0500

    Update preinstall.ps1
    path update

--- packages/cdendro-arts/tools/preinstall.ps1
+++ packages/cdendro-arts/tools/preinstall.ps1
@@ -2,4 +2,4 @@
 DISM /Online /Enable-Feature /FeatureName:NetFx3 /All 
 
 $INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
-Copy-Item -Path "$INSTALL_DIR\Cybis" -Destination $env:PROGRAMFILES(x86)\Cybis -recurse
+Copy-Item -Path "$INSTALL_DIR\Cybis" -Destination "C:\PROGRAMFILES(x86)\Cybis" -recurse
