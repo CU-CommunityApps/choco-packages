a6f81a335b35b658ae195bb5bbfe232b5e723bdf
kb599 <34450561+kb599@users.noreply.github.com>
2021-02-02 17:26:34 -0500

    Update preinstall.ps1
    .net 3.5 and folder copy

--- packages/cdendro-arts/tools/preinstall.ps1
+++ packages/cdendro-arts/tools/preinstall.ps1
@@ -1,2 +1,5 @@
 # Runs before the choco package is installed
+DISM /Online /Enable-Feature /FeatureName:NetFx3 /All 
 
+$INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
+Copy-Item -Path "$INSTALL_DIR\Cybis" -Destination $env:SYSTEMDRIVE\Cybis -recurse
