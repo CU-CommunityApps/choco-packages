6af1a4b80087817f462f9690e09fc79042875078
Dan Klinger <dpk25@cornell.edu>
2021-02-21 20:56:43 -0500

    Missing \

--- packages/acrobat-named-cornell/tools/preinstall.ps1
+++ packages/acrobat-named-cornell/tools/preinstall.ps1
@@ -1,6 +1,6 @@
 # Runs before the choco package is installed
-$AdminKey = “HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}”
-$UserKey = “HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}”
-Set-ItemProperty -Path $AdminKey -Name “IsInstalled” -Value 0
-Set-ItemProperty -Path $UserKey -Name “IsInstalled” -Value 0
+$AdminKey = â€œHKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}â€
+$UserKey = â€œHKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}â€
+Set-ItemProperty -Path $AdminKey -Name â€œIsInstalledâ€ -Value 0
+Set-ItemProperty -Path $UserKey -Name â€œIsInstalledâ€ -Value 0
 Stop-Process -Name Explorer
