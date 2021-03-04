73c60189c7b12b3f4566908bc9666d0fe139c8a1
lb30 <39345501+lb30@users.noreply.github.com>
2021-03-04 13:06:27 -0500

    Update preinstall.ps1

--- packages/dendroclim-arts/tools/preinstall.ps1
+++ packages/dendroclim-arts/tools/preinstall.ps1
@@ -2,4 +2,4 @@
 
 $INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
 
-Copy-Item -Path "$env:INSTALL_DIR\dendroclim.zip" -DestinationPath "env:ProgramFiles(x86)\UNR" -recurse
+Copy-Item -Path "$env:INSTALL_DIR\*" -Destination "$env:ProgramFiles(x86)\UNR" -recurse
