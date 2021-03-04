ede35e122a6c9cc8500b51499c78a9a5015a73e8
lb30 <39345501+lb30@users.noreply.github.com>
2021-03-04 11:29:57 -0500

    Update preinstall.ps1

--- packages/dendroclim-arts/tools/preinstall.ps1
+++ packages/dendroclim-arts/tools/preinstall.ps1
@@ -1,3 +1,5 @@
 # Runs before the choco package is installed
 
+$INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
+
 Expand-Archive -LiteralPath "$env:INSTALL_DIR\dendroclim.zip" -DestinationPath "c:\Program Files(x86)\UNR"
