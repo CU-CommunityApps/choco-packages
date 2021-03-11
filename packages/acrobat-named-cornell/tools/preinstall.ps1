668f1a7a8dd09a25684f5a3208c2b2d166d3e06c
lb30 <39345501+lb30@users.noreply.github.com>
2021-03-11 14:35:06 -0500

    Acrobat named cornell (#274)
    * Update preinstall.ps1
    
    * Update config.yml

--- packages/acrobat-named-cornell/tools/preinstall.ps1
+++ packages/acrobat-named-cornell/tools/preinstall.ps1
@@ -3,4 +3,3 @@ $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A
 $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
 Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
 Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
-Stop-Process -Name Explorer
