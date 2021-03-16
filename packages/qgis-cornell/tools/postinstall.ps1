bd59c52166c36c2c3857039f725e61d99b54221e
Dan Klinger <dpk25@cornell.edu>
2021-03-15 21:52:08 -0400

    Remove VC++ 08 and 2010

--- packages/qgis-cornell/tools/postinstall.ps1
+++ packages/qgis-cornell/tools/postinstall.ps1
@@ -1,2 +1,28 @@
 # Runs after the choco package is installed
 Invoke-WebRequest https://ubuntu.qgis.org/version.txt
+
+# List of applications to remove
+# 2005 = 8
+# 2008 = 9
+# 2010 = 10
+# 2012 = 11
+$vers = @("8", "9", "10")
+
+ForEach ($ver in $vers) {
+
+    # Get uninstall info for each possible application
+    If (gci -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall  | `
+    gp | where {($_.DisplayName -match "Microsoft Visual C") -and (($_.DisplayVersion).StartsWith("$ver."))}){
+
+        $install = gci -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall  | `
+        gp | where {($_.DisplayName -match "Microsoft Visual C") -and (($_.DisplayVersion).StartsWith("$ver."))} | select DisplayName, InstallLocation, UninstallString
+    
+        If ($install.UninstallString -match "/X"){
+            write-host $install.UninstallString
+            $install.UninstallString = $install.UninstallString.Substring($install.UninstallString.IndexOf("/X")+2).Trim()
+            write-host $install.UninstallString
+        }
+        Start-Process "msiexec.exe" -ArgumentList "/x $($install.UninstallString) /q" -Wait
+    }
+
+}
