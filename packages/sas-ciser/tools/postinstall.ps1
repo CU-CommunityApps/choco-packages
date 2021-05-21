7d51133e8eeb39f2891323b3ee268f5eb6fa4c35
lb30 <39345501+lb30@users.noreply.github.com>
2021-05-21 15:33:46 -0400

    Sas lic swap (#294)
    * Update config.yml
    
    * Update sdwresponse.properties
    
    * Update postinstall.ps1
    
    * Update preinstall.ps1

--- packages/sas-ciser/tools/postinstall.ps1
+++ packages/sas-ciser/tools/postinstall.ps1
@@ -1,4 +1,4 @@
 # Runs after the choco package is installed
 # Remove temporary installation files
 rm "$env:windir\Temp\sdwresponse.properties" -Force
-rm "$env:windir\Temp\SAS94_9CCLNB_70084773_Win_X64_Srv.txt" -Force
+rm "$env:windir\Temp\SAS94_9CK519_70084773_Win_X64_Srv.txt" -Force
