7356cb29ec13b27c76fa8a066d38832e42a8ce04
lb30 <39345501+lb30@users.noreply.github.com>
2021-06-10 13:26:13 -0400

    Autocad2022 (#297)
    * autocad22
    
    * Update config.yml
    
    * Update config.yml
    
    * Update config.yml

--- packages/autocad-cornell/tools/postinstall.ps1
+++ packages/autocad-cornell/tools/postinstall.ps1
@@ -1,3 +1,3 @@
 # Runs after the choco package is installed
-$custExperience = "$env:ProgramFiles\Autodesk\AutoCAD 2021\AcExperience.arx"
+$custExperience = "$env:ProgramFiles\Autodesk\AutoCAD 2022\AcExperience.arx"
 If (Test-Path $custExperience){Remove-Item $custExperience -Force}
