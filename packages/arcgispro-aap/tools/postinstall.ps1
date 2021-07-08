f752013ac6d97701378e27054d0349643714c5c7
lb30 <39345501+lb30@users.noreply.github.com>
2021-07-08 13:45:13 -0400

    Arcgispro aap (#312)
    * update
    
    * Update postinstall.ps1
    
    * Update config.yml
    
    * Update config.yml

--- packages/arcgispro-aap/tools/postinstall.ps1
+++ packages/arcgispro-aap/tools/postinstall.ps1
@@ -2,5 +2,4 @@
 
 $INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
 
-Start-Process msiexec.exe -ArgumentList '/p $INSTALL_DIR\ArcGISd1\ArcGIS_Pro_251_173949.msp /qn /norestart'
-Start-Process msiexec.exe -ArgumentList '/p $INSTALL_DIR\ArcGISd1\ArcGIS_Pro_252_173950.msp /qn /norestart'
+Start-Process msiexec.exe -ArgumentList '/p $INSTALL_DIR\ArcGISd1\ArcGIS_Pro_281_177644.msp /qn /norestart'
