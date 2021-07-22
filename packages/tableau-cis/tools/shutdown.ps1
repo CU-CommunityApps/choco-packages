28b8ae1c55cd7c4776f6f41c4d114db0a808005c
lb30 <39345501+lb30@users.noreply.github.com>
2021-07-22 10:53:01 -0400

    Update shutdown.ps1

--- packages/tableau-cis/tools/shutdown.ps1
+++ packages/tableau-cis/tools/shutdown.ps1
@@ -1,2 +1,2 @@
 Write-Output "Returning license..."
-Start-Process "$env:ProgramFiles\Tableau\Tableau 2019.2\bin\tableau.exe" -ArgumentList "-return $env:STATIC_SYSTEM_TABLEAU_ACTIVATE_KEY" -Wait
+Start-Process "$env:ProgramFiles\Tableau\Tableau 2021.2\bin\tableau.exe" -ArgumentList "-return $env:STATIC_SYSTEM_TABLEAU_ACTIVATE_KEY" -Wait
