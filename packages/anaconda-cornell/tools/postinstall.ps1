00a56494f4f2521e440a8ffd002dc16a92cc73de
lb30 <39345501+lb30@users.noreply.github.com>
2021-07-27 08:48:01 -0400

    Update postinstall.ps1

--- packages/anaconda-cornell/tools/postinstall.ps1
+++ packages/anaconda-cornell/tools/postinstall.ps1
@@ -4,7 +4,10 @@ start-sleep -second 60
 start-process "c:\program Files\Anaconda\Scripts\conda.exe" -argumentlist "install -y --upgrade pip wheel setuptools" -NoNewWindow
 start-process "c:\program Files\Anaconda\Scripts\conda.exe" -argumentlist "install -y pypiwin32" -NoNewWindow
 start-process "c:\program Files\Anaconda\Scripts\conda.exe" -argumentlist "install -y docutils pygments" -NoNewWindow
-start-process "c:\program Files\Anaconda\Scripts\conda.exe" -argumentlist "install -y kivy_deps.sdl2==0.1.22 kivy_deps.glew==0.1.12" -NoNewWindow
-start-process "c:\program Files\Anaconda\Scripts\conda.exe" -argumentlist "install -y kivy_deps.gstreamer==0.1.17" -NoNewWindow
+start-process "c:\program Files\Anaconda\Scripts\conda.exe" -argumentlist "uninstall -y kivy.deps.glew kivy.deps.gstreamer kivy.deps.sd12 kivy.deps.angle" -NoNewWindow
+start-process "c:\program Files\Anaconda\Scripts\conda.exe" -argumentlist "install -y kivy_deps.sdl2==0.1.* kivy_deps.glew==0.1.*" -NoNewWindow
+start-process "c:\program Files\Anaconda\Scripts\conda.exe" -argumentlist "install -y kivy_deps.gstreamer kivy_deps.gstreamer==0.1.*" -NoNewWindow
+start-process "c:\program Files\Anaconda\Scripts\conda.exe" -argumentlist "install -y kivy_deps.angle==0.1*" -NoNewWindow
+start-process "c:\program Files\Anaconda\Scripts\conda.exe" -argumentlist "install -y kivy==1.11.1" -NoNewWindow
 start-process "c:\program Files\Anaconda\Scripts\conda.exe" -argumentlist "install -y kivy --upgrade" -NoNewWindow
 start-process "c:\program Files\Anaconda\Scripts\conda.exe" -argumentlist "install -y introcs --upgrade" -NoNewWindow
