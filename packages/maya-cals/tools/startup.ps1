fd1e83b33aaa87f0db8796f2fff489af99433858
Lori Beebe <lb30@cornell.edu>
2021-07-29 15:02:57 -0400

    maya-cornell

--- packages/maya-cals/tools/startup.ps1
+++
@@ -1,1 +0,0 @@
-$env:ADSKFLEX_LICENSE_FILE.split(';') | % { If ($_.Startswith("@")) {Write-Output "Setting network license settings...";Start-Process "${env:CommonProgramFiles(x86)}\Autodesk Shared\AdskLicensing\Current\helper\AdskLicensingInstHelper.exe" -ArgumentList "change -pk 657L1 -pv 2020.0.0.F -lm NETWORK -lt SINGLE -ls 2080$env:ADSKFLEX_LICENSE_FILE"}}
