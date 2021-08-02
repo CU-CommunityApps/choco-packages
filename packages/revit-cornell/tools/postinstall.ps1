a54de3847f7a69b4aaade03b147b22aa622dd4fa
lb30 <39345501+lb30@users.noreply.github.com>
2021-08-02 09:50:42 -0400

    Update postinstall.ps1

--- packages/revit-cornell/tools/postinstall.ps1
+++ packages/revit-cornell/tools/postinstall.ps1
@@ -1,3 +1,1 @@
 # Runs after the choco package is installed
-$AdminKey = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
-Set-ItemProperty -Path $AdminKey -Name "pac" -Value '"C:\Program Files\Autodesk\Personal Accelerator for Revit\RevitAccelerator.exe"'
