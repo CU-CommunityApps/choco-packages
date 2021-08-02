fbcbe20b443d119065fd6c5a0b1296a8262d911a
lb30 <39345501+lb30@users.noreply.github.com>
2021-08-02 14:56:07 -0400

    Anaconda cornell (#334)
    * version update
    
    * Update postinstall.ps1
    
    * Update postinstall.ps1
    
    * Add files via upload
    
    * Update postinstall.ps1
    
    * Update postinstall.ps1
    
    * Update postinstall.ps1
    
    * Update postinstall.ps1
    
    * Update config.yml

--- packages/anaconda-cornell/tools/postinstall.ps1
+++ packages/anaconda-cornell/tools/postinstall.ps1
@@ -1,3 +1,4 @@
 # Runs after the choco package is installed
+
 start-sleep -second 60
-start-process "$env:programFiles\Anaconda\Scripts\conda.exe" -argumentlist "install -y numpy" -NoNewWindow
+Start-Process "$INSTALL_DIR\CornellExtensionsInstall\install.lnk" -ArgumentList "/quiet /norestart" -wait
