f0d7ec1e10e4a8eed6f3f4eb9a4f700ce8dac8f9
lb30 <39345501+lb30@users.noreply.github.com>
2021-08-02 13:08:57 -0400

    Update postinstall.ps1

--- packages/anaconda-cornell/tools/postinstall.ps1
+++ packages/anaconda-cornell/tools/postinstall.ps1
@@ -1,4 +1,4 @@
 # Runs after the choco package is installed
 
 start-sleep -second 60
-Start-Process "$INSTALL_DIR\CornellExtensionsInstall\install.lnk" -ArgumentList "/quiet /norestart" -force
+Start-Process "$INSTALL_DIR\CornellExtensionsInstall\install.lnk" -ArgumentList "/quiet /norestart" -wait
