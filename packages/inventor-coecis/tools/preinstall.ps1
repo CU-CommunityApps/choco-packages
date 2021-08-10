258754904b3d2426816c33f8f06f98f74efb3343
lb30 <39345501+lb30@users.noreply.github.com>
2021-08-10 09:50:04 -0400

    Update preinstall.ps1

--- packages/inventor-coecis/tools/preinstall.ps1
+++ packages/inventor-coecis/tools/preinstall.ps1
@@ -1,3 +1,3 @@
 # Runs before the choco package is installed
 
-Copy-Item -Path "%INSTALL_DIR%\inventor" -Destination "%SYSTEMDRIVE%\inventor" -Recurse
+Copy-Item -Path %INSTALL_DIR%\inventor -Destination c:\inventor -Recurse
