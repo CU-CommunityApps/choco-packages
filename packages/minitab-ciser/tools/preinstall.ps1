3ba632ea217cb7c019be2cf6ddad2d7f57ba8727
lb30 <39345501+lb30@users.noreply.github.com>
2021-08-16 17:14:39 -0400

    Update preinstall.ps1

--- packages/minitab-ciser/tools/preinstall.ps1
+++ packages/minitab-ciser/tools/preinstall.ps1
@@ -1,7 +1,1 @@
 # Runs before the choco package is installed
-
-$INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
-
-New-Item -ItemType Directory "$env:allusersprofile\Minitab\Minitab 20" -Force
-Copy-Item "$INSTALL_DIR\minitab.lic" "$env:allusersprofile\Minitab\Minitab 20\minitab.lic" -Recurse -Force
-Copy-Item "$INSTALL_DIR\License.ini" "$env:ALLUSERSPROFILE\Minitab\License.ini" -Recurse -Force
