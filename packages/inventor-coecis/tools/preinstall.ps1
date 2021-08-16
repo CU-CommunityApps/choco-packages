e23fd580f6572759541be09f516872106e6aff32
lb30 <39345501+lb30@users.noreply.github.com>
2021-08-16 12:20:56 -0400

    Update preinstall.ps1

--- packages/inventor-coecis/tools/preinstall.ps1
+++ packages/inventor-coecis/tools/preinstall.ps1
@@ -1,7 +1,1 @@
 # Runs before the choco package is installed
-
-
-$INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
-
-# Install prereq's
-Start-Process "$INSTALL_DIR\inventor\Install inventor.bat" -ArgumentList "/quiet /norestart" -Wait
