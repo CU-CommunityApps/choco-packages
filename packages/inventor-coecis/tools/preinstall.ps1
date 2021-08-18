1ffccd24b46408ee7c6992f509c07624d4164560
lb30 <39345501+lb30@users.noreply.github.com>
2021-08-18 09:53:24 -0400

    Update preinstall.ps1

--- packages/inventor-coecis/tools/preinstall.ps1
+++ packages/inventor-coecis/tools/preinstall.ps1
@@ -1,4 +1,4 @@
-# Runs before the choco package is installed
+# Runs before the choco package is installed 
 
 start-sleep -second 60
 Start-Process "$INSTALL_DIR\install\Installinventor.cmd" -ArgumentList "/quiet /norestart" -wait
