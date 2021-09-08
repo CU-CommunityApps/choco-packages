c414cbad264ad147091d77cf31660212dc9c4ac4
unknown <ImageBuilderAdmin@2814a2fddf1d4b7.cornell.edu>
2021-09-08 14:19:05 -0400

    revit 2022 updates

--- packages/revit-cornell/tools/preinstall.ps1
+++ packages/revit-cornell/tools/preinstall.ps1
@@ -1,1 +1,6 @@
 # Runs before the choco package is installed
+
+# Installation dir location
+$INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
+
+Start-Process "$INSTALL_DIR\install-revit-cornell.bat" -wait
