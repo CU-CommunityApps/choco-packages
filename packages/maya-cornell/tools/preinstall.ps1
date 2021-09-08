4005d51c39025aa40913eb0b95ffcabcca1ea206
unknown <ImageBuilderAdmin@2814a2fddf1d4b7.cornell.edu>
2021-09-07 23:02:27 -0400

    maya 2022

--- packages/maya-cornell/tools/preinstall.ps1
+++ packages/maya-cornell/tools/preinstall.ps1
@@ -1,2 +1,5 @@
-# Runs before the choco package is installed
+# Runs before the choco package is installed 
 
+$INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
+
+Start-Process "$INSTALL_DIR\install-maya-cornell.bat" -wait
