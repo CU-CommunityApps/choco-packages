02505302c1409755732a5d7daa53a4a9981ed58a
unknown <ImageBuilderAdmin@2814a2fddf1d4b7.cornell.edu>
2021-09-07 21:01:26 -0400

    Inventor 2022 updates

--- packages/inventor-coecis/tools/preinstall.ps1
+++ packages/inventor-coecis/tools/preinstall.ps1
@@ -1,4 +1,5 @@
 # Runs before the choco package is installed 
 
-start-sleep -second 60
-Start-Process "$INSTALL_DIR\install\Installinventor.cmd" -ArgumentList "/quiet /norestart" -wait
+$INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
+
+Start-Process "$INSTALL_DIR\install-inventor-coecis.bat" -wait
