dc683d78015953912dbf223ae5dbb6c83de4b47b
lb30 <39345501+lb30@users.noreply.github.com>
2021-09-09 15:18:36 -0400

    Update preinstall.ps1

--- packages/inventor-coecis/tools/preinstall.ps1
+++ packages/inventor-coecis/tools/preinstall.ps1
@@ -2,4 +2,6 @@
 
 $INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
 
-Start-Process "$INSTALL_DIR\install-inventor-coecis.bat" -wait
+Copy-Item -Path %INSTALL_DIR%\image -Destination $SYSTEMDRIVE%\ -PassThru
+
+#Start-Process "$INSTALL_DIR\install-inventor-coecis.bat" -wait
