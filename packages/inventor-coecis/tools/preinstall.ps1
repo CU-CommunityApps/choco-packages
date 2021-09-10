3eba73b36859f3a4fe59259b87b596d340dd362a
Dan Klinger <dpk25@cornell.edu>
2021-09-10 11:37:25 -0400

    No preinstall necessary

--- packages/inventor-coecis/tools/preinstall.ps1
+++ packages/inventor-coecis/tools/preinstall.ps1
@@ -1,7 +1,1 @@
 # Runs before the choco package is installed 
-
-$INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
-
-# Copy-Item -Path %INSTALL_DIR%\image -Destination $SYSTEMDRIVE%\ -PassThru
-
-Start-Process "$INSTALL_DIR\install-inventor-coecis.bat" -wait
