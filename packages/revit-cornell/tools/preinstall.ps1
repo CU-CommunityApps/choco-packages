26794394d7384c83008f1549ff9e4fa79d5a2b73
Dan Klinger <dpk25@cornell.edu>
2021-09-10 11:30:20 -0400

    Wrong path in Autodesk generated .bat file...

--- packages/revit-cornell/tools/preinstall.ps1
+++ packages/revit-cornell/tools/preinstall.ps1
@@ -1,6 +1,6 @@
 # Runs before the choco package is installed
 
-# Installation dir location
-$INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
+# # Installation dir location
+# $INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
 
-Start-Process "$INSTALL_DIR\install-revit-cornell.bat" -wait
+# Start-Process "$INSTALL_DIR\install-revit-cornell.bat" -wait
