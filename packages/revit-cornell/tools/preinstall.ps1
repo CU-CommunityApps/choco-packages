8480556b072354e910bffabeb09f9397f415657d
unknown <ImageBuilderAdmin@2814a2fddf1d4b7.cornell.edu>
2021-09-10 13:38:07 -0400

    Bad file name fix

--- packages/revit-cornell/tools/preinstall.ps1
+++ packages/revit-cornell/tools/preinstall.ps1
@@ -1,6 +1,7 @@
 # Runs before the choco package is installed
 
-# # Installation dir location
-# $INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
+# Installation dir location
+$INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
 
-# Start-Process "$INSTALL_DIR\install-revit-cornell.bat" -wait
+# File contains chars that chocolatey will not extract, copying manually before installation
+Copy-Item -Path "$PSScriptRoot\ArchVisionC1Tina2.rpc" -Destination "$INSTALL_DIR\image\RVT_2022_en-US\x64\RVT\PF32\CF32\AS32\Materials\2022\assetlibrary_base.fbm\RPCs\ArchVision_C1_Tina[2].rpc" -Force
