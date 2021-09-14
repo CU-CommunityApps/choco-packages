35054425fb366024dead20c3fa037ddeab101a51
Dan Klinger <dpk25@cornell.edu>
2021-09-13 21:46:17 -0400

    inventor 2022 (#376)

--- packages/inventor-coecis/tools/preinstall.ps1
+++ packages/inventor-coecis/tools/preinstall.ps1
@@ -1,9 +1,7 @@
-# Runs before the choco package is installed
+# Runs before the choco package is installed 
+
+# Installation dir location
 $INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
 
-# # Install .NET
-# Add-WindowsFeature NET-Framework-Core
-
-# Install prereq's
-Start-Process "$INSTALL_DIR\VC_redist.x86.exe" -ArgumentList "/quiet /norestart" -Wait
-Start-Process "$INSTALL_DIR\VC_redist.x64.exe" -ArgumentList "/quiet /norestart" -Wait
+# File contains chars that chocolatey will not extract, copying manually before installation
+Copy-Item -Path "$PSScriptRoot\ArchVisionC1Tina2.rpc" -Destination "$INSTALL_DIR\image\INVPROSA_2022_en-US\x64\RX\PF32\CF32\AS32\Materials\2022\assetlibrary_base.fbm\RPCs\ArchVision_C1_Tina[2].rpc" -Force
