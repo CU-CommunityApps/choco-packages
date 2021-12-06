df346fe68216fe55db1629708a693ed1cde35d68
Dan Klinger <dpk25@cornell.edu>
2021-12-06 16:44:12 -0500

    Update postinstall.ps1

--- packages/googledrivefilestream-cornell/tools/postinstall.ps1
+++ packages/googledrivefilestream-cornell/tools/postinstall.ps1
@@ -9,4 +9,4 @@ Stop-Service gupdatem -Force
 Set-Service gupdatem -StartupType Disabled
 
 # Kill all running Google Drive processes
-Get-Process 'GoogleDriveFS' | Stop-process
+Get-Process 'GoogleDriveFS' | Stop-process -Force
