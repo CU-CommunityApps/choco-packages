9da9ac6175d8353f00c1ecb91a46fcfd21233058
Dan Klinger <dpk25@cornell.edu>
2021-12-06 16:31:02 -0500

    Update postinstall.ps1

--- packages/googledrivefilestream-cornell/tools/postinstall.ps1
+++ packages/googledrivefilestream-cornell/tools/postinstall.ps1
@@ -1,6 +1,4 @@
 # Runs after the choco package is installed
-SCHTASKS /DELETE /TN "GoogleUpdateTaskMachineUA" /F
-SCHTASKS /DELETE /TN "GoogleUpdateTaskMachineCore" /F
 Stop-Service gupdate
 Set-Service gupdate -StartupType Disabled
 Stop-Service gupdatem
