51ccb08b2ca67265c531cc525e3f2f71330d5db3
kb599 <34450561+kb599@users.noreply.github.com>
2021-12-22 14:48:37 -0500

    Update postinstall.ps1
    schedule tasks showing up, but services disabled.

--- packages/googledrivefilestream-cornell/tools/postinstall.ps1
+++ packages/googledrivefilestream-cornell/tools/postinstall.ps1
@@ -1,12 +1,14 @@
 # Runs after the choco package is installed
-
-#Start-Sleep -s 10
+SCHTASKS /DELETE /TN "GoogleUpdateTaskMachineUA" /F
+SCHTASKS /DELETE /TN "GoogleUpdateTaskMachineCore" /F
 
 # Stop and disable updater services
 Stop-Service gupdate -Force
 Set-Service gupdate -StartupType Disabled
 Stop-Service gupdatem -Force
 Set-Service gupdatem -StartupType Disabled
+
+Start-Sleep -s 10
 
 # Kill all running Google Drive processes
 If (Get-Process "GoogleDrive*"){Get-Process "GoogleDrive*" | Stop-Process}
