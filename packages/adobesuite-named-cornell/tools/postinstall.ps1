8e0ee59d569755e851cbfd80881889e84c20e641
lb30 <39345501+lb30@users.noreply.github.com>
2022-01-28 14:26:42 -0500

    turnoffupdater

--- packages/adobesuite-named-cornell/tools/postinstall.ps1
+++ packages/adobesuite-named-cornell/tools/postinstall.ps1
@@ -5,3 +5,10 @@ $INSTALL_DIR = Join-Path $PSScriptRoot 'installer'
 
 # Fix Updated Browser Warnings
 Start-Process -filepath $env:windir\regedit.exe -Argumentlist "/s $INSTALL_DIR\adobe.reg"
+
+# Remove Adobe Updater Scheduled Task
+SCHTASKS /DELETE /TN "AdobeCCAppRegistration-AdobeNotificationClient_2.0.1.8_x86_enpm4xejd91yc" /F
+
+# Stop and disable updater services
+Stop-Service AdobeUpdateService -Force
+Set-Service AdobeUpdateService -StartupType Disabled
