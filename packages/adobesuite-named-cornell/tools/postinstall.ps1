b2be2f078a808a1ec1421357c831f7939285de51
lb30 <39345501+lb30@users.noreply.github.com>
2022-02-01 10:52:30 -0500

    Suppressadobeupdates (#417)
    * turnoffupdater
    
    * Update postinstall.ps1
    
    * Update postinstall.ps1
    
    * Update postinstall.ps1
    
    * Update postinstall.ps1

--- packages/adobesuite-named-cornell/tools/postinstall.ps1
+++ packages/adobesuite-named-cornell/tools/postinstall.ps1
@@ -5,3 +5,10 @@ $INSTALL_DIR = Join-Path $PSScriptRoot 'installer'
 
 # Fix Updated Browser Warnings
 Start-Process -filepath $env:windir\regedit.exe -Argumentlist "/s $INSTALL_DIR\adobe.reg"
+
+# Stop and disable updater services
+Stop-Service AdobeUpdateService -Force
+Set-Service AdobeUpdateService -StartupType Disabled
+
+#Remove Adobe Notifications
+Get-AppxPackage -AllUsers *AdobeNotificationClient* | Remove-AppxPackage -AllUsers
