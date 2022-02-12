1f2f97cf8b8804d013fbcfab747958352d35216a
kb599 <34450561+kb599@users.noreply.github.com>
2022-02-12 08:47:23 -0500

    Update postinstall.ps1

--- packages/googledrivefilestream-cornell/tools/postinstall.ps1
+++ packages/googledrivefilestream-cornell/tools/postinstall.ps1
@@ -8,7 +8,7 @@ Set-Service gupdate -StartupType Disabled
 Stop-Service gupdatem -Force
 Set-Service gupdatem -StartupType Disabled
 
-Rename GoogleUpdate
+#Rename GoogleUpdate
 Rename-Item -Path "c:\program files (x86)\google\update\GoogleUpdate.exe" -NewName "GoogleUpdateno.no" 
 
 # Rename .bat to .cmd
