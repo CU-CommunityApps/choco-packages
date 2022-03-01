fcba138fd527969228fe8b7d94125c8893ce86ae
Dan Klinger <dpk25@cornell.edu>
2022-03-01 17:12:02 -0500

    Populate env. variable with streaming instance values

--- packages/k2client-cornell/tools/startup.ps1
+++ packages/k2client-cornell/tools/startup.ps1
@@ -1,1 +1,5 @@
+# Populate variable with Streaming Instance specific values
+$env:AppStream_Combo = "$env:AppStream_UserName.$env:AppStream_Resource_Name"
+
+# Launch Sassafras client
 Start-Process "$env:windir\keyacc32.exe" -ArgumentList "-minimize"
