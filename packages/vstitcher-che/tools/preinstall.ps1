a47e563bce4ed26047aab74af6531b927b52a15b
lb30 <39345501+lb30@users.noreply.github.com>
2022-05-16 08:44:56 -0400

    preinstall stuff

--- packages/vstitcher-che/tools/preinstall.ps1
+++ packages/vstitcher-che/tools/preinstall.ps1
@@ -1,2 +1,10 @@
 # Runs before the choco package is installed
 
+# Installation dir location
+$INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
+
+Start-Process msiexec.exe -Wait -ArgumentList '/I %INSTALL_DIR%\SSCERuntime_x64-ENU.msi /quiet'
+Start-Process msiexec.exe -Wait -ArgumentList '/I %INSTALL_DIR%\vcredist.x86.msi /quiet'
+Start-Process msiexec.exe -Wait -ArgumentList '/I %INSTALL_DIR%\vcredist.x64.msi /quiet'
+Start-Process -FilePath "%INSTALL_DIR%\haspdinst.exe" -ArgumentList "-install"
+Start-Process msiexec.exe -Wait -ArgumentList '/I %INSTALL_DIR%\SSCERuntime_x86-ENU.msi /quiet'
