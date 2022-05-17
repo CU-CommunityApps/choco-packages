4c8dc6704a35c4ab65f28f32bbd65a67337bd970
lb30 <39345501+lb30@users.noreply.github.com>
2022-05-17 12:48:34 -0400

    Update preinstall.ps1

--- packages/vstitcher-che/tools/preinstall.ps1
+++ packages/vstitcher-che/tools/preinstall.ps1
@@ -7,4 +7,4 @@ Start-Process msiexec.exe -Wait -ArgumentList '/I $INSTALL_DIR\SSCERuntime_x64-E
 Start-Process msiexec.exe -Wait -ArgumentList '/I $INSTALL_DIR\SSCERuntime_x86-ENU.msi /quiet /norestart'
 Start-Process "$INSTALL_DIR\vcredist.x86.exe" -ArgumentList "/quiet /norestart" -Wait
 Start-Process "$INSTALL_DIR\vcredist_x64.exe" -ArgumentList "/quiet /norestart" -Wait
-Start-Process -FilePath '$INSTALL_DIR\haspdinst.exe' -ArgumentList '-install'
+#Start-Process "$INSTALL_DIR\haspdinst.exe" -ArgumentList "-install"
