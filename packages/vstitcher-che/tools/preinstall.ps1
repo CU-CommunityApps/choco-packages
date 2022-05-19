433b268d46128b542fcde37dfa17eb8b656a28e1
Dan Klinger <dpk25@cornell.edu>
2022-05-19 12:10:27 -0400

    Silent

--- packages/vstitcher-che/tools/preinstall.ps1
+++ packages/vstitcher-che/tools/preinstall.ps1
@@ -3,8 +3,8 @@
 # Installation dir location
 $INSTALL_DIR = Join-Path $PSScriptRoot 'installer'
 
-Start-Process "msiexec.exe" -Wait -ArgumentList "/i $INSTALL_DIR\SSCERuntime_x86-ENU.msi /qb /norestart"
-Start-Process "msiexec.exe" -Wait -ArgumentList "/i $INSTALL_DIR\SSCERuntime_x64-ENU.msi /qb /norestart"
+Start-Process "msiexec.exe" -Wait -ArgumentList "/i $INSTALL_DIR\SSCERuntime_x86-ENU.msi /qn /norestart"
+Start-Process "msiexec.exe" -Wait -ArgumentList "/i $INSTALL_DIR\SSCERuntime_x64-ENU.msi /qn /norestart"
 Start-Process "$INSTALL_DIR\vcredist.x86.exe" -ArgumentList "/quiet /norestart" -Wait
 Start-Process "$INSTALL_DIR\vcredist_x64.exe" -ArgumentList "/quiet /norestart" -Wait
 Start-Process "$INSTALL_DIR\haspdinst.exe" -ArgumentList "-install -nomsg"
