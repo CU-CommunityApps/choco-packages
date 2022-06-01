cfbba7ff34e08bc10a006e006bfd6cbbd289e8fa
lb30 <39345501+lb30@users.noreply.github.com>
2022-06-01 12:59:29 -0400

    Update preinstall.ps1

--- packages/sketchup-che/tools/preinstall.ps1
+++ packages/sketchup-che/tools/preinstall.ps1
@@ -1,5 +1,5 @@
 # Runs before the choco package is installed
 
 # Install prereq's
-Start-Process "$INSTALL_DIR\VC_redistx64.exe" -ArgumentList "/quiet /norestart" -Wait
-Start-Process "$INSTALL_DIR\VC_redistx86.exe" -ArgumentList "/quiet /norestart" -Wait
+Start-Process "$INSTALL_DIR\VC_redist.x64.exe" -ArgumentList "/quiet /norestart" -Wait
+Start-Process "$INSTALL_DIR\VC_redist.x86.exe" -ArgumentList "/quiet /norestart" -Wait
