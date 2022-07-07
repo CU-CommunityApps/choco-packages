13a1a9627cd5a1209170e9beb074566c1d4b9aaa
lb30 <39345501+lb30@users.noreply.github.com>
2022-07-07 14:22:14 -0400

    Update postinstall.ps1

--- packages/3ds-coecis/tools/postinstall.ps1
+++ packages/3ds-coecis/tools/postinstall.ps1
@@ -4,4 +4,4 @@
 $INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
 
 # File contains chars that chocolatey will not extract, copying manually before installation
-Copy-Item -Path "$PSScriptRoot\ArchVisionC1Tina2.rpc" -Destination "$INSTALL_DIR\3ds\x64\RX\PF32\CF32\AS32\Materials\2023\assetlibrary_base.fbm\RPCs\ArchVision_C1_Tina[2].rpc" -Force
+Copy-Item -Path "$PSScriptRoot\ArchVisionC1Tina2.rpc" -Destination "$INSTALL_DIR\Autodesk_3ds_Max_2023\Files\x64\RX\PF32\CF32\AS32\Materials\2023\assetlibrary_base.fbm\RPCs\ArchVision_C1_Tina[2].rpc" -Force
