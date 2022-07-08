86ed8ec27877e125ee9f204a409cb32a3ff23ede
lb30 <39345501+lb30@users.noreply.github.com>
2022-07-08 11:08:37 -0400

    Update preinstall.ps1

--- packages/3ds-coecis/tools/preinstall.ps1
+++ packages/3ds-coecis/tools/preinstall.ps1
@@ -1,6 +1,1 @@
 # Runs before the choco package is installed
-
-# Installation dir location
-$INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
-
-Start-Process Powershell -ArgumentList "-File $INSTALL_DIR\Deploy-Autodesk_3ds_Max_2023.ps1"
