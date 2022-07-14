9743f9c748557d9650e3a8f2a7177c4f42bed8c1
lb30 <39345501+lb30@users.noreply.github.com>
2022-07-14 12:53:59 -0400

    Update postinstall.ps1 (#482)

--- packages/autocad-cornell/tools/postinstall.ps1
+++ packages/autocad-cornell/tools/postinstall.ps1
@@ -61,5 +61,5 @@ $args = "register --pk $productKey --pv $productVer --cf $configFile --el US --l
 # Run the license helper
 Start-Process $exe -ArgumentList $args -Verbose
 
-$custExperience = "$env:ProgramFiles\Autodesk\AutoCAD 2022\AcExperience.arx"
+$custExperience = "$env:ProgramFiles\Autodesk\AutoCAD 2023\AcExperience.arx"
 If (Test-Path $custExperience){Remove-Item $custExperience -Force}
