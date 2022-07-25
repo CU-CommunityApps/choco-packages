225bd6208acc79e27c6b9193c3f016896f00a47f
lb30 <39345501+lb30@users.noreply.github.com>
2022-07-25 12:57:28 -0400

    Update preinstall.ps1

--- packages/ces-edupack-coecis/tools/preinstall.ps1
+++ packages/ces-edupack-coecis/tools/preinstall.ps1
@@ -8,5 +8,5 @@ Start-Process "$INSTALL_DIR\VC_redist.x64.exe" -ArgumentList "/quiet /norestart"
 Start-Process "$INSTALL_DIR\VC_redist.x86.exe" -ArgumentList "/quiet /norestart" -Wait
 Start-Process "$INSTALL_DIR\ReportViewer.exe" -ArgumentList "/quiet /norestart" -Wait
 
-If (!(Test-Path "$env:ALLUSERSPROFILE\Granta Design\CES EduPack 2020")){New-Item "$env:ALLUSERSPROFILE\Granta Design\CES EduPack 2020" -ItemType Directory -Force}
-Copy-Item "$env:INSTALL_DIR\license.lic" -Destination "$env:ALLUSERSPROFILE\Granta Design\CES EduPack 2020"
+If (!(Test-Path "$env:ALLUSERSPROFILE\Granta Design\CES EduPack 2022")){New-Item "$env:ALLUSERSPROFILE\Granta Design\CES EduPack 2022" -ItemType Directory -Force}
+Copy-Item "$env:INSTALL_DIR\license.lic" -Destination "$env:ALLUSERSPROFILE\Granta Design\CES EduPack 2022"
