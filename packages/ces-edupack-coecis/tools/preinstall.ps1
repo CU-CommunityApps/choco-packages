12f0aecc6e827653ff914a0ca64d4d98a8bbb448
lb30 <39345501+lb30@users.noreply.github.com>
2022-08-02 09:49:05 -0400

    Ces edupack coecis (#494)
    * version update
    
    * version update
    
    * Update preinstall.ps1
    
    * Update config.yml
    
    * Update preinstall.ps1
    
    * Update preinstall.ps1
    
    * Delete CES.png
    
    * Update config.yml
    
    * newicon
    
    * Update config.yml
    
    * Update config.yml
    
    * Update config.yml
    
    * Update config.yml
    
    * Update config.yml
    
    * Update preinstall.ps1
    
    * rename
    
    * rename
    
    * fixedicon
    
    * removing icon
    
    * different icon
    
    * Update config.yml
    
    * Delete ansysgrantaedu.png
    
    * Add files via upload
    
    * Delete ansysgrantaedu.png
    
    * Add files via upload
    
    Co-authored-by: Dan Klinger <dpk25@cornell.edu>

--- packages/ces-edupack-coecis/tools/preinstall.ps1
+++ packages/ces-edupack-coecis/tools/preinstall.ps1
@@ -1,12 +1,9 @@
 # Runs before the choco package is installed
 
-#Install .NET
-Start-Process "$INSTALL_DIR\ndp48-x86-x64-allos-enu.exe" -ArgumentList "/quiet /norestart" -Wait
-
 #Install other pre-reqs
 Start-Process "$INSTALL_DIR\VC_redist.x64.exe" -ArgumentList "/quiet /norestart" -Wait
 Start-Process "$INSTALL_DIR\VC_redist.x86.exe" -ArgumentList "/quiet /norestart" -Wait
 Start-Process "$INSTALL_DIR\ReportViewer.exe" -ArgumentList "/quiet /norestart" -Wait
 
-If (!(Test-Path "$env:ALLUSERSPROFILE\Granta Design\CES EduPack 2020")){New-Item "$env:ALLUSERSPROFILE\Granta Design\CES EduPack 2020" -ItemType Directory -Force}
-Copy-Item "$env:INSTALL_DIR\license.lic" -Destination "$env:ALLUSERSPROFILE\Granta Design\CES EduPack 2020"
+If (!(Test-Path "$env:ALLUSERSPROFILE\Granta Design\CES EduPack 2022")){New-Item "$env:ALLUSERSPROFILE\Granta Design\CES EduPack 2022" -ItemType Directory -Force}
+Copy-Item "$env:INSTALL_DIR\license.lic" -Destination "$env:ALLUSERSPROFILE\Granta Design\CES EduPack 2022"
