22240fb427cb9dd505fe980c0395dfdfe3bcef39
lb30 <39345501+lb30@users.noreply.github.com>
2022-08-05 22:03:28 -0400

    Update activation.ps1

--- packages/tableau-cis/tools/activation.ps1
+++ packages/tableau-cis/tools/activation.ps1
@@ -1,3 +1,3 @@
 # Activate product
 Write-Output "Activating..."
-Start-Process "$env:ProgramFiles\Tableau\Tableau 2021.2\bin\tableau.exe" -ArgumentList "-activate $env:STATIC_SYSTEM_TABLEAU_ACTIVATE_KEY" -Wait
+Start-Process "$env:ProgramFiles\Tableau\Tableau 2022.2\bin\tableau.exe" -ArgumentList "-activate $env:STATIC_SYSTEM_TABLEAU_ACTIVATE_KEY" -Wait
