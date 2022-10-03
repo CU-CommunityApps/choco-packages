bfb70e31bee005fb4054d307ba26a102b4980f5c
Dan Barton <drb294@cornell.edu>
2022-10-03 11:09:31 -0400

    updated yaml and dummy updater post install ps script

--- packages/onedrive-cornell/tools/postinstall.ps1
+++ packages/onedrive-cornell/tools/postinstall.ps1
@@ -1,2 +1,4 @@
-# Runs after the choco package is installed
+#copies the dummy updater.exe
+ Copy-Item "%INSTALL_DIR%\OneDriveStandaloneUpdater.exe" -Destination "%PROGRAMFILES%\Microsoft OneDrive\" -Force 
+
 
