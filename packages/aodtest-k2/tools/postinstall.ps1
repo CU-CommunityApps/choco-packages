ea956a548dee1aa2c6220f531d729691066d7ccc
Lori Beebe <lb30@cornell.edu>
2023-01-04 09:01:28 -0500

    aodtestk2

---
+++ packages/aodtest-k2/tools/postinstall.ps1
@@ -0,0 +1,12 @@
+# Runs after the choco package is installed
+
+# Stop any running processes
+Stop-Process -Name keyacc32 -Force -ErrorAction SilentlyContinue
+
+# Update user field as outlined by Support
+New-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\KeyAccess\Settings\logon' -Name 'user' -Value '%AoD_Combo%' -PropertyType string -Force
+
+# Remove any system specific values prior to sysprep
+Remove-Item -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\KeyAccess\Settings\pref' -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path 'HKLM:\SYSTEM\CurrentControlSet\Sevices\KeyAccess\Settings\settings' -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "$env:ALLUSERSPROFILE\KeyAccess" -Recurse -Force -ErrorAction SilentlyContinue
