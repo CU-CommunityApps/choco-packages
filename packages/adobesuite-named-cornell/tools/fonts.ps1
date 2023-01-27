849b9aa13b738e660a7ac244e7ed44f8319f9ebb
unknown <ImageBuilderAdmin@6e0b2696007d431.cornell.edu>
2023-01-27 15:58:19 -0500

    Adding fonts and fonts script

---
+++ packages/adobesuite-named-cornell/tools/fonts.ps1
@@ -0,0 +1,9 @@
+﻿
+$SourceDir   = "$env:ALLUSERSPROFILE\chocolatey\lib\adobesuite-named-cornell\tools\Fonts\"
+$Destination = (New-Object -ComObject Shell.Application).Namespace(0x14)
+
+foreach ($FontItem in (Get-ChildItem -Path $SourceDir -Include '*.ttf','*.ttc','*.otf' -Recurse))  {  
+    
+	# Install each font in the Fonts folder in Tools Dir
+	$Destination.CopyHere($FontItem.FullName,0x10)
+}
