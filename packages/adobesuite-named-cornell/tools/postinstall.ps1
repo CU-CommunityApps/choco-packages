27209abcd4a0bd2da0968beeab1a69c2752c8b84
Dan Klinger <dpk25@cornell.edu>
2023-01-28 18:07:49 -0500

    Testing postinstall with new process

--- packages/adobesuite-named-cornell/tools/postinstall.ps1
+++ packages/adobesuite-named-cornell/tools/postinstall.ps1
@@ -16,3 +16,23 @@ Get-AppxPackage -AllUsers *AdobeNotificationClient* | Remove-AppxPackage -AllUse
 # Copy missing Photoshop 2022 settings and libraries
 # $path = "$env:SYSTEMDRIVE\Users\Default\AppData\Roaming\Adobe\Adobe Photoshop 2022\Adobe Photoshop 2022 Settings\"
 # cp "$TOOLS_DIR\Adobe Photoshop 2022 Settings" $path -Recurse
+
+$SourceDir   = "$PSScriptRoot\Fonts\"
+
+foreach ($fontItem in (Get-ChildItem -Path $SourceDir -Include '*.ttf','*.ttc','*.otf' -Recurse))  {  
+    
+	# Install each font in the Fonts folder in Tools Dir
+
+	# Copy fonts to C:\Windows\Fonts directory
+    If (!(Test-Path ("$($env:windir)\Fonts\" + $fontItem.Name))) {  
+        write-host "Copying font: $($fontItem.Name)"
+        Copy-Item -Path $fontItem.FullName -Destination ("$($env:windir)\Fonts\" + $fontItem.Name) -Force 
+    } else {  write-host "Font already exists: $fontItem" }
+
+    # Create corresponding registry entry to register the font
+    If (!(Get-ItemProperty -Name $fontItem -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" -ErrorAction SilentlyContinue)) {  
+        write-host "Registering font: $fontItem"
+        New-ItemProperty -Name $fontItem -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" -PropertyType string -Value $fontItem.Name -Force -ErrorAction SilentlyContinue | Out-Null  
+    } else {  write-host "Font already registered: $fontItem" }
+
+}
