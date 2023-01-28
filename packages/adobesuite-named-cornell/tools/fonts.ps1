48de94174de2bb59961911a7865b905cde5938d6
Dan Klinger <dpk25@cornell.edu>
2023-01-28 18:06:32 -0500

    Update font start script

--- packages/adobesuite-named-cornell/tools/fonts.ps1
+++ packages/adobesuite-named-cornell/tools/fonts.ps1
@@ -1,9 +1,19 @@
-﻿
 $SourceDir   = "$env:ALLUSERSPROFILE\chocolatey\lib\adobesuite-named-cornell\tools\Fonts\"
-$Destination = (New-Object -ComObject Shell.Application).Namespace(0x14)
 
-foreach ($FontItem in (Get-ChildItem -Path $SourceDir -Include '*.ttf','*.ttc','*.otf' -Recurse))  {  
+foreach ($fontItem in (Get-ChildItem -Path $SourceDir -Include '*.ttf','*.ttc','*.otf' -Recurse))  {  
     
-	# Install each font in the Fonts folder in Tools Dir
-	$Destination.CopyHere($FontItem.FullName,0x10)
+    # Install each font in the Fonts folder in Tools Dir
+
+    # Copy fonts to C:\Windows\Fonts directory
+    If (!(Test-Path ("$($env:windir)\Fonts\" + $fontItem.Name))) {  
+        write-host "Copying font: $($fontItem.Name)"
+        Copy-Item -Path $fontItem.FullName -Destination ("$($env:windir)\Fonts\" + $fontItem.Name) -Force 
+    } else {  write-host "Font already exists: $fontItem" }
+
+    # Create corresponding registry entry to register the font
+    If (!(Get-ItemProperty -Name $fontItem -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" -ErrorAction SilentlyContinue)) {  
+        write-host "Registering font: $fontItem"
+        New-ItemProperty -Name $fontItem -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" -PropertyType string -Value $fontItem.Name -Force -ErrorAction SilentlyContinue | Out-Null  
+    } else {  write-host "Font already registered: $fontItem" }
+
 }
