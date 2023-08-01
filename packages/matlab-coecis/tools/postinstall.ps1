45cbeec2f9e33b0852ebfd2f0adf61ad68cb9dd1
lb30 <39345501+lb30@users.noreply.github.com>
2023-08-01 10:53:30 -0400

    Matlab coecis (#599)
    * Update postinstall.ps1
    
    * Update config.yml
    
    * Update config.yml

--- packages/matlab-coecis/tools/postinstall.ps1
+++ packages/matlab-coecis/tools/postinstall.ps1
@@ -1,4 +1,4 @@
 # Runs after the choco package is installed
 $INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
 
-Copy-Item "$INSTALL_DIR\network.lic" "c:\Program Files\MATLAB\R2022a\licenses\network.lic"
+Copy-Item "$INSTALL_DIR\network.lic" "c:\Program Files\MATLAB\R2023a\licenses\network.lic"
