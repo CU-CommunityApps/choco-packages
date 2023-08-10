c5ecad93be3913f2ed569ce637d15dbd514b17eb
lb30 <39345501+lb30@users.noreply.github.com>
2023-08-10 12:01:07 -0400

    Update preinstall.ps1

--- packages/electronics-coecis/tools/preinstall.ps1
+++ packages/electronics-coecis/tools/preinstall.ps1
@@ -1,4 +1,4 @@
 # Runs before the choco package is installed
 
 $INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
-Start-Process "$INSTALL_DIR\RSM\setup.exe" -ArgumentList "/quiet /norestart" -Wait
+Start-Process "$INSTALL_DIR\RSM\setup.exe" -ArgumentList "/silent" -Wait
