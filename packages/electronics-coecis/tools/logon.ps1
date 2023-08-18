85863d1f4ecb525ec0e09019fa44d4038fe16a26
Dan Klinger <dpk25@cornell.edu>
2023-08-18 14:48:02 -0400

    Electronics coecis (#622)
    * Update config.yml
    
    * Update config.yml
    
    * Add files via upload
    
    * Update config.yml
    
    * Update and rename d4eac1d0625e4da_user.XML to 491561b832d649a_user.xml
    
    * Rename d4eac1d0625e4da.cfg to 491561b832d649a.cfg
    
    * Copy new version files
    
    * Update config.yml
    
    * Rename 491561b832d649a.cfg to host.cfg
    
    * Rename 491561b832d649a_user.xml to host_user.xml
    
    * Update host_user.xml
    
    * Create startup.ps1
    
    * Startup script to update files
    
    * Update host.cfg
    
    * Create logon.ps1
    
    * Update host_user.xml
    
    * Project directory update
    
    * Update logon.ps1
    
    * Update host_user.xml
    
    ---------
    
    Co-authored-by: lb30 <39345501+lb30@users.noreply.github.com>

---
+++ packages/electronics-coecis/tools/logon.ps1
@@ -0,0 +1,14 @@
+# Logon Script
+
+# Get hostname
+$hostname = $env:COMPUTERNAME.ToLower()
+
+# Config files
+$config_files = "$env:UserProfile\Documents\Ansoft\ElectronicsDesktop2023.2\config"
+
+# Update XML Contents as logged on user
+Do {
+    Write-Output "Waiting for $env:UserName home directory creation"
+}Until(Test-Path $config_files)
+
+(Get-Content "$($config_files)\$($hostname)_user.XML").replace("temp_username", "$env:UserName") | Set-Content "$($config_files)\$($hostname)_user.XML"
