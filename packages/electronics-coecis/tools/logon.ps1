b9d83adf1a74c2684ef73b43f9fa9d5087cc7fa7
Dan Klinger <dpk25@cornell.edu>
2023-08-19 12:04:41 -0400

    Update logon.ps1

--- packages/electronics-coecis/tools/logon.ps1
+++ packages/electronics-coecis/tools/logon.ps1
@@ -11,4 +11,9 @@ Do {
     Write-Output "Waiting for $env:UserName home directory creation"
 }Until(Test-Path $config_files)
 
+# Rename config files
+gci -Path $config_files "host*" | % { Rename-Item $_.FullName $_.FullName.Replace("host", $hostname) }
+
+# Update XML contents
+(Get-Content "$($config_files)\$($hostname)_user.XML").replace("replace_user", "$($hostname)_user") | Set-Content "$($config_files)\$($hostname)_user.XML"
 (Get-Content "$($config_files)\$($hostname)_user.XML").replace("temp_username", "$env:UserName") | Set-Content "$($config_files)\$($hostname)_user.XML"
