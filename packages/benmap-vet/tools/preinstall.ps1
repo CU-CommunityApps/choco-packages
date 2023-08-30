1c80d1641a4e683176f7466d8530e02d7632982a
lb30 <39345501+lb30@users.noreply.github.com>
2023-08-30 15:37:39 -0400

    Update preinstall.ps1

--- packages/benmap-vet/tools/preinstall.ps1
+++ packages/benmap-vet/tools/preinstall.ps1
@@ -4,4 +4,4 @@
 
 # Install prereq's
 Start-Process "$INSTALL_DIR\ndp48-x86-x64-allos-enu" -ArgumentList "/quiet /norestart" -Wait
-Start-Process "$INSTALL_DIR\VC_redistx64.exe" -ArgumentList "/quiet /norestart" -Wait
+Start-Process "$INSTALL_DIR\VC_redist.x64.exe" -ArgumentList "/quiet /norestart" -Wait
