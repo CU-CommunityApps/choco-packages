356a46ad3fc13c4f19f7a109785f581308d820d3
lb30 <39345501+lb30@users.noreply.github.com>
2023-12-19 12:50:44 -0500

    Update preinstall.ps1

--- packages/benmap-vet/tools/preinstall.ps1
+++ packages/benmap-vet/tools/preinstall.ps1
@@ -1,5 +1,5 @@
 # Runs before the choco package is installed
 
 # Install prereq's
-Start-Process "$INSTALL_DIR\ndp48-x86-x64-allos-enu" -ArgumentList "/quiet /norestart" -Wait
+Start-Process "$INSTALL_DIR\NDP481-x86-x64-AllOS-ENU.exe" -ArgumentList "/quiet /norestart" -Wait
 Start-Process "$INSTALL_DIR\VC_redistx64.exe" -ArgumentList "/quiet /norestart" -Wait
