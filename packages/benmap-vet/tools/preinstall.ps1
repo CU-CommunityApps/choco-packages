b160c48ce2a3d0ea2271dcf0ceaad6f3419f9a75
lb30 <39345501+lb30@users.noreply.github.com>
2023-12-22 13:51:19 -0500

    Benmap vet (#635)
    * Update config.yml
    
    * Update preinstall.ps1
    
    * Update preinstall.ps1
    
    * Update config.yml
    
    * Update preinstall.ps1
    
    * Update config.yml
    
    * Update config.yml
    
    * Update config.yml
    
    * Update config.yml
    
    * Update config.yml
    
    * Update config.yml
    
    * Update preinstall.ps1
    
    * Update preinstall.ps1
    
    * Update config.yml
    
    * Update preinstall.ps1
    
    * Update preinstall.ps1
    
    * Update preinstall.ps1
    
    * Update config.yml
    
    * Update config.yml

--- packages/benmap-vet/tools/preinstall.ps1
+++ packages/benmap-vet/tools/preinstall.ps1
@@ -1,5 +1,10 @@
-# Runs before the choco package is installed
+# Runs before the choco package is installed 
 
 # Install prereq's
-Start-Process "$INSTALL_DIR\ndp48-x86-x64-allos-enu" -ArgumentList "/quiet /norestart" -Wait
+Start-Process "$INSTALL_DIR\NDP481-x86-x64-AllOS-ENU.exe" -ArgumentList "/quiet /norestart" -Wait
 Start-Process "$INSTALL_DIR\VC_redistx64.exe" -ArgumentList "/quiet /norestart" -Wait
+
+#Install Databases
+Expand-Archive -Path "$INSTALL_DIR\BENMAP50.zip" -DestinationPath "$SYSTEMDRIVE\Users\Default\AppData\Local\BenMAP-CE\Database"
+Expand-Archive -Path "$INSTALL_DIR\BENMAP50_GBD.zip" -DestinationPath "$SYSTEMDRIVE\Users\Default\AppData\Local\BenMAP-CE\Database"
+Expand-Archive -Path "$INSTALL_DIR\POPSIMDB.zip" -DestinationPath "$SYSTEMDRIVE\Users\Default\AppData\Local\BenMAP-CE\Database"
