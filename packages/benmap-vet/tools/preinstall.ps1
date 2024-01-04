92f499bb66ca17f713542268aff4257cb10c0805
Dan Klinger <dpk25@cornell.edu>
2024-01-04 16:00:19 -0500

    Benmap (#639)
    * Move to file copy vs. unzip
    
    * Variable syntax fix
    
    * File copy does not work as .exe's must exist for proper installation
    
    * Reverting...
    
    * Update config.yml
    
    ---------
    
    Co-authored-by: unknown <ImageBuilderAdmin@43e72eb9106144e.cornell.edu>

--- packages/benmap-vet/tools/preinstall.ps1
+++ packages/benmap-vet/tools/preinstall.ps1
@@ -1,7 +1,6 @@
 # Runs before the choco package is installed 
 
 # Install prereq's
-Start-Process "$INSTALL_DIR\NDP481-x86-x64-AllOS-ENU.exe" -ArgumentList "/quiet /norestart" -Wait
 Start-Process "$INSTALL_DIR\VC_redistx64.exe" -ArgumentList "/quiet /norestart" -Wait
 
 #Install Databases
