09b47c70aeb6cfe48a71eb854f4d56dcdf3a660f
Dan Klinger <dpk25@cornell.edu>
2024-01-09 18:00:21 -0500

    Update preinstall.ps1

--- packages/benmap-vet/tools/preinstall.ps1
+++ packages/benmap-vet/tools/preinstall.ps1
@@ -1,7 +1,7 @@
 # Runs before the choco package is installed 
 
 # Install prereq's
-Start-Process "$INSTALL_DIR\VC_redistx64.exe" -ArgumentList "/quiet /norestart" -Wait
+# Start-Process "$INSTALL_DIR\VC_redistx64.exe" -ArgumentList "/quiet /norestart" -Wait
 
 #Install Databases
 # Expand-Archive -Path "$INSTALL_DIR\BENMAP50.zip" -DestinationPath "$SYSTEMDRIVE\Users\Default\AppData\Local\BenMAP-CE\Database"
