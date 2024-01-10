3e0abe1be075e7e6991be2b12eef3447c9a49d7c
Dan Klinger <dpk25@cornell.edu>
2024-01-09 22:55:27 -0500

    Benmap (#644)
    * Move to file copy vs. unzip
    
    * Variable syntax fix
    
    * File copy does not work as .exe's must exist for proper installation
    
    * Reverting...
    
    * Update config.yml
    
    * No transform test
    
    * Update preinstall.ps1
    
    * Update preinstall.ps1
    
    * Update config.yml
    
    * Update config.yml
    
    * New transform
    
    ---------
    
    Co-authored-by: unknown <ImageBuilderAdmin@43e72eb9106144e.cornell.edu>

--- packages/benmap-vet/tools/preinstall.ps1
+++ packages/benmap-vet/tools/preinstall.ps1
@@ -1,9 +1,9 @@
 # Runs before the choco package is installed 
 
 # Install prereq's
-Start-Process "$INSTALL_DIR\VC_redistx64.exe" -ArgumentList "/quiet /norestart" -Wait
+# Start-Process "$INSTALL_DIR\VC_redistx64.exe" -ArgumentList "/quiet /norestart" -Wait
 
 #Install Databases
-Expand-Archive -Path "$INSTALL_DIR\BENMAP50.zip" -DestinationPath "$SYSTEMDRIVE\Users\Default\AppData\Local\BenMAP-CE\Database"
-Expand-Archive -Path "$INSTALL_DIR\BENMAP50_GBD.zip" -DestinationPath "$SYSTEMDRIVE\Users\Default\AppData\Local\BenMAP-CE\Database"
-Expand-Archive -Path "$INSTALL_DIR\POPSIMDB.zip" -DestinationPath "$SYSTEMDRIVE\Users\Default\AppData\Local\BenMAP-CE\Database"
+# Expand-Archive -Path "$INSTALL_DIR\BENMAP50.zip" -DestinationPath "$SYSTEMDRIVE\Users\Default\AppData\Local\BenMAP-CE\Database"
+# Expand-Archive -Path "$INSTALL_DIR\BENMAP50_GBD.zip" -DestinationPath "$SYSTEMDRIVE\Users\Default\AppData\Local\BenMAP-CE\Database"
+# Expand-Archive -Path "$INSTALL_DIR\POPSIMDB.zip" -DestinationPath "$SYSTEMDRIVE\Users\Default\AppData\Local\BenMAP-CE\Database"
