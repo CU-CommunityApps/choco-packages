61bab63b5a0bbb51afb0d09b8322c3535d073239
Dan Barton <drb294@cornell.edu>
2024-05-08 12:07:04 -0400

    edited tools_dir

--- packages/base-cornellstream/tools/preinstall.ps1
+++ packages/base-cornellstream/tools/preinstall.ps1
@@ -1,14 +1,16 @@
 # Runs before the choco package is installed
 
+$TOOLS_DIR = $PSScriptRoot
+
 #Chrome download
 
-Start-BitsTransfer "https://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise64.msi" -Destination "%TOOLS_DIR%\GoogleChromeStandaloneEnterprise64.msi"
+Start-BitsTransfer "https://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise64.msi" -Destination "$TOOLS_DIR\GoogleChromeStandaloneEnterprise64.msi"
 
 ## 7 Zip latest download link
 
 $7Zip = "https://www.7-zip.org/$((Invoke-WebRequest https://www.7-zip.org/download.html | Select-Object -ExpandProperty Links | Where-Object -Property href -like "*-x64.msi")[0].href)"
 
-Start-BitsTransfer "$7Zip" -Destination "%TOOLS_DIR%\7zip.msi"
+Start-BitsTransfer "$7Zip" -Destination "$TOOLS_DIR\7zip.msi"
 
 ## Notepad ++ Download
 
@@ -16,7 +18,7 @@ $linkPath = ((Invoke-WebRequest -URI https://notepad-plus-plus.org -UseBasicPars
 $downloadurl = "https://notepad-plus-plus.org$linkpath"
 $NotePadPlusPlus = ((Invoke-WebRequest -URI $downloadurl -UseBasicParsing) | Select-Object -ExpandProperty links | Where-Object -Property href -like "*npp.*.installer.x64.exe").href | Select-Object -Index 0
 
-Start-BitsTransfer "$NotePadPlusPlus" -Destination "%TOOLS_DIR%\NotePadPlusPlus.exe"
+Start-BitsTransfer "$NotePadPlusPlus" -Destination "$TOOLS_DIR\NotePadPlusPlus.exe"
 
 # Check OS Version
 $OSVersion = (get-itemproperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name ProductName).ProductName
