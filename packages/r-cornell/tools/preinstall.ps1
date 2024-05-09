da1f855ee750194993ecbc9018f81ad3531e3322
Dan Barton <drb294@cornell.edu>
2024-05-09 16:20:07 -0400

    added directory parameter

--- packages/r-cornell/tools/preinstall.ps1
+++ packages/r-cornell/tools/preinstall.ps1
@@ -5,21 +5,3 @@
 $url = "https://cloud.r-project.org//bin/windows/base/$((Invoke-WebRequest https://cloud.r-project.org/bin/windows/base/ | Select -ExpandProperty Links | Where innerText -like "Download R*for Windows")[0].href)"
 
 Start-BitsTransfer $url -Destination "$PSScriptRoot\RInstall.exe"
-
-#R installs to a version specific directory, so we need to find the version information of the .exe
-
-$version = ((Get-ItemProperty "$PSScriptRoot\RInstall.exe").VersionInfo.ProductVersion).Trim()
-
-# Now we go up one level to dynamically update the config.yaml to have the correct version
-
-$yamlFilePath = "$PSScriptRoot\config.yml"
-
-# Read the content of the YAML file
-$fileContent = Get-Content -Path $yamlFilePath -Raw
-
-# Search for the word "#VERSION#" and replace it with "4.4.0"
-$newContent = $fileContent -replace "000.000.000", "$version"
-
-# Write the updated content back to the YAML file
-$newContent | Set-Content -Path $yamlFilePath
-
