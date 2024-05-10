42fdb023b059e85022035221edcb77415024a583
lb30 <39345501+lb30@users.noreply.github.com>
2024-05-10 10:12:01 -0400

    R cornell (#667)
    * dynamic R installer
    
    * fix config.yml version
    
    * one more fix
    
    * added directory parameter
    
    * version number fix
    
    ---------
    
    Co-authored-by: Dan Barton <drb294@cornell.edu>

--- packages/r-cornell/tools/preinstall.ps1
+++ packages/r-cornell/tools/preinstall.ps1
@@ -1,2 +1,7 @@
 # Runs before the choco package is installed
 
+#Download latest version of R
+
+$url = "https://cloud.r-project.org//bin/windows/base/$((Invoke-WebRequest https://cloud.r-project.org/bin/windows/base/ | Select -ExpandProperty Links | Where innerText -like "Download R*for Windows")[0].href)"
+
+Start-BitsTransfer $url -Destination "$PSScriptRoot\RInstall.exe"
