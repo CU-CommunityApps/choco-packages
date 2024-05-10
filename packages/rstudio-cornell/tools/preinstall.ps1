55a425a6258663c86138f603a5ebd1a84f854e8c
Dan Barton <drb294@cornell.edu>
2024-05-10 12:25:27 -0400

    Switched to direct file extraction to program files

--- packages/rstudio-cornell/tools/preinstall.ps1
+++ packages/rstudio-cornell/tools/preinstall.ps1
@@ -1,5 +1,7 @@
 # Runs before the choco package is installed
 
-$RStudio = ((Invoke-WebRequest -Uri "https://posit.co/download/rstudio-desktop/").Links | Where-Object -Property "href" -like "*windows/RStudio*").href[0]
+$RStudio = ((Invoke-WebRequest -Uri "https://posit.co/download/rstudio-desktop/").Links | Where-Object -Property "href" -like "*.zip").href
 
-Start-BitsTransfer "$RStudio" -Destination "$PSScriptRoot\RStudio.exe"
+Start-BitsTransfer "$RStudio" -Destination "$PSScriptRoot\RStudio.zip"
+
+Invoke-Expression "7z x `"$PSScriptRoot\RStudio.zip`" -y -bd -o`"C:\Program Files\RStudio`""
