c0e153f0072848fe9926da0f99285dfe46fff4ff
Dan Barton <drb294@cornell.edu>
2024-05-10 10:47:46 -0400

    dynamic RStudio installer

--- packages/rstudio-cornell/tools/preinstall.ps1
+++ packages/rstudio-cornell/tools/preinstall.ps1
@@ -1,2 +1,5 @@
 # Runs before the choco package is installed
 
+$RStudio = ((Invoke-WebRequest -Uri "https://posit.co/download/rstudio-desktop/").Links | Where-Object -Property "href" -like "*windows/RStudio*").href[0]
+
+Start-BitsTransfer "$RStudio" -Destination "$PSScriptRoot\RStudio.exe"
