954b20a9201c9629b14514e185f473fbe28ebd29
lb30 <39345501+lb30@users.noreply.github.com>
2024-05-21 10:25:38 -0400

    Rstudio cornell (#671)
    * dynamic RStudio installer
    
    * Switched to direct file extraction to program files
    
    ---------
    
    Co-authored-by: Dan Barton <drb294@cornell.edu>

--- packages/rstudio-cornell/tools/preinstall.ps1
+++ packages/rstudio-cornell/tools/preinstall.ps1
@@ -1,2 +1,7 @@
 # Runs before the choco package is installed
 
+$RStudio = ((Invoke-WebRequest -Uri "https://posit.co/download/rstudio-desktop/").Links | Where-Object -Property "href" -like "*.zip").href
+
+Start-BitsTransfer "$RStudio" -Destination "$PSScriptRoot\RStudio.zip"
+
+Invoke-Expression "7z x `"$PSScriptRoot\RStudio.zip`" -y -bd -o`"C:\Program Files\RStudio`""
