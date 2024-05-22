1f1da00d68f09b99cb213902deca2316c05b6f7a
lb30 <39345501+lb30@users.noreply.github.com>
2024-05-22 12:54:22 -0400

    Update postinstall.ps1

--- packages/matlab-ciser/tools/postinstall.ps1
+++ packages/matlab-ciser/tools/postinstall.ps1
@@ -1,5 +1,5 @@
 # Runs after the choco package is installed
 
-$INSTALL_DIR =  Join-Path $PSScriptRoot 'installer
+$INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
 
 Copy-Item "$env:INSTALL_DIR\network.lic" "c:\Program Files\MATLAB\R2024a\licenses\network.lic"
