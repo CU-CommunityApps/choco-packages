b3b61274ea05b62582645f14fd74e73932e353a1
Dan Barton <109614851+DRB294@users.noreply.github.com>
2024-05-31 12:47:26 -0400

    fixing invoke webrequest and usebasicparsing (#678)

--- packages/r-cornell/tools/preinstall.ps1
+++ packages/r-cornell/tools/preinstall.ps1
@@ -2,6 +2,6 @@
 
 #Download latest version of R
 
-$url = "https://cloud.r-project.org//bin/windows/base/$((Invoke-WebRequest https://cloud.r-project.org/bin/windows/base/ | Select -ExpandProperty Links | Where innerText -like "Download R*for Windows")[0].href)"
+$url = "https://cloud.r-project.org//bin/windows/base/$((Invoke-WebRequest -Uri "https://cloud.r-project.org/bin/windows/base/" -UseBasicParsing | Select-Object -ExpandProperty Links | Where-Object href -like "*win.exe")[0].href)" 
 
 Start-BitsTransfer $url -Destination "$PSScriptRoot\RInstall.exe"
