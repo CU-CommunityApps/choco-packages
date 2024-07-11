b50d30764be1c5f90eeb5701be5766432b6a826e
lb30 <39345501+lb30@users.noreply.github.com>
2024-07-11 08:45:29 -0400

    Qgis cornell (#700)
    * Update preinstall.ps1
    
    * Update config.yml
    
    * Update preinstall.ps1
    
    * Update preinstall.ps1
    
    * Update config.yml
    
    * Update config.yml
    
    * Update config.yml
    
    * Update config.yml
    
    * Update config.yml
    
    * Update config.yml
    
    * Update config.yml
    
    * Update config.yml
    
    * Update config.yml
    
    * Update config.yml
    
    * Update preinstall.ps1
    
    * for app catalog
    
    * Update config.yml
    
    * Update config.yml
    
    * Update config.yml

--- packages/qgis-cornell/tools/preinstall.ps1
+++ packages/qgis-cornell/tools/preinstall.ps1
@@ -1,2 +1,10 @@
 # Runs before the choco package is installed
 
+#Download latest version of QGIS
+$data = Invoke-WebRequest -Uri "https://www.qgis.org/en/site/forusers/download.html" -UseBasicParsing 
+
+$filterdData = $data.Links | Where-Object -Property href -like "*.msi"
+
+$downloadLink = $filterdData[0].href
+
+Start-BitsTransfer -Source $downloadLink -Destination "$PSScriptRoot\qgis.msi"
