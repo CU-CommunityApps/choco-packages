c807ab1a232b87320b5cfb439db7c9094ec43064
lb30 <39345501+lb30@users.noreply.github.com>
2024-07-22 13:01:03 -0400

    Update postinstall.ps1 (#706)

--- packages/electronics-coecis/tools/postinstall.ps1
+++ packages/electronics-coecis/tools/postinstall.ps1
@@ -1,2 +1,6 @@
 # Runs after the choco package is installed
 
+Remove-Item "c:\users*\public\desktop\Ansys Electronics Desktop.lnk"
+Remove-Item "c:\users*\public\desktop\Ansys Nuhertz FilterSolutions.lnk"
+Remove-Item "c:\users*\public\desktop\Ansys SIwave.lnk"
+Remove-Item "c:\users*\public\desktop\Ansys Twin Builder.lnk"
