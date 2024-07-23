a327d203baa5b15b31d5d867681efb7af2ac5169
lb30 <39345501+lb30@users.noreply.github.com>
2024-07-23 11:09:18 -0400

    Update postinstall.ps1

--- packages/solidworks-coecis/tools/postinstall.ps1
+++ packages/solidworks-coecis/tools/postinstall.ps1
@@ -1,10 +1,4 @@
 # Runs after the choco package is installed
 
 #Remove Solidworks Shortcuts
-Remove-Item "c:\users*\public\desktop\SOLIDWORKS 2024.lnk"
-Remove-Item "c:\users*\public\desktop\SOLIDWORKS Composer 2024.lnk"
-Remove-Item "c:\users*\public\desktop\SOLIDWORKS Composer Player 2024.lnk"
-Remove-Item "c:\users*\public\desktop\SOLIDWORKS Composer Sync 2024.lnk"
-Remove-Item "c:\users*\public\desktop\SOLIDWORKS Visualize 2024.lnk"
-Remove-Item "c:\users*\public\desktop\eDrawings 2024 x64 Edition.lnk"
-Remove-Item "c:\users*\public\desktop\SOLIDWORKS 2024.lnk"
+Remove-Item "c:\users\public\desktop\*.lnk"
