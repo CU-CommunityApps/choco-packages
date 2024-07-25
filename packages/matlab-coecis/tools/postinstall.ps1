c1d17b3eeb95aeb611cab7d6c336119c1aa8b4a7
lb30 <39345501+lb30@users.noreply.github.com>
2024-07-25 14:59:29 -0400

    Update postinstall.ps1 (#711)

--- packages/matlab-coecis/tools/postinstall.ps1
+++ packages/matlab-coecis/tools/postinstall.ps1
@@ -2,3 +2,6 @@
 $INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
 
 Copy-Item "$INSTALL_DIR\network.lic" "c:\Program Files\MATLAB\R2024a\licenses\network.lic"
+
+#Remove Matlab Shortcut
+Remove-Item "c:\users\public\desktop\MATLAB R2024a.lnk"
