8f43549c15789189d18741e987e906fd0ca14c8e
lb30 <39345501+lb30@users.noreply.github.com>
2024-08-07 11:53:19 -0400

    Update startup.ps1

--- packages/autocad-cornell/tools/startup.ps1
+++ packages/autocad-cornell/tools/startup.ps1
@@ -1,2 +1,2 @@
-New-Item -Path "HKUD:\Software\Autodesk" -Name "ODIS" -Force
-New-ItemProperty -Path "HKUD:\Software\Autodesk\ODIS" -Name "DisableManualUpdateInstall" -Value 1 -PropertyType DWORD -Force
+#New-Item -Path "HKUD:\Software\Autodesk" -Name "ODIS" -Force
+#New-ItemProperty -Path "HKUD:\Software\Autodesk\ODIS" -Name "DisableManualUpdateInstall" -Value 1 -PropertyType DWORD -Force
