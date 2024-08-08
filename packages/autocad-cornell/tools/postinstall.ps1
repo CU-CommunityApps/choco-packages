f244b54d01198185371d274f65ebd5c042fc3666
lb30 <39345501+lb30@users.noreply.github.com>
2024-08-08 14:09:48 -0400

    Autocad cornell (#723)
    * Update startup.ps1
    
    * Update startup.ps1
    
    * Update startup.ps1
    
    * Update postinstall.ps1
    
    * Update config.yml
    
    * Update config.yml
    
    * Update startup.ps1
    
    * Update postinstall.ps1

--- packages/autocad-cornell/tools/postinstall.ps1
+++ packages/autocad-cornell/tools/postinstall.ps1
@@ -2,3 +2,6 @@
 
 #Remove autocad desktop shortcut
 Remove-Item "c:\users\public\desktop\AutoCAD 2025 - English.lnk"
+
+New-Item -Path "HKCU:\Software\Autodesk" -Name "ODIS" -Force
+New-ItemProperty -Path "HKCU:\Software\Autodesk\ODIS" -Name "DisableManualUpdateInstall" -Value 1 -PropertyType DWORD -Force
--- packages/autocad-cornell/tools/startup.ps1
+++ packages/autocad-cornell/tools/startup.ps1
@@ -1,1 +1,2 @@
-New-ItemProperty -Path "HKCU:\Software\Autodesk\ODIS" -Name "DisableManualUpdateInstall" -Value 1 -PropertyType DWORD
+New-Item -Path "HKCU:\Software\Autodesk" -Name "ODIS" -Force
+New-ItemProperty -Path "HKCU:\Software\Autodesk\ODIS" -Name "DisableManualUpdateInstall" -Value 1 -PropertyType DWORD -Force
