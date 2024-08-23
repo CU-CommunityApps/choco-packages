a6956d02023e533aa963974e122d56fa78d2b108
lb30 <39345501+lb30@users.noreply.github.com>
2024-08-23 12:28:44 -0400

    Spss ciser (#732)
    * Update postinstall.ps1
    
    * Update postinstall.ps1
    
    * Update postinstall.ps1

--- packages/spss-ciser/tools/postinstall.ps1
+++ packages/spss-ciser/tools/postinstall.ps1
@@ -1,4 +1,3 @@
 # Runs after the choco package is installed
-# Runs after the choco package is installed
-start-process "$env:programfiles\IBM\SPSS\Statistics\26\licenseactivator.exe" -ArgumentList "$env:SPSS_LICENSE_KEY"
+start-process "$env:programfiles\IBM\SPSS\Statistics\26\licenseactivator.exe" -ArgumentList "$env:SPSS_LICENSE_KEY" -wait
 
