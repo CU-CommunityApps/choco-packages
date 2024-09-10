ea5b864356b95d0963c797c28981afa72d8e4193
lb30 <39345501+lb30@users.noreply.github.com>
2024-09-10 11:53:16 -0400

    Update preinstall.ps1

--- packages/arcgispro-aap/tools/preinstall.ps1
+++ packages/arcgispro-aap/tools/preinstall.ps1
@@ -6,3 +6,4 @@ $INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
 Start-Process "$INSTALL_DIR\VC_redist.x64.exe" -ArgumentList "/quiet /norestart" -Wait
 Start-Process "$INSTALL_DIR\VC_redist.x86.exe" -ArgumentList "/quiet /norestart" -Wait
 Start-Process "$INSTALL_DIR\windowsdesktop-runtime-8.0.8-win-x64.exe" -ArgumentList "/quiet /norestart" -Wait
+Start-Process "$INSTALL_DIR\MicrosoftEdgeWebView2RuntimeInstallerX64.exe" -ArgumentList "/silent /install" -Wait
