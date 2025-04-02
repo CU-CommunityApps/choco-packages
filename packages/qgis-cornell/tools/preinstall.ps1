7866378359fafb6dbb236266e968926aa9f80427
lb30 <39345501+lb30@users.noreply.github.com>
2025-04-02 08:29:50 -0400

    Update preinstall.ps1 (#744)
    Updated qgis auto download preinstall script
    
    Co-authored-by: Dan Barton <109614851+DRB294@users.noreply.github.com>

--- packages/qgis-cornell/tools/preinstall.ps1
+++ packages/qgis-cornell/tools/preinstall.ps1
@@ -3,8 +3,14 @@
 #Download latest version of QGIS
 $data = Invoke-WebRequest -Uri "https://www.qgis.org/download/" -UseBasicParsing
  
-$filterdData = $data.Links | Where-Object -Property outerHTML -like "*Latest Version for Windows*"
+$filteredData = $data.Links | Where-Object -Property outerHTML -like "*Latest Version for Windows*"
+
+if ($filteredData.count -gt 1) {
+     $link = $filteredData[0].href }
+else {
+     $link = $filteredData.href
+}
  
-$downloadLink = "https://www.qgis.org" + $filterdData.href
+$downloadLink = "https://www.qgis.org" + $link
  
 Start-BitsTransfer -Source $downloadLink -Destination "$PSScriptRoot\qgis.msi"
