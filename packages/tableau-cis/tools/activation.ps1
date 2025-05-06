ec15531665f26845c14b215f7a0be3136fc341b2
lb30 <39345501+lb30@users.noreply.github.com>
2025-05-06 11:39:02 -0400

    Tableau cis (#745)
    * Update config.yml
    
    * Update Tableau_Activation.xml
    
    * Update Tableau_Prereqs.xml
    
    * Update activation.ps1
    
    * Update shutdown.ps1

--- packages/tableau-cis/tools/activation.ps1
+++ packages/tableau-cis/tools/activation.ps1
@@ -1,3 +1,3 @@
 # Activate product
 Write-Output "Activating..."
-Start-Process "$env:ProgramFiles\Tableau\Tableau 2024.1\bin\tableau.exe" -ArgumentList "-activate $env:STATIC_SYSTEM_TABLEAU_ACTIVATE_KEY" -Wait
+Start-Process "$env:ProgramFiles\Tableau\Tableau 2025.1\bin\tableau.exe" -ArgumentList "-activate $env:STATIC_SYSTEM_TABLEAU_ACTIVATE_KEY" -Wait
