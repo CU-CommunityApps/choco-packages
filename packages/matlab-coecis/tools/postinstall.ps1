73116295da2d0fe4e95c8d2321b334814bfe2390
lb30 <39345501+lb30@users.noreply.github.com>
2025-06-05 13:38:48 -0400

    Update postinstall.ps1

--- packages/matlab-coecis/tools/postinstall.ps1
+++ packages/matlab-coecis/tools/postinstall.ps1
@@ -1,7 +1,7 @@
 # Runs after the choco package is installed
 $INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
 
-Copy-Item "$INSTALL_DIR\network.lic" "c:\Program Files\MATLAB\R2024a\licenses\network.lic"
+Copy-Item "$INSTALL_DIR\network.lic" "c:\Program Files\MATLAB\R2025a\licenses\network.lic"
 
 #Remove Matlab Shortcut
-Remove-Item "c:\users\public\desktop\MATLAB R2024a.lnk"
+Remove-Item "c:\users\public\desktop\MATLAB R2025a.lnk"
