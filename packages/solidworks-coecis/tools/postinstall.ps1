f80dc4da2b5f998e409540131abf21fb2afad3e4
lb30 <39345501+lb30@users.noreply.github.com>
2025-06-13 13:16:50 -0400

    Solidworks coecis (#752)
    * Update config.yml
    
    * Update postinstall.ps1
    
    * Update solidworks-accept-eula.ps1
    
    * Update config.yml
    
    * Update solidworks-accept-eula.ps1
    
    * Update solidworks-accept-eula.ps1
    
    * Update solidworks-accept-eula.ps1
    
    * Update solidworks-accept-eula.ps1

--- packages/solidworks-coecis/tools/postinstall.ps1
+++ packages/solidworks-coecis/tools/postinstall.ps1
@@ -1,9 +1,9 @@
 # Runs after the choco package is installed
 
 #Remove Solidworks Shortcuts
-Remove-Item "c:\users\public\desktop\SOLIDWORKS 2024.lnk"
-Remove-Item "c:\users\public\desktop\SOLIDWORKS Composer 2024.lnk"
-Remove-Item "c:\users\public\desktop\SOLIDWORKS Composer Player 2024.lnk"
-Remove-Item "c:\users\public\desktop\SOLIDWORKS Composer Sync 2024.lnk"
-Remove-Item "c:\users\public\desktop\SOLIDWORKS Visualize 2024.lnk"
-Remove-Item "c:\users\public\desktop\eDrawings 2024 x64 Edition.lnk"
+Remove-Item "c:\users\public\desktop\SOLIDWORKS 2025.lnk"
+Remove-Item "c:\users\public\desktop\SOLIDWORKS Composer 2025.lnk"
+Remove-Item "c:\users\public\desktop\SOLIDWORKS Composer Player 2025.lnk"
+Remove-Item "c:\users\public\desktop\SOLIDWORKS Composer Sync 2025.lnk"
+Remove-Item "c:\users\public\desktop\SOLIDWORKS Visualize 2025.lnk"
+Remove-Item "c:\users\public\desktop\eDrawings 2025 x64 Edition.lnk"
