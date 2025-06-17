a14cb17525ed0c8cfc857ef4217927c11adc162f
lb30 <39345501+lb30@users.noreply.github.com>
2025-06-17 15:03:13 -0400

    Update postinstall.ps1

--- packages/autocad-cornell/tools/postinstall.ps1
+++ packages/autocad-cornell/tools/postinstall.ps1
@@ -1,4 +1,4 @@
 # Runs after the choco package is installed
 
 #Remove autocad desktop shortcut
-Remove-Item "c:\users\public\desktop\AutoCAD 2025 - English.lnk"
+Remove-Item "c:\users\public\desktop\AutoCAD 2026 - English.lnk"
