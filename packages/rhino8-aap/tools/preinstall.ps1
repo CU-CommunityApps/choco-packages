daf2b398f51821e6cf9e917679fde02547897c5f
lb30 <39345501+lb30@users.noreply.github.com>
2025-06-24 14:23:38 -0400

    Update preinstall.ps1

--- packages/rhino8-aap/tools/preinstall.ps1
+++ packages/rhino8-aap/tools/preinstall.ps1
@@ -17,7 +17,7 @@ ForEach ($ver in $vers) {
     If (!(gci -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall  | `
     gp | where {($_.DisplayName -match "Microsoft Visual C") -and (($_.DisplayVersion).StartsWith("$ver."))})){
 
-        If ($ver -eq "8"){write-output "$ver not found, installing...";Start-Process "$INSTALL_DIR\redist\VC2005_redist_x64.exe" -ArgumentList "/Q" -Wait}
+       # If ($ver -eq "8"){write-output "$ver not found, installing...";Start-Process "$INSTALL_DIR\redist\VC2005_redist_x64.exe" -ArgumentList "/Q" -Wait}
        # If ($ver -eq "10"){write-output "$ver not found, installing...";Start-Process "$INSTALL_DIR\redist\VC2010_redist_x64.exe" -ArgumentList "/q /norestart" -Wait}
         If ($ver -eq "12"){write-output "$ver not found, installing...";Start-Process "$INSTALL_DIR\redist\VC2013_redist_x64.exe" -ArgumentList "/install /quiet /norestart" -Wait}
         If ($ver -eq "14"){write-output "$ver not found, installing...";Start-Process "$INSTALL_DIR\redist\VC2015_redist_x64.exe" -ArgumentList "/q /norestart" -Wait}
