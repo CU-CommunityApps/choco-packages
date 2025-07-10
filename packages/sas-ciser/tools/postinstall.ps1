2224afb3b492955265ddd6a7a6c35392004d889e
lb30 <39345501+lb30@users.noreply.github.com>
2025-07-10 14:58:16 -0400

    Update postinstall.ps1

--- packages/sas-ciser/tools/postinstall.ps1
+++ packages/sas-ciser/tools/postinstall.ps1
@@ -1,4 +1,4 @@
 # Runs after the choco package is installed
 # Remove temporary installation files
 rm "$env:windir\Temp\sasresponse.properties" -Force
-rm "$env:windir\Temp\SAS94_9CZ2C1_70084773_Win_X64_Srv.txt" -Force
+rm "$env:windir\Temp\SAS94_9D27XV_70084773_Win_X64_Srv.txt" -Force
