4b2d3e1fceee30ace139a9c423dba3787528d5c8
lb30 <39345501+lb30@users.noreply.github.com>
2025-07-14 07:59:57 -0400

    Update postinstall.ps1

--- packages/autocad-cornell/tools/postinstall.ps1
+++ packages/autocad-cornell/tools/postinstall.ps1
@@ -2,3 +2,12 @@
 
 #Remove autocad desktop shortcut
 Remove-Item "c:\users\public\desktop\AutoCAD 2026 - English.lnk"
+
+#Set the Location to the registry
+Set-Location -Path "HKCU:\Software"
+
+#Create a new key
+Get-Item -Path 'HKCU:\Software' | New-Item -Name "Autodesk\ODIS" -Force
+
+#Create new items with values
+New-ItemProperty -Path "HKCU:\Software\Autodesk\ODIS" -Name "DisableManualUpdateInstall" -Value 1 -PropertyType DWORD -Force
