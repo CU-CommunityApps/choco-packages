e4ce0ffe724d421317860456595c246479fa828e
lb30 <39345501+lb30@users.noreply.github.com>
2025-07-15 15:39:39 -0400

    Autocad cornell2 (#768)
    * Update config.yml
    
    * Update preinstall.ps1
    
    Took out preinstall
    
    * Update config.yml
    
    * Update config.yml
    
    ---------
    
    Co-authored-by: Dan Barton <109614851+DRB294@users.noreply.github.com>

--- packages/autocad-cornell/tools/preinstall.ps1
+++ packages/autocad-cornell/tools/preinstall.ps1
@@ -1,6 +1,1 @@
-$TOOLS_DIR = $PSScriptRoot
 
-Start-BitsTransfer "https://emsfs.autodesk.com/utility/odis/1/installer/latest/AdODIS-installer.exe" -Destination "$TOOLS_DIR\AdODIS-installer.exe"
-# Runs before the choco package is installed
-
-Start-Process -FilePath "$TOOLS_DIR\AdODIS-installer.exe" -ArgumentList "--mode unattended --unattendedmodeui none" -Wait
