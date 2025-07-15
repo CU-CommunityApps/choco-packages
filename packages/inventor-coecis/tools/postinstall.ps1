215b30ea5a6a1a6caea41f5423f906c2f212167e
lb30 <39345501+lb30@users.noreply.github.com>
2025-07-15 11:56:25 -0400

    Update postinstall.ps1

--- packages/inventor-coecis/tools/postinstall.ps1
+++ packages/inventor-coecis/tools/postinstall.ps1
@@ -1,4 +1,4 @@
 # Runs after the choco package is installed
 
 #Remove Inventor Shortcuts
-Remove-Item "c:\users\public\desktop\Autodesk Inventor Professional 2026.lnk"
+#Remove-Item "c:\users\public\desktop\Autodesk Inventor Professional 2026.lnk"
