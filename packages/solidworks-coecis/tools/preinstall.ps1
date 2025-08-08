dd30947549d5473e9e3c165b7e5382a2ec10a67c
lb30 <39345501+lb30@users.noreply.github.com>
2025-08-08 14:06:24 -0400

    Update preinstall.ps1

--- packages/solidworks-coecis/tools/preinstall.ps1
+++ packages/solidworks-coecis/tools/preinstall.ps1
@@ -3,4 +3,7 @@
 # Installation dir location
 $INSTALL_DIR = Join-Path $PSScriptRoot 'installer'
 
+Copy-Item -Path "$INSTALL_DIR\startbaja.cmd" -Destination "C:\"
+Copy-Item -Path "$INSTALL_DIR\Baja_Vault" -Destination "C:\"
+
 gci -Path $INSTALL_DIR\Microsoft* -Filter *.exe -Recurse | % {Start-Process $_.FullName -ArgumentList "/q /norestart" -wait -Verbose}
