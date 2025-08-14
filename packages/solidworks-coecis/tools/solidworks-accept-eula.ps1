fc3f727cedff19c6005fd318aad7b156fe6b2575
lb30 <39345501+lb30@users.noreply.github.com>
2025-08-14 09:27:06 -0400

    Update solidworks-accept-eula.ps1

--- packages/solidworks-coecis/tools/solidworks-accept-eula.ps1
+++ packages/solidworks-coecis/tools/solidworks-accept-eula.ps1
@@ -4,9 +4,12 @@
 # SolidWorks 2025
 $solid = "Registry::\HKCU\Software\SolidWorks\SolidWorks 2025"
 If (!(Test-Path $solid\Security)){New-Item -Path $solid\Security -force}
+New-ItemProperty -Path $solid\Security -Name "EULA Accepted 2025 SP02 $env:ComputerName $env:UserName" -Value "Yes" -PropertyType "string" -force
+New-ItemProperty -Path $solid\Security -Name "EULA Accepted 2025 SP03 $env:ComputerName $env:UserName" -Value "Yes" -PropertyType "string" -force
 New-ItemProperty -Path $solid\Security -Name "EULA Accepted 2025 SP2.0 $env:ComputerName $env:UserName" -Value "Yes" -PropertyType "string" -force
-# If (!(Test-Path $solid\General\DontAskAgainOptions)){New-Item -Path $solid\General\DontAskAgainOptions -force}
-# New-ItemProperty -Path $solid\General\DontAskAgainOptions -Name "MSG_SHOW_WELCOME_DLG_AT_STARTUP%*^%$&9862" -Value "7" -PropertyType "DWORD" -force
+New-ItemProperty -Path $solid\Security -Name "EULA Accepted 2025 SP3.0 $env:ComputerName $env:UserName" -Value "Yes" -PropertyType "string" -force
+If (!(Test-Path $solid\General\DontAskAgainOptions)){New-Item -Path $solid\General\DontAskAgainOptions -force}
+New-ItemProperty -Path $solid\General\DontAskAgainOptions -Name "MSG_SHOW_WELCOME_DLG_AT_STARTUP%*^%$&9862" -Value "7" -PropertyType "DWORD" -force
 
 $composer = "Registry::\HKCU\Software\Dassault Systemes\Composer\7.12\Preferences"
 If (!(Test-Path $composer)){New-Item -Path $composer -force}
@@ -22,4 +25,4 @@ New-ItemProperty -Path $composer -Name "Sync.EulaAccepted" -Value "0" -PropertyT
 # eDrawings
 $eDraw = "Registry::\HKCU\Software\eDrawings\e2025\General"
 If (!(Test-Path $eDraw)){New-Item -Path $eDraw -force}
-New-ItemProperty -Path $eDraw -Name "ShowLicense 2025 sp02 $env:ComputerName $env:UserName" -Value "0" -PropertyType "DWORD" -Force
+New-ItemProperty -Path $eDraw -Name "ShowLicense 2025 sp03 $env:ComputerName $env:UserName" -Value "0" -PropertyType "DWORD" -Force
