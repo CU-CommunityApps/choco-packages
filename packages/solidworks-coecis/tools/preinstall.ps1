55c387527ae964392cc5cb174d6b7a511a8acc46
lb30 <39345501+lb30@users.noreply.github.com>
2025-08-21 11:20:30 -0400

    Update preinstall.ps1

--- packages/solidworks-coecis/tools/preinstall.ps1
+++ packages/solidworks-coecis/tools/preinstall.ps1
@@ -1,4 +1,6 @@
 # Runs before the choco package is installed
 $INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
 
+Copy-Item "$env:INSTALL_DIR\startbaja.cmd" "%PROGRAMFILES%\SOLIDWORKS PDM\startbaja.cmd"
+
 gci -Path $INSTALL_DIR\Microsoft* -Filter *.exe -Recurse | % {Start-Process $_.FullName -ArgumentList "/q /norestart" -wait -Verbose}
