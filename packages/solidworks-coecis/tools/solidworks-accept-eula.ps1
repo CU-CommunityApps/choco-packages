c60267b2d7933a31e0e63ed730abf09effe091c3
lb30 <39345501+lb30@users.noreply.github.com>
2025-08-21 10:48:36 -0400

    Update solidworks-accept-eula.ps1

--- packages/solidworks-coecis/tools/solidworks-accept-eula.ps1
+++ packages/solidworks-coecis/tools/solidworks-accept-eula.ps1
@@ -1,12 +1,12 @@
 # Startup script for suppressing SolidWorks prompts
 # Run as session script at login
 
-# SolidWorks 2023
+# SolidWorks 2025
 $solid = "Registry::\HKCU\Software\SolidWorks\SolidWorks 2025"
 If (!(Test-Path $solid\Security)){New-Item -Path $solid\Security -force}
 New-ItemProperty -Path $solid\Security -Name "EULA Accepted 2025 SP2.0 $env:ComputerName $env:UserName" -Value "Yes" -PropertyType "string" -force
-# If (!(Test-Path $solid\General\DontAskAgainOptions)){New-Item -Path $solid\General\DontAskAgainOptions -force}
-# New-ItemProperty -Path $solid\General\DontAskAgainOptions -Name "MSG_SHOW_WELCOME_DLG_AT_STARTUP%*^%$&9862" -Value "7" -PropertyType "DWORD" -force
+If (!(Test-Path $solid\General\DontAskAgainOptions)){New-Item -Path $solid\General\DontAskAgainOptions -force}
+New-ItemProperty -Path $solid\General\DontAskAgainOptions -Name "MSG_SHOW_WELCOME_DLG_AT_STARTUP%*^%$&9862" -Value "7" -PropertyType "DWORD" -force
 
 $composer = "Registry::\HKCU\Software\Dassault Systemes\Composer\7.12\Preferences"
 If (!(Test-Path $composer)){New-Item -Path $composer -force}
