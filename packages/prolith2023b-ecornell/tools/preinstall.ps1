a47eba015d0e2370550c5cdd29dcf5b0ea3764b3
Dan Klinger <dpk25@cornell.edu>
2025-08-22 10:06:21 -0400

    Remove pre-install script for PROLITHDatabase.DB

--- packages/prolith2023b-ecornell/tools/preinstall.ps1
+++ packages/prolith2023b-ecornell/tools/preinstall.ps1
@@ -1,7 +1,1 @@
 # Runs before the choco package is installed
-$destDir = Split-Path "$env:PROGRAMFILES\PROLITH_Toolkit\PROLITH 2023b\PROLITHDatabase.DB"  
-if (!(Test-Path $destDir)) {  
-    New-Item -ItemType Directory -Path $destDir -Force | Out-Null  
-}  
-  
-Copy-Item "$env:INSTALL_DIR\PROLITHDatabase.DB" "$env:PROGRAMFILES\PROLITH_Toolkit\PROLITH 2023b\PROLITHDatabase.DB" -Force
