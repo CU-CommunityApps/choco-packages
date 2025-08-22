d33909d901407a870657f42ae8771a5211d13712
lb30 <39345501+lb30@users.noreply.github.com>
2025-08-22 11:18:17 -0400

    Update preinstall.ps1

--- packages/solidworks-coecis/tools/preinstall.ps1
+++ packages/solidworks-coecis/tools/preinstall.ps1
@@ -1,7 +1,7 @@
 # Runs before the choco package is installed
 $INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
 
-Copy-Item "$env:INSTALL_DIR\startbaja.cmd" "%SYSTEMDRIVE%\"
-Copy-Item "$env:INSTALL_DIR\Baja_Vault.cvs" "%SYSTEMDRIVE%\"
+Copy-Item "$env:INSTALL_DIR\startbaja.cmd" "$env:SYSTEMDRIVE"
+Copy-Item "$env:INSTALL_DIR\Baja_Vault.cvs" "$env:SYSTEMDRIVE"
 
 gci -Path $INSTALL_DIR\Microsoft* -Filter *.exe -Recurse | % {Start-Process $_.FullName -ArgumentList "/q /norestart" -wait -Verbose}
