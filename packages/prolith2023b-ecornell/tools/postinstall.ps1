b0d9494cf459bd26be5e28d27d48d6e027e9152d
Dan Klinger <dpk25@cornell.edu>
2025-08-22 10:06:04 -0400

    Force copy of PROLITHDatabase.DB in postinstall script
    Ensure the database file is copied even if it exists.

--- packages/prolith2023b-ecornell/tools/postinstall.ps1
+++ packages/prolith2023b-ecornell/tools/postinstall.ps1
@@ -1,2 +1,2 @@
 # Runs after the choco package is installed
-# Copy-Item "$env:INSTALL_DIR\PROLITHDatabase.DB" "$env:PROGRAMFILES\PROLITH_Toolkit\PROLITH 2023b\PROLITHDatabase.DB"
+Copy-Item "$env:INSTALL_DIR\PROLITHDatabase.DB" "$env:PROGRAMFILES\PROLITH_Toolkit\PROLITH 2023b\PROLITHDatabase.DB" -Force
