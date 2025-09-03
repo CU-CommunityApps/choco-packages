842db15bc8d8cd575df9087b4de9a3cc80547312
lb30 <39345501+lb30@users.noreply.github.com>
2025-09-03 08:30:08 -0400

    Update preinstall.ps1

--- packages/solidworks-coecis/tools/preinstall.ps1
+++ packages/solidworks-coecis/tools/preinstall.ps1
@@ -1,4 +1,4 @@
 # Runs before the choco package is installed
 $INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
 
-gci -Path $INSTALL_DIR\Microsoft* -Filter *.exe -Recurse | % {Start-Process $_.FullName -ArgumentList "/q /norestart" -wait -Verbose}
+#gci -Path $INSTALL_DIR\Microsoft* -Filter *.exe -Recurse | % {Start-Process $_.FullName -ArgumentList "/q /norestart" -wait -Verbose}
