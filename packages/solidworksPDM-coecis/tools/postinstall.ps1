49de7a9cea11d852d1b3737ca3e20b2525b2be70
lb30 <39345501+lb30@users.noreply.github.com>
2025-09-03 14:05:16 -0400

    Update postinstall.ps1

--- packages/solidworksPDM-coecis/tools/postinstall.ps1
+++ packages/solidworksPDM-coecis/tools/postinstall.ps1
@@ -1,9 +1,2 @@
 # Runs after the choco package is installed
 
-#Remove Solidworks Shortcuts
-Remove-Item "c:\users\public\desktop\SOLIDWORKS 2025.lnk"
-Remove-Item "c:\users\public\desktop\SOLIDWORKS Composer 2025.lnk"
-Remove-Item "c:\users\public\desktop\SOLIDWORKS Composer Player 2025.lnk"
-Remove-Item "c:\users\public\desktop\SOLIDWORKS Composer Sync 2025.lnk"
-Remove-Item "c:\users\public\desktop\SOLIDWORKS Visualize 2025.lnk"
-Remove-Item "c:\users\public\desktop\eDrawings 2025 x64 Edition.lnk"
