f729a046febc054380cb5e150bf5f7f350ae15d7
lb30 <39345501+lb30@users.noreply.github.com>
2025-09-04 10:41:32 -0400

    Update preinstall.ps1

--- packages/solidworksPDM-coecis/tools/preinstall.ps1
+++ packages/solidworksPDM-coecis/tools/preinstall.ps1
@@ -1,7 +1,7 @@
 # Runs before the choco package is installed
 $INSTALL_DIR =  Join-Path $PSScriptRoot 'installer' 
 
-Start-Process -FilePath "$INSTALL_DIR\odbcsql.bat" -WindowStyle Hidden
+#Start-Process -FilePath "$INSTALL_DIR\odbcsql.bat" -WindowStyle Hidden
  
 #Copy-Item "$env:INSTALL_DIR\startbaja.cmd" "%PROGRAMFILES%\SOLIDWORKS PDM\startbaja.cmd"
 #gci -Path $INSTALL_DIR\Microsoft* -Filter *.exe -Recurse | % {Start-Process $_.FullName -ArgumentList "/q /norestart" -wait -Verbose}
