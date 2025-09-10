920a2ec87f314b7d94c880dbfa757c55e3cf760d
Dan Klinger <dpk25@cornell.edu>
2025-09-10 17:45:49 -0400

    Run webview and odbcsql installers

--- packages/solidworksPDM-coecis/tools/preinstall.ps1
+++ packages/solidworksPDM-coecis/tools/preinstall.ps1
@@ -1,7 +1,8 @@
 # Runs before the choco package is installed
-$INSTALL_DIR =  Join-Path $PSScriptRoot 'installer' 
+$INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
 
-#Start-Process -FilePath "$INSTALL_DIR\odbcsql.bat" -WindowStyle Hidden
- 
-#Copy-Item "$env:INSTALL_DIR\startbaja.cmd" "%PROGRAMFILES%\SOLIDWORKS PDM\startbaja.cmd"
-#gci -Path $INSTALL_DIR\Microsoft* -Filter *.exe -Recurse | % {Start-Process $_.FullName -ArgumentList "/q /norestart" -wait -Verbose}
+# Install ODBC SQL client
+Start-Process msiexec.exe -ArgumentList "/i $INSTALL_DIR\msodbcsql.msi /qn IACCEPTMSODBCSQLLICENSETERMS=YES" -Wait  
+
+# Install MS Edge Webview 2 offline installer
+Start-Process -FilePath "$INSTALL_DIR\MicrosoftEdgeWebView2RuntimeInstallerX64.exe" -ArgumentList "/silent /install" -Wait
