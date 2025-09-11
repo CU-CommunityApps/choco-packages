5522c875158c0c0a512bed30a6ff46ff23feee1d
Dan Klinger <dpk25@cornell.edu>
2025-09-11 08:36:05 -0400

    Update path for Baja_Vault.cvs in postinstall script

--- packages/solidworksPDM-coecis/tools/postinstall.ps1
+++ packages/solidworksPDM-coecis/tools/postinstall.ps1
@@ -1,3 +1,3 @@
 # Runs after the choco package is installed
 
-Start-Process -FilePath "$env:PROGRAMFILES\SOLIDWORKS PDM\ViewSetup.exe" -ArgumentList "$env:PROGRAMFILES\SOLIDWORKS PDM\Baja_Vault.cvs /q" -Wait
+Start-Process -FilePath "$env:PROGRAMFILES\SOLIDWORKS PDM\ViewSetup.exe" -ArgumentList "$env:SYSTEMDRIVE\Baja_Vault.cvs /q" -Wait
