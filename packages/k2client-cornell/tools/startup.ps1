01231de53da6de786c965aca539a4638ed0f4114
lb30 <39345501+lb30@users.noreply.github.com>
2025-10-08 15:21:18 -0400

    K2client cornell (#797)
    * Update config.yml
    
    * Update startup.ps1

--- packages/k2client-cornell/tools/startup.ps1
+++ packages/k2client-cornell/tools/startup.ps1
@@ -1,11 +1,13 @@
 # Create User Environment variable
-#[Environment]::SetEnvironmentVariable("AoD_Combo", "$env:AppStream_UserName.$env:AppStream_Resource_Name", "User")
+[Environment]::SetEnvironmentVariable("AoD_Combo", "$env:AppStream_UserName.$env:AppStream_Resource_Name", "User")
 
 # Populate variable with Streaming Instance specific values
-#$env:AoD_Combo = "$env:AppStream_UserName.$env:AppStream_Resource_Name"
-#Write-Output "$env:AoD_Combo"
+$env:AoD_Combo = "$env:AppStream_UserName.$env:AppStream_Resource_Name"
+Write-Output "$env:AoD_Combo"
 
 # Launch Sassafras client
 
 Start-Process "$env:windir\keyacc32.exe" -ArgumentList "-minimize"
 
+# Launch Sassafras Client Service
+Start-Service KeyAccess
