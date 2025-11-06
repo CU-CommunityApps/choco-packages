fe9f52efe68bf032e2a2adcab027bc60e6ff62f7
lb30 <39345501+lb30@users.noreply.github.com>
2025-11-06 14:01:35 -0500

    Update postinstall.ps1

--- packages/k2cit-cornell/tools/postinstall.ps1
+++ packages/k2cit-cornell/tools/postinstall.ps1
@@ -1,14 +1,13 @@
 # Runs after the choco package is installed
 
 # Stop any running processes
-#Stop-Process -Name keyacc32 -Force -ErrorAction SilentlyContinue
+Stop-Process -Name keyacc32 -Force -ErrorAction SilentlyContinue
 
 # Update user field as outlined by Support
-#New-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\KeyAccess\Settings\logon' -Name 'user' -Value '%AppStream_UserName%.%AppStream_Resource_Name%' -PropertyType ExpandString -Force
+New-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\KeyAccess\Settings\logon' -Name 'user' -Value '%AppStream_UserName%.%AppStream_Resource_Name%' -PropertyType ExpandString -Force
 
 
 # Remove any system specific values prior to sysprep
-#Remove-Item -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\KeyAccess\Settings\pref' -Recurse -Force -ErrorAction SilentlyContinue
-#Remove-Item -Path 'HKLM:\SYSTEM\CurrentControlSet\Sevices\KeyAccess\Settings\settings' -Recurse -Force -ErrorAction SilentlyContinue
-#Remove-Item -Path "$env:ALLUSERSPROFILE\KeyAccess" -Recurse -Force -ErrorAction SilentlyContinue
-
+Remove-Item -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\KeyAccess\Settings\pref' -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path 'HKLM:\SYSTEM\CurrentControlSet\Sevices\KeyAccess\Settings\settings' -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "$env:ALLUSERSPROFILE\KeyAccess" -Recurse -Force -ErrorAction SilentlyContinue
