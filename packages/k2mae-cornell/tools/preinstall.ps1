1f732f20a8d4efd03ed8c8c91ea967061970863a
lb30 <39345501+lb30@users.noreply.github.com>
2025-11-12 13:29:40 -0500

    K2mae cornell (#806)
    * k2mae-cornell
    
    * Update config.yml

---
+++ packages/k2mae-cornell/tools/preinstall.ps1
@@ -0,0 +1,3 @@
+# Runs before the choco package is installed
+
+New-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment' -Name "AoD_Combo" -Value '%AppStream_UserName%.%AppStream_Resource_Name%' -PropertyType ExpandString -Force
