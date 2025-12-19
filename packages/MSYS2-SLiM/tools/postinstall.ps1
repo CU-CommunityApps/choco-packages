62799b53fe44a4b87e080a10a6c274a9cfd5a2c8
Dan Klinger <dpk25@cornell.edu>
2025-12-19 15:39:27 -0500

    commented out post install command

--- packages/MSYS2-SLiM/tools/postinstall.ps1
+++ packages/MSYS2-SLiM/tools/postinstall.ps1
@@ -1,5 +1,5 @@
 # Runs after the choco package is installed
 
-start-sleep -second 60
-start-process "C:\msys64\msys2_shell.cmd" -argumentlist "-defterm -no-start -mingw64 -c "pacman -Syu --noconfirm" -wait
-start-process "C:\msys64\msys2_shell.cmd" -argumentlist "-defterm -no-start -mingw64 -c "pacman -S --noconfirm mingw-w64-x86_64-slim-simulator"
+#start-sleep -second 60
+#start-process "C:\msys64\msys2_shell.cmd" -argumentlist "-defterm -no-start -mingw64 -c "pacman -Syu --noconfirm" -wait
+#start-process "C:\msys64\msys2_shell.cmd" -argumentlist "-defterm -no-start -mingw64 -c "pacman -S --noconfirm mingw-w64-x86_64-slim-simulator"
