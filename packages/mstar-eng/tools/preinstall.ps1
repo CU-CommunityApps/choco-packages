c6b929082502b7eb5ea51a6fc1d19a24ef979778
James-VanEe <jiv2@cornell.edu>
2026-02-12 12:59:36 -0500

    troubleshoot mstar- skip preinstall
    troubleshoot mstar- skip preinstall

--- packages/mstar-eng/tools/preinstall.ps1
+++ packages/mstar-eng/tools/preinstall.ps1
@@ -4,4 +4,4 @@ $INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
 # Install prereq's - for some reason was failing - maybe a zip file issue?
 #Start-Process "$INSTALL_DIR\VC_redist.x86.exe" -ArgumentList "/quiet /norestart" -Wait
 #Start-Process "$INSTALL_DIR\VC_redist.x64.exe" -ArgumentList "/quiet /norestart" -Wait
-Start-Process "$INSTALL_DIR\msmpisetup.exe" -ArgumentList "-unattend -minimal" -Wait
+#Start-Process "$INSTALL_DIR\msmpisetup.exe" -ArgumentList "-unattend -minimal" -Wait
