900843ebdb46d32bd0951b26487e9b74b9a6d007
James-VanEe <jiv2@cornell.edu>
2026-02-12 14:07:03 -0500

    mstar - fixed zip file
    mstar - fixed zip file

--- packages/mstar-eng/tools/preinstall.ps1
+++ packages/mstar-eng/tools/preinstall.ps1
@@ -1,7 +1,7 @@
 # Runs before the choco package is installed 
 $INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
 
-# Install prereq's - for some reason was failing - maybe a zip file issue?
+# Install prereq's
 Start-Process "$INSTALL_DIR\VC_redist.x86.exe" -ArgumentList "/quiet /norestart" -Wait
 Start-Process "$INSTALL_DIR\VC_redist.x64.exe" -ArgumentList "/quiet /norestart" -Wait
 Start-Process "$INSTALL_DIR\msmpisetup.exe" -ArgumentList "-unattend -minimal" -Wait
