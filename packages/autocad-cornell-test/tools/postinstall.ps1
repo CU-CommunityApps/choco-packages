2e18e575d7ec3d855d8c26c87af9fe2f2f258c57
Dan Klinger <dpk25@cornell.edu>
2026-02-16 16:10:30 -0500

    Remove Reference Manager shortcut post-installation
    Added a command to remove a specific shortcut file after installation.

--- packages/autocad-cornell-test/tools/postinstall.ps1
+++ packages/autocad-cornell-test/tools/postinstall.ps1
@@ -1,1 +1,3 @@
 # Runs after the choco package is installed
+# Removing file based on Autodesk doc - https://www.autodesk.com/support/technical/article/caas/sfdcarticles/sfdcarticles/Warning-1909-Could-not-create-Shortcut-Add-A-Plot-Style-Table-Wizzard-lnk-Verify-that-the-destination-folder-exists-and-that-you-can-access-it-or-similar-error-error-when-launching-AutoCAD-2023-or-verticals.html
+rm "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\AutoCAD 2026 - English\Reference Manager.lnk" -Force
