977c71257e9845e47be1ae5b4761a16b55be30b6
Dan Klinger <dpk25@cornell.edu>
2026-02-19 16:55:43 -0500

    Update preinstall.ps1 to modify policy file path
    Changed the path for the policy file to C:\Temp and added output of current token permissions.

--- packages/autocad-cornell/tools/preinstall.ps1
+++ packages/autocad-cornell/tools/preinstall.ps1
@@ -1,11 +1,16 @@
+mkdir C:\temp -ErrorAction SilentlyContinue
+
 Write-Host "Get current group policy settings"
 gpupdate /target:computer /force /wait:-1
 
 Write-Host "Ensuring SYSTEM has symlink privs"
-$policyFile="$env:TEMP\secpol_current.inf"
+$policyFile="C:\Temp\secpol_current.inf"
 
 # export current group policy settings to file
 secedit /export /cfg $policyFile
 
 # read file contents for symlink privs
 Select-String -Path $policyFile -Pattern 'secreatesymboliclinkprivilege' -CaseSensitive:$false
+
+# output current token permissions
+whoami /all | Out-File -FilePath 'C:\Temp\system-token.txt' -Encoding UTF8 -Force
