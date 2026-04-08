a289dd6f6cc7ee631d419305c97f37af32943bac
James-VanEe <jiv2@cornell.edu>
2026-04-08 15:48:32 -0400

    Update base-cornell-utils to fix 7zip (#820)
    * Update preinstall.ps1
    
    changed to use github liink based on new notepad++ website. It should get the first x64.exe download which should be latest version.
    
    * removed notepad++ for testing
    
    removed noteplad++ to confirm it is what is failing to download
    
    * troubleshooting - remove 7zip
    
    troubleshooting - remove 7zip
    
    * added notepad++ back for troubleshooting
    
    added notepad++ back for troubleshooting with original config
    
    * put 7zip back to see if it fails
    
    * modify 7-zip preinstall for troubleshooting

--- packages/base-utils-cornell/tools/preinstall.ps1
+++ packages/base-utils-cornell/tools/preinstall.ps1
@@ -5,8 +5,11 @@ $TOOLS_DIR = $PSScriptRoot
 Start-BitsTransfer "https://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise64.msi" -Destination "$TOOLS_DIR\GoogleChromeStandaloneEnterprise64.msi"
 
 ## 7 Zip latest download link
-$7Zip = "https://www.7-zip.org/$((Invoke-WebRequest -Uri "https://www.7-zip.org/download.html" -UseBasicParsing | Select-Object -ExpandProperty Links | Where-Object -Property href -like "*-x64.msi")[0].href)"
+#$7Zip = "https://www.7-zip.org/$((Invoke-WebRequest -Uri "https://www.7-zip.org/download.html" -UseBasicParsing | Select-Object -ExpandProperty Links | Where-Object -Property href -like "*-x64.msi")[0].href)"
+$7Zip = "$((Invoke-WebRequest -Uri "https://www.7-zip.org/download.html" -UseBasicParsing | Select-Object -ExpandProperty Links | Where-Object -Property href -like "*-x64.msi")[0].href)"
 Start-BitsTransfer "$7Zip" -Destination "$TOOLS_DIR\7zip.msi"
+
+
 
 ## Notepad ++ Download
 $linkPath = ((Invoke-WebRequest -URI https://notepad-plus-plus.org -UseBasicParsing) | Select-Object -ExpandProperty links | Where-Object -Property href -like "/downloads/v*").href 
