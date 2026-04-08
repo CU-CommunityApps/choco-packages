05007fe4fc81ed6a7d31a50093f14cc1059e1efe
James-VanEe <jiv2@cornell.edu>
2026-04-08 11:24:10 -0400

    Update preinstall.ps1
    changed to use github liink based on new notepad++ website. It should get the first x64.exe download which should be latest version.

--- packages/base-utils-cornell/tools/preinstall.ps1
+++ packages/base-utils-cornell/tools/preinstall.ps1
@@ -9,7 +9,10 @@ $7Zip = "https://www.7-zip.org/$((Invoke-WebRequest -Uri "https://www.7-zip.org/
 Start-BitsTransfer "$7Zip" -Destination "$TOOLS_DIR\7zip.msi"
 
 ## Notepad ++ Download
-$linkPath = ((Invoke-WebRequest -URI https://notepad-plus-plus.org -UseBasicParsing) | Select-Object -ExpandProperty links | Where-Object -Property href -like "/downloads/v*").href 
-$downloadurl = "https://notepad-plus-plus.org$linkpath"
+##example after website changes April 2026
+##https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.9.3/npp.8.9.3.Installer.x64.exe
+#$linkPath = ((Invoke-WebRequest -URI https://notepad-plus-plus.org -UseBasicParsing) | Select-Object -ExpandProperty links | Where-Object -Property href -like "/downloads/v*").href 
+#$downloadurl = "https://notepad-plus-plus.org$linkpath"
+$downloadurl = "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/"
 $NotePadPlusPlus = ((Invoke-WebRequest -URI $downloadurl -UseBasicParsing) | Select-Object -ExpandProperty links | Where-Object -Property href -like "*npp.*.installer.x64.exe").href | Select-Object -Index 0
 Start-BitsTransfer "$NotePadPlusPlus" -Destination "$TOOLS_DIR\NotePadPlusPlus.exe"
