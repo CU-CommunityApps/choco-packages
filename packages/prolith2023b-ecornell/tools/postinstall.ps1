fcb7c39bcc4075c70c6bf8536107c2048ffe35e7
dwbotsch <botsch@cnf.cornell.edu>
2026-06-08 14:06:15 -0400

    Add template file for PROLITH 2023b: 	modified:   prolith2023b-ecornell/config.yml 	modified:   prolith2023b-ecornell/tools/postinstall.ps1

--- packages/prolith2023b-ecornell/tools/postinstall.ps1
+++ packages/prolith2023b-ecornell/tools/postinstall.ps1
@@ -1,2 +1,3 @@
 # Runs after the choco package is installed
 Copy-Item "$env:INSTALL_DIR\PROLITHDatabase.DB" "$env:PROGRAMFILES\PROLITH_Toolkit\PROLITH 2023b\PROLITHDatabase.DB" -Force
+Copy-Item "$env:INSTALL_DIR\eCornell.plt" "$env:PROGRAMFILES\PROLITH_Toolkit\PROLITH 2023b\PROLITH.plt" -Force
