3b2f4cda26ac234201c3a97bacd43b5ea60e0526
James-VanEe <jiv2@cornell.edu>
2026-08-03 14:57:16 -0400

    more clean up and reconfig for new version
    more clean up and reconfig for new version

---
+++ packages/arcgispro-cornell/tools/postinstall.ps1
@@ -0,0 +1,5 @@
+# Runs after the choco package is installed
+
+#commented out Aug 2026 - patch for an older version of ArcGIS
+#$INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
+#Start-Process msiexec.exe -ArgumentList '/p $INSTALL_DIR\ArcGISd1\ArcGIS_Pro_281_177644.msp /qn /norestart'
