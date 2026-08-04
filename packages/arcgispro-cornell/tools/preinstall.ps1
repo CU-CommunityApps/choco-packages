b26e3cf415e4b00b2d05b6f40efdc108282e9a66
James-VanEe <jiv2@cornell.edu>
2026-08-04 12:40:01 -0400

    ArcGIS (#835)
    * cleaning up old ArcGIS package configs
    
    cleaning up old ArcGIS package configs
    
    * more clean up and reconfig for new version
    
    more clean up and reconfig for new version
    
    * updated .net runtime
    
    updated .net runtime
    
    * fixed typo
    
    fixed typo
    
    * changed to /qn msi switch from /qb
    
    changed to /qn msi switch from /qb

--- packages/arcgispro-cornell/tools/preinstall.ps1
+++ packages/arcgispro-cornell/tools/preinstall.ps1
@@ -1,2 +1,9 @@
 # Runs before the choco package is installed
 
+$INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
+
+#Install other pre-reqs
+Start-Process "$INSTALL_DIR\VC_redist.x64.exe" -ArgumentList "/quiet /norestart" -Wait
+Start-Process "$INSTALL_DIR\VC_redist.x86.exe" -ArgumentList "/quiet /norestart" -Wait
+Start-Process "$INSTALL_DIR\windowsdesktop-runtime-10.0.10-win-x64.exe" -ArgumentList "/quiet /norestart" -Wait
+Start-Process "$INSTALL_DIR\MicrosoftEdgeWebView2RuntimeInstallerX64.exe" -ArgumentList "/silent /install" -Wait
--- packages/arcgispro-vet/tools/preinstall.ps1
+++
@@ -1,8 +0,0 @@
-# Runs before the choco package is installed
-
-$INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
-
-#Install other pre-reqs
-Start-Process "$INSTALL_DIR\VC_redist.x64.exe" -ArgumentList "/quiet /norestart" -Wait
-Start-Process "$INSTALL_DIR\VC_redist.x86.exe" -ArgumentList "/quiet /norestart" -Wait
-Start-Process "$INSTALL_DIR\windowsdesktop-runtime-6.0.21-win-x64.exe" -ArgumentList "/quiet /norestart" -Wait
