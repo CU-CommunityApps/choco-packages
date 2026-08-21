8c1c8f5c7876259c6ae662b051d78d803063b5f0
James-VanEe <jiv2@cornell.edu>
2026-08-21 10:32:45 -0400

    hardcode download link and increment description
    hardcode download link and increment description

--- packages/qgis-cornell/tools/preinstall.ps1
+++ packages/qgis-cornell/tools/preinstall.ps1
@@ -1,16 +1,17 @@
 # Runs before the choco package is installed
 
 #Download latest version of QGIS
-$data = Invoke-WebRequest -Uri "https://www.qgis.org/download/" -UseBasicParsing
+#$data = Invoke-WebRequest -Uri "https://www.qgis.org/download/" -UseBasicParsing
  
-$filteredData = $data.Links | Where-Object -Property outerHTML -like "*Latest Version for Windows*"
+#$filteredData = $data.Links | Where-Object -Property outerHTML -like "*Latest Version for Windows*"
 
-if ($filteredData.count -gt 1) {
-     $link = $filteredData[0].href }
-else {
-     $link = $filteredData.href
-}
+#if ($filteredData.count -gt 1) {
+#     $link = $filteredData[0].href }
+#else {
+#     $link = $filteredData.href
+#}
  
-$downloadLink = "https://www.qgis.org" + $link
+#$downloadLink = "https://www.qgis.org" + $link
  
+$downloadLink = "https://download.qgis.org/downloads/QGIS-OSGeo4W-3.44.13-1.msi"
 Start-BitsTransfer -Source $downloadLink -Destination "$PSScriptRoot\qgis.msi"
