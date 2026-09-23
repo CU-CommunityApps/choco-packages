522fd239c5663e5e0ed65d2a4b947c3b36314d7e
James-VanEe <jiv2@cornell.edu>
2026-09-23 15:08:35 -0400

    inital setup of civil3d and revit2027
    inital setup of civil3d and revit2027

--- packages/civil3d-cornell/tools/startup.ps1
+++ packages/civil3d-cornell/tools/startup.ps1
@@ -3,8 +3,8 @@
 $helperPath  = "${env:CommonProgramFiles(x86)}\Autodesk Shared\AdskLicensing\Current\helper\AdskLicensingInstHelper.exe"
 $configFile  = "$env:ProgramData\Autodesk\adlm\Productinformation.pit"
 # License settings
-$prodKey     = "829R1"
-$prodVer     = "2026.0.0.F"
+$prodKey     = "237S1"
+$prodVer     = "2027.0.0.F"
 $licMethod   = "NETWORK"
 $licServers  = "$env:ADSKFLEX_LICENSE_FILE"
 # Build argument string
